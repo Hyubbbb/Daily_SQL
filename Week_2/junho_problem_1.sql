3b1d1ed3a061f5601814f5d384fe2725a7199c0a
LEE JUN HO <100112445+JunhoLee107@users.noreply.github.com>
2024-07-08 21:43:53 +0900

    RANK() OVER 기억하자

---
+++ Week_2/junho_problem_1.sql
@@ -0,0 +1,12 @@
+-- (https://school.programmers.co.kr/learn/courses/30/lessons/301649, Lv.3)
+
+SELECT ID, 
+CASE WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.25) THEN 'CRITICAL'
+WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.5) THEN 'HIGH'
+WHEN rank_size <= ((SELECT COUNT(*) FROM ECOLI_DATA) * 0.75) THEN 'MEDIUM'
+ELSE 'LOW'
+END AS COLONY_NAME
+
+FROM (SELECT ID, RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS rank_size
+      FROM ECOLI_DATA) AS a
+ORDER BY ID;
