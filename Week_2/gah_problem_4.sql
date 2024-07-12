4530440c0b1f5ff222404438c7eee94903f888c0
gah508 <109083740+gah508@users.noreply.github.com>
2024-07-12 23:11:05 +0900

    Create gah_problem_4.sql

---
+++ Week_2/gah_problem_4.sql
@@ -0,0 +1,11 @@
+-- [프로그래머스]
+
+SELECT u.USER_ID, u.NICKNAME,
+       CONCAT(u.CITY, " ", u.STREET_ADDRESS1, " ", u.STREET_ADDRESS2) '전체주소',
+       CONCAT(SUBSTR(u.TLNO, 1, 3), "-", SUBSTR(u.TLNO, 4, 4), "-", SUBSTR(u.TLNO, 8, 4)) "전화번호"
+FROM USED_GOODS_USER u, (SELECT WRITER_ID
+                         FROM USED_GOODS_BOARD b
+                         GROUP BY WRITER_ID
+                         HAVING COUNT(BOARD_ID) >= 3) t
+WHERE u.USER_ID = t.WRITER_ID
+ORDER BY 1 DESC;
