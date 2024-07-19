a6872a699e9f76fbd75d4f8d4970a0c271863a60
Yoonseok Kang <kangcl1609@gmail.com>
2024-07-19 19:18:04 +0900

    할만해~

---
+++ Week_3/YS_Problem_1.sql
@@ -0,0 +1,8 @@
+# 흉부외과 또는 일반외과 의사 목록 출력하기
+#각각 조건을 표시, 혹은 IN 쓰기
+
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS', 'GS') 
+ORDER BY HIRE_YMD DESC, DR_NAME;
