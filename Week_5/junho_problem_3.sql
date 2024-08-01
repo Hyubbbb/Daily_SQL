8f1c8f825666201fc390a46f6ca2821a7a7ec862
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2024-08-01 15:35:46 +0900

    쉽다

---
+++ Week_5/junho_problem_3.sql
@@ -0,0 +1,7 @@
+-- (https://school.programmers.co.kr/learn/courses/30/lessons/59412, Lv.2)
+
+SELECT HOUR(DATETIME) AS HOUR, COUNT(*) AS COUNT
+FROM ANIMAL_OUTS
+WHERE HOUR(DATETIME) >=9 AND HOUR(DATETIME)<20
+GROUP BY 1
+ORDER BY 1;
