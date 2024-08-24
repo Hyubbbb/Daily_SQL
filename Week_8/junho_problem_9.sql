f43140abfaa2b262fa6322c1a9cac71c5878e882
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2024-08-24 15:16:59 +0900

    쉽다

---
+++ Week_8/junho_problem_9.sql
@@ -0,0 +1,7 @@
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE,'%Y-%m-%d') AS OUT_DATE,
+        CASE WHEN OUT_DATE IS NULL THEN '출고미정'
+            WHEN '2022-05-01' >= OUT_DATE THEN '출고완료'
+            ELSE '출고대기'
+            END AS '출고여부'
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
