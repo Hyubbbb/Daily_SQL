7fecd2ccea57f3c69fa8a5dcbc1478a6600e147d
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2024-09-10 15:38:08 +0900

    쉽다

---
+++ Week_2/junho_problem_4.sql
@@ -0,0 +1,8 @@
+-- (https://www.hackerrank.com/challenges/what-type-of-triangle/problem?isFullScreen=true)
+
+SELECT CASE WHEN A+B<=C or B+C<=A or C+A<=B THEN 'Not A Triangle'
+            WHEN A=B AND B=C THEN 'Equilateral'
+            WHEN A=B or B=C or C=A THEN 'Isosceles'
+            ELSE 'Scalene'
+            END AS t
+FROM triangles;
