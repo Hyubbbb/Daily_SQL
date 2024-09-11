946b84d6b73b8d2764b686ceff0bbf03fdc81a4b
hiingjan <113091000+hiingjan@users.noreply.github.com>
2024-09-11 16:23:10 +0900

    Create hyungjun_problem_1.sql
    1

---
+++ Week_2/hyungjun_problem_1.sql
@@ -0,0 +1,8 @@
+# Level 1 | 아픈 동물 찾기 | https://school.programmers.co.kr/learn/courses/30/lessons/59036
+
+
+
+SELECT ANIMAL_ID, NAME
+FROM ANIMAL_INS
+WHERE INTAKE_CONDITION = 'Sick'
+ORDER BY ANIMAL_ID ASC
