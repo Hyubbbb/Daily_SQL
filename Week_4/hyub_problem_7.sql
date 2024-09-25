b6cbfe49651ec7dd20cf0e41e95c911a6343ff66
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2024-09-25 22:59:30 +0900

    EZ

---
+++ Week_4/hyub_problem_7.sql
@@ -0,0 +1,10 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-17/problem?isFullScreen=true
+
+SELECT ROUND(LONG_W, 4)
+FROM STATION
+WHERE LAT_N =
+    (SELECT LAT_N
+    FROM STATION
+    WHERE LAT_N > 38.7780
+    ORDER BY LAT_N
+    LIMIT 1)
