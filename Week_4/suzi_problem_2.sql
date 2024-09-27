10279fc22c8527542a3f10c690ccc41f14ceeb27
Suji Yeom <127907081+Yeom-Suji@users.noreply.github.com>
2024-09-27 22:53:41 +0900

    Create suzi_problem_2.sql

---
+++ Week_4/suzi_problem_2.sql
@@ -0,0 +1,14 @@
+
+
+SELECT
+    COUNT(*) AS FISH_COUNT
+FROM
+    FISH_INFO
+WHERE
+    FISH_TYPE IN (
+    	SELECT 
+        	FISH_TYPE 
+        FROM 
+        	FISH_NAME_INFO 
+        WHERE FISH_NAME IN ('BASS', 'SNAPPER')
+    )
