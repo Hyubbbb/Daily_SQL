77bfdfa144a5a6bebfcf853ba1a58c724d990971
gah508 <109083740+gah508@users.noreply.github.com>
2024-10-04 20:02:54 +0900

    Create gah_problem_2.sql

---
+++ Week_5/gah_problem_2.sql
@@ -0,0 +1,7 @@
+-- [PROGRAMMERS] SELECT > 12세 이하인 여자 환자 목록 출력하기
+
+SELECT PT_NAME, PT_NO, GEND_CD, AGE, CASE WHEN TLNO IS NULL THEN 'NONE'
+                                     ELSE TLNO END TLNO
+FROM PATIENT
+WHERE AGE <= 12 AND GEND_CD = 'W'
+ORDER BY 4 DESC, 1;
