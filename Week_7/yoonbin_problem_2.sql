8516d65ce50d030a304697eab7ae949fa9ba9275
Y1OO5B <113369031+Y1OO5B@users.noreply.github.com>
2024-10-18 17:27:14 +0900

    week7

---
+++ Week_7/yoonbin_problem_2.sql
@@ -0,0 +1,12 @@
+--대장균의 크기에 따라 분류하기 1
+SELECT 
+    ID, 
+    CASE 
+        WHEN SIZE_OF_COLONY <= 100 THEN 'LOW'
+        WHEN SIZE_OF_COLONY > 100 AND SIZE_OF_COLONY <= 1000 THEN 'MEDIUM'
+        ELSE 'HIGH'
+    END AS SIZE
+FROM 
+    ECOLI_DATA
+ORDER BY 
+    ID ASC;
