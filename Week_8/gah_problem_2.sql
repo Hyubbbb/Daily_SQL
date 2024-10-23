bfb6d881b6e8277a29c5ca0ff4104b2e3e04fc7a
gah508 <109083740+gah508@users.noreply.github.com>
2024-10-24 00:57:03 +0900

    Create gah_problem_2.sql

---
+++ Week_8/gah_problem_2.sql
@@ -0,0 +1,9 @@
+-- [PROGRAMMERS] String, Date > 중성화 여부 파악하기
+
+SELECT ANIMAL_ID, NAME,
+       (CASE WHEN SEX_UPON_INTAKE LIKE 'Neutered%' THEN 'O'
+             WHEN SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O'
+             ELSE 'X'
+        END) '중성화'
+FROM ANIMAL_INS
+ORDER BY 1
