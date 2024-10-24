7240299fc5c68ad13bf825d4e53e29b4d71db09c
hiingjan <113091000+hiingjan@users.noreply.github.com>
2024-10-25 02:24:42 +0900

    Create hyungjun_problem_2.sql

---
+++ Week_8/hyungjun_problem_2.sql
@@ -0,0 +1,6 @@
+Python 개발자 찾기 | https://school.programmers.co.kr/learn/courses/30/lessons/276013
+
+SELECT ID, EMAIL, FIRSTNAME, LASTNAME
+FROM DEVELOPERINFOS
+WHERE "Python" IN(SKILL1, SKILL2, SKILL3)
+ORDER BY ID
