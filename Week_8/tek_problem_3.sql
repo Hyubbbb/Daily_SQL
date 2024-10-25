3d93236a485361b732ea0ee982c81e1e3d9732c5
benidjor <benidjor@gmail.com>
2024-10-25 20:20:13 +0900

    HackerRank 3

---
+++ Week_8/tek_problem_3.sql
@@ -0,0 +1,7 @@
+/* HackerRank Revising the Select Query II
+https://www.hackerrank.com/challenges/revising-the-select-query-2/submissions/code/318754292 */
+
+select name
+from city
+where population > 120000
+and countrycode = "USA"
