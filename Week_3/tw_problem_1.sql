dd6c132943dcd51eb36df404d674d601c7f105af
tux2won <114200466+tux2won@users.noreply.github.com>
2024-11-21 00:36:46 +0200

    Create tw_problem_1.sql

---
+++ Week_3/tw_problem_1.sql
@@ -0,0 +1,8 @@
+-- 조회수가 가장 높은 중고거래 게시물에 대한 첨부파일 경로를 조회하는 SQL문
+-- 입력한 순서대로 합쳐
+SELECT CONCAT('/home/grep/src/', a.BOARD_ID, '/', FILE_ID, FILE_NAME, FILE_EXT) AS FILE_PATH
+FROM USED_GOODS_BOARD a JOIN USED_GOODS_FILE b ON a.BOARD_ID = b.BOARD_ID
+WHERE VIEWS = (
+    SELECT MAX(VIEWS)
+    FROM USED_GOODS_BOARD)
+ORDER BY FILE_ID DESC
