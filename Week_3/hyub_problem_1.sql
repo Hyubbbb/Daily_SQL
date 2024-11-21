8393d07a1f659b3095a0be59ca0e876fb6e94630
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2024-11-21 23:04:49 +0900

    데이터셋을 잘 살펴보자

---
+++ Week_3/hyub_problem_1.sql
@@ -0,0 +1,10 @@
+-- https://solvesql.com/problems/installment-month/
+
+SELECT payment_installments, 
+  COUNT(DISTINCT order_id) AS order_count, 
+  MIN(payment_value) AS min_value, 
+  MAX(payment_value) AS max_value, 
+  AVG(payment_value) AS avg_value
+FROM olist_order_payments_dataset
+WHERE payment_type = 'credit_card'
+GROUP BY payment_installments
