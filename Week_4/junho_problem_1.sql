e0fd45e9b042140763bd8d0c41df0beab8cd39c9
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2024-11-29 21:07:05 +0900

    Update junho_problem_1.sql

--- Week_4/junho_problem_1.sql
+++ Week_4/junho_problem_1.sql
@@ -1,4 +1,4 @@
-SELECT project_id, AVG(experience_years) as average_years
+SELECT project_id, ROUND(AVG(experience_years),2) as average_years
 FROM Project as p
 JOIN Employee as e
 ON p.employee_id = e.employee_id
