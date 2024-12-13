f1aa12898ad39765ff339ac453265407fe19c4c6
Dahee-J <heeda0928@gmail.com>
2024-12-13 21:30:07 +0900

    W6

---
+++ Week_6/dahee_problem_2.sql
@@ -0,0 +1,6 @@
+-- Programmers > IS NULL > 이름이 없는 동물의 아이디
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59039
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID ASC;
