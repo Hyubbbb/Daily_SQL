6d8cedfab86ca3ff681422fc25c45d016811ef59
Hyubbbb <ekwjsxl200@gmail.com>
2024-12-30 10:27:50 +0900

    시즌 4 시작

--- Week_8/sh_problem_1.sql
+++
@@ -1,3 +0,0 @@
--- datetime에서 date로 형변환
-SELECT animal_id, name, date_format(datetime, '%Y-%m-%d')
-FROM animal_ins
--- Week_8/sh_problem_2.sql
+++
@@ -1,7 +0,0 @@
--- 입양 시각 구하기(1)
-SELECT hours as hour, count(hours)
-FROM (SELECT hour(datetime) as hours
-        FROM animal_outs
-        WHERE hour(datetime)>=9 and hour(datetime)<20) as T
-group by hours
-order by hour asc
