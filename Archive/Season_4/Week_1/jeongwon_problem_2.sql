5d186173912e6d341c4cc1cf8043022e1ab470e0
jeongwon8u8 <jjeongwon8u8@gmail.com>
2025-01-03 16:04:48 +0900

    w1

---
+++ Archive/Season_4/Week_1/jeongwon_problem_2.sql
@@ -0,0 +1,7 @@
+-- SELECT: 흉부외과 또는 일반외과 의사 목록 출력하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132203
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD IN ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
