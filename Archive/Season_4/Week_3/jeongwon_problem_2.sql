8946ab5202b4eb28862e9526a5e384f6f6b0da22
jeongwon8u8 <jjeongwon8u8@gmail.com>
2025-01-17 23:22:47 +0900

    w3

---
+++ Archive/Season_4/Week_3/jeongwon_problem_2.sql
@@ -0,0 +1,11 @@
+-- GROUP BY: 물고기 종류 별 잡은 수 구하기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/293257
+
+SELECT 
+    COUNT(FI.ID) AS FISH_COUNT,
+    FNI.FISH_NAME
+FROM FISH_INFO FI
+JOIN FISH_NAME_INFO FNI
+ON FI.FISH_TYPE = FNI.FISH_TYPE
+GROUP BY FNI.FISH_NAME
+ORDER BY FISH_COUNT DESC;
