f2c40cbc5beb42e6a32d575d8aa69866c6a5a18f
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-01-26 15:22:51 +0900

    COALESCE 스펠링을 맨날 까먹네

---
+++ Archive/Season_4/Week_5/hyub_problem_2.sql
@@ -0,0 +1,37 @@
+-- https://solvesql.com/problems/number-of-friends/
+
+WITH t_cnt_a AS (
+  SELECT
+      user_a_id
+    , COUNT(*) AS cnt_a
+    -- , SUM(CASE 
+    --       WHEN user_b_id THEN 1
+    --       ELSE 0 END) AS cnt_a
+  FROM edges
+  GROUP BY user_a_id),
+
+t_cnt_b AS (
+  SELECT
+      user_b_id
+    , COUNT(*) AS cnt_b
+    -- , SUM(CASE 
+    --       WHEN user_a_id THEN 1
+    --       ELSE 0 END) AS cnt_b
+  FROM edges
+  GROUP BY user_b_id),
+
+t_cnt_total AS(
+  SELECT
+      COALESCE(user_a_id, user_b_id) AS user_id
+    , COALESCE(cnt_a, 0) + COALESCE(cnt_b, 0) AS num_friends
+  FROM t_cnt_a a
+  FULL OUTER JOIN t_cnt_b b 
+    on a.user_a_id = b.user_b_id)
+
+SELECT
+    u.user_id
+  , IFNULL(num_friends, 0) AS num_friends
+FROM users u
+LEFT JOIN t_cnt_total t
+  on u.user_id = t.user_id
+ORDER BY num_friends DESC, u.user_id ASC
