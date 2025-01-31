0107f959eb3769eecf8ebe90ca53ee0976b5ca30
tux2won <114200466+tux2won@users.noreply.github.com>
2025-01-31 17:40:47 +0900

    HackerRank_Basic (삼각형 조건 진짜 오랜만..)

---
+++ Archive/Season_4/Week_5/tw_problem_3.sql
@@ -0,0 +1,6 @@
+SELECT CASE 
+      WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+      WHEN A = B AND B = C THEN 'Equilateral'
+      WHEN A = B OR B = C OR A = C THEN 'Isosceles'
+      ELSE 'Scalene' END AS Triangle_Type
+FROM TRIANGLES
