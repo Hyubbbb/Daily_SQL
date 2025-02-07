be1ad7829eeb66abd34a72806332a4eca9a0e16f
jeongwon8u8 <jjeongwon8u8@gmail.com>
2025-02-07 21:40:46 +0900

    w6

---
+++ Archive/Season_4/Week_6/jeongwon_problem_3.sql
@@ -0,0 +1,8 @@
+-- JOIN: 없어진 기록 찾기
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT O.ANIMAL_ID, O.NAME
+FROM ANIMAL_OUTS O
+LEFT JOIN ANIMAL_INS I ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE I.ANIMAL_ID IS NULL
+ORDER BY O.ANIMAL_ID;
