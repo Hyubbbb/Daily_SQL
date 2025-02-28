9687a39d69992e4466c4697acaf15a7e4b329ac0
tux2won <114200466+tux2won@users.noreply.github.com>
2025-02-28 22:00:08 +0900

    HackerRank_Intermediate

---
+++ Archive/Season_4/Week_9/tw_problem_2.sql
@@ -0,0 +1,12 @@
+WITH m AS (
+    SELECT h.hacker_id, h.name, s.challenge_id, MAX(score) AS score
+    FROM Hackers h
+    RIGHT JOIN Submissions s ON h.hacker_id = s.hacker_id
+    GROUP BY h.hacker_id, h.name, s.challenge_id)
+
+SELECT hacker_id, name, SUM(score) AS total_score
+FROM m
+GROUP BY hacker_id, name
+HAVING SUM(score) > 0
+ORDER BY total_score desc, hacker_id
+ 
