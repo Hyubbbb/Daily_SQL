4ee7e35c1479eb5c17c4eabec19c6b461a349b9f
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-03-11 12:30:08 +0900

    week2

---
+++ Archive/Season_5/Week_2/hj_problem_1.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/298518
+
+SELECT COUNT(*) AS FISH_COUNT
+FROM FISH_INFO I 
+LEFT JOIN FISH_NAME_INFO N ON I.FISH_TYPE=N.FISH_TYPE
+WHERE FISH_NAME IN ('BASS', 'SNAPPER');
