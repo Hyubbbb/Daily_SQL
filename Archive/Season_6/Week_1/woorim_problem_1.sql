2da5688c94ef310a063419ba74d589333c7e0693
imiroow <61489069+imiroow@users.noreply.github.com>
2025-05-09 00:16:14 +0900

    안녕하세요 신입입니다

---
+++ Archive/Season_6/Week_1/woorim_problem_1.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131536
+SELECT USER_ID, PRODUCT_ID
+FROM ONLINE_SALE
+GROUP BY USER_ID, PRODUCT_ID
+HAVING COUNT(*) >= 2
+ORDER BY USER_ID ASC, PRODUCT_ID DESC;
