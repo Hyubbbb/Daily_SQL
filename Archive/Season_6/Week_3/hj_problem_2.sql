c906b573d3e88973db4ffb2bb0dbae7b3c4d6957
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-05-23 23:55:54 +0900

    Add files via upload

---
+++ Archive/Season_6/Week_3/hj_problem_2.sql
@@ -0,0 +1,7 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131115
+SELECT PRODUCT_ID, PRODUCT_NAME, PRODUCT_CD, CATEGORY, PRICE
+FROM FOOD_PRODUCT
+WHERE PRICE = (
+    SELECT MAX(PRICE)
+    FROM FOOD_PRODUCT
+);
