80d7244b90c01832235dd7f9dbcae0e6b8fad3df
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-07-14 20:00:01 +0900

    SUBSTRING 너무 오랜만이라 잊고 지냈어

---
+++ Archive/Season_7/Week_3/hyub_problem_2.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/fix-names-in-a-table/?envType=study-plan-v2&envId=top-sql-50
+SELECT
+    user_id,
+    CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id
