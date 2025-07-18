0e91d159f881dd1a79af9c2101cef167e3532b02
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-07-18 23:56:04 +0900

    Add files via upload

---
+++ Archive/Season_7/Week_3/hj_problem_2.sql
@@ -0,0 +1,9 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/284530
+SELECT 
+  YEAR(YM) AS YEAR, 
+  ROUND(AVG(PM_VAL1), 2) AS PM10, 
+  ROUND(AVG(PM_VAL2), 2) AS "PM2.5"
+FROM AIR_POLLUTION
+WHERE LOCATION2 = '수원'
+GROUP BY YEAR(YM)
+ORDER BY YEAR ASC;
