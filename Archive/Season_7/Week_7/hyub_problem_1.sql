a52cda2343b97e20eaeb3114d72a5d434b50f6f3
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-08-15 17:42:33 +0900

    GROUP_CONCAT, COUNT(DISTINCT)

---
+++ Archive/Season_7/Week_7/hyub_problem_1.sql
@@ -0,0 +1,9 @@
+-- https://leetcode.com/problems/group-sold-products-by-the-date/?envType=study-plan-v2&envId=top-sql-50
+
+SELECT
+    sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product ASC) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
