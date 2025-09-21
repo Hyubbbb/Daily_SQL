a5ff47ee3348fea5366503cf2ce091c55b1cb5c8
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-09-21 13:46:39 +0900

    Submit: S8_W1_김협

---
+++ programmers/select/대장균의_크기에_따라_분류하기_2/hyub.sql
@@ -0,0 +1,10 @@
+SElECT
+    id,
+    CASE 
+        WHEN PERCENT_RANK() OVER(ORDER BY size_of_colony DESC) <= 0.25 THEN 'CRITICAL'
+        WHEN PERCENT_RANK() OVER(ORDER BY size_of_colony DESC) <= 0.50 THEN 'HIGH'
+        WHEN PERCENT_RANK() OVER(ORDER BY size_of_colony DESC) <= 0.75 THEN 'MEDIUM'
+        ELSE 'LOW'
+    END AS colony_name
+FROM ecoli_data
+ORDER BY id;
