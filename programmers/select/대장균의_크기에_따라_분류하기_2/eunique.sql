7b1dc8344b7776d700251945a5b0eb1f6e580004
Eunji Lee <jiyomi30@gmail.com>
2025-09-21 23:24:23 +0900

    Create eunique.sql

---
+++ programmers/select/대장균의_크기에_따라_분류하기_2/eunique.sql
@@ -0,0 +1,12 @@
+SELECT
+    id,
+    CASE NTILE(4) OVER (ORDER BY size_of_colony DESC)
+        WHEN 1 THEN 'CRITICAL'
+        WHEN 2 THEN 'HIGH'
+        WHEN 3 THEN 'MEDIUM'
+        WHEN 4 THEN 'LOW'
+    END AS colony_name
+FROM
+    ecoli_data
+ORDER BY
+    id;
