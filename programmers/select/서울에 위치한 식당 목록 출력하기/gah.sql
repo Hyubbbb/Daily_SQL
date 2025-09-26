cd12f7db482a93260d5dcd17599173f33d80b16e
gah508 <109083740+gah508@users.noreply.github.com>
2025-09-26 16:55:04 +0900

    Create gah.sql

---
+++ programmers/select/서울에 위치한 식당 목록 출력하기/gah.sql
@@ -0,0 +1,18 @@
+SELECT
+    i.rest_id,
+    i.rest_name,
+    i.food_type,
+    i.favorites,
+    i.address,
+    ROUND(AVG(r.review_score), 2) AS score
+FROM
+    rest_info AS i,
+    rest_review AS r
+WHERE 1=1
+    AND i.rest_id = r.rest_id
+    AND i.address LIKE '서울%'
+GROUP BY
+    i.rest_id
+ORDER BY
+    6 DESC,
+    4 DESC;
