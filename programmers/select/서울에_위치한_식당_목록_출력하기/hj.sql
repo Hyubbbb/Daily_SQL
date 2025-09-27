ed80e35546d090d1ae3b641a1b74ff1aa3117464
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-09-27 19:37:17 +0900

    week2

---
+++ programmers/select/서울에_위치한_식당_목록_출력하기/hj.sql
@@ -0,0 +1,13 @@
+SELECT 
+    r1.rest_id,
+    r1.rest_name,
+    r1.food_type,
+    r1.favorites,
+    r1.address,
+    ROUND(AVG(r2.review_score), 2) AS score
+FROM rest_info AS r1
+    INNER JOIN rest_review AS r2 
+        ON r1.rest_id = r2.rest_id
+WHERE r1.address LIKE '서울%'
+GROUP BY r1.rest_id
+ORDER BY score DESC, r1.favorites DESC;
