f9c33e4fc9d7912aead30ddcc30416954746b952
Jungmin Park <bakjeongmin021009@gmail.com>
2025-09-27 23:00:29 +0900

    Week2

---
+++ programmers/select/서울에_위치한_식당_목록_출력하기/jungmin.sql
@@ -0,0 +1,17 @@
+SELECT
+    r1.rest_id,
+    r1.rest_name,
+    r1.food_type,
+    r1.favorites,
+    r1.address, 
+    ROUND(AVG(r2.review_score), 2) as score
+FROM rest_info as r1
+    JOIN rest_review as r2
+        ON r1.rest_id = r2.rest_id
+WHERE 1=1
+    AND r1.address LIKE '서울%'
+GROUP BY
+    r1.rest_id
+ORDER BY
+    score DESC,
+    r1.favorites DESC
