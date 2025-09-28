f7b82b6bdecf33ed4dd53b0a1c650db426571f56
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-09-28 20:31:21 +0900

    Submit

---
+++ programmers/select/서울에_위치한_식당_목록_출력하기/hyub.sql
@@ -0,0 +1,13 @@
+SELECT 
+    ri.rest_id,
+    ri.rest_name,
+    ri.food_type,
+    ri.favorites,
+    ri.address,
+    ROUND(AVG(rr.review_score), 2) AS score
+FROM rest_info AS ri
+JOIN rest_review AS rr
+    ON ri.rest_id = rr.rest_id
+WHERE address LIKE '서울%'
+GROUP BY ri.rest_id, ri.rest_name, ri.food_type, ri.favorites, ri.address
+ORDER BY score DESC, favorites DESC
