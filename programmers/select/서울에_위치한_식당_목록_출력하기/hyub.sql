edc43f2bead23515570226d11c4a8582c178f691
Hyub Kim <98584348+Hyubbbb@users.noreply.github.com>
2025-09-28 21:19:03 +0900

    Update: JOIN 들여쓰기 추가

--- programmers/select/서울에_위치한_식당_목록_출력하기/hyub.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/hyub.sql
@@ -6,8 +6,8 @@ SELECT
     ri.address,
     ROUND(AVG(rr.review_score), 2) AS score
 FROM rest_info AS ri
-JOIN rest_review AS rr
-    ON ri.rest_id = rr.rest_id
+    JOIN rest_review AS rr
+        ON ri.rest_id = rr.rest_id
 WHERE address LIKE '서울%'
 GROUP BY ri.rest_id, ri.rest_name, ri.food_type, ri.favorites, ri.address
 ORDER BY score DESC, favorites DESC
