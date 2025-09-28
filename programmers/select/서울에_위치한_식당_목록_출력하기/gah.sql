37bf2ed2a4056766ccbdb8af3cb78eb750adf17c
gah508 <109083740+gah508@users.noreply.github.com>
2025-09-28 21:54:20 +0900

    Update: 줄바꿈

--- programmers/select/서울에_위치한_식당_목록_출력하기/gah.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/gah.sql
@@ -5,14 +5,10 @@ SELECT
     i.favorites,
     i.address,
     ROUND(AVG(r.review_score), 2) AS score
-FROM
-    rest_info AS i,
-    rest_review AS r
+FROM rest_info AS i,
+     rest_review AS r
 WHERE 1=1
     AND i.rest_id = r.rest_id
     AND i.address LIKE '서울%'
-GROUP BY
-    i.rest_id
-ORDER BY
-    6 DESC,
-    4 DESC;
+GROUP BY i.rest_id
+ORDER BY 6 DESC, 4 DESC;
