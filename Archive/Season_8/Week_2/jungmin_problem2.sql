13a86821e89cc7789067879d63930bfd059c0b76
Jungmin Park <bakjeongmin021009@gmail.com>
2025-09-28 21:06:01 +0900

    Update jungmin_problem2.sql

--- Archive/Season_8/Week_2/jungmin_problem2.sql
+++ Archive/Season_8/Week_2/jungmin_problem2.sql
@@ -2,12 +2,10 @@
 SELECT
     p.product_code,
     SUM (p.price * o.sales_amount) as sales
-FROM
-    product as p
+FROM product as p
     JOIN offline_sale as o
         ON p.product_id = o.product_id
-GROUP BY
-    p.product_code
+GROUP BY p.product_code
 ORDER BY 
     sales DESC,
     p.product_code ASC
