78b8a52e40e4e68407084c7a7315a97e60d1e047
chaewonsong <chaewon1019@ewhain.net>
2025-10-04 00:50:11 +0900

    refactor: SQL 쿼리 컨벤션 적용

--- programmers/select/서울에_위치한_식당_목록_출력하기/chaewon.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/chaewon.sql
@@ -1,7 +1,14 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131118
-SELECT i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS, ROUND(AVG(r.REVIEW_SCORE), 2) AS SCORE
-FROM  REST_INFO i
-JOIN  REST_REVIEW r ON i.REST_ID = r.REST_ID
-WHERE i.ADDRESS LIKE '서울%'
-GROUP BY i.REST_ID, i.REST_NAME, i.FOOD_TYPE, i.FAVORITES, i.ADDRESS
-ORDER BY SCORE DESC, i.FAVORITES DESC;
+SELECT 
+    i.rest_id,
+    i.rest_name,
+    i.food_type,
+    i.favorites,
+    i.address,
+    ROUND(AVG(r.review_score), 2) AS score
+FROM rest_info AS i
+    INNER JOIN rest_review AS r
+        ON i.rest_id = r.rest_id
+WHERE i.address LIKE '서울%'
+GROUP BY i.rest_id
+ORDER BY score DESC, i.favorites DESC;
