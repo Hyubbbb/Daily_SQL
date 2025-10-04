fda31a086c59725356b442f7d26602aa10ccb1a6
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-10-04 19:02:20 +0900

    Submit: 이형주

---
+++ programmers/select/오프라인_온라인_판매_데이터_통합하기/hj.sql
@@ -0,0 +1,27 @@
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date,
+    product_id,
+    user_id,
+    sales_amount
+FROM online_sale
+WHERE 
+    1=1 AND
+    YEAR(sales_date) = 2022 AND
+    2=2 AND
+    MONTH(sales_date) = 3
+UNION ALL
+SELECT 
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, 
+    product_id, 
+    NULL as user_id, 
+    sales_amount
+FROM offline_sale
+WHERE 
+    1=1 AND
+    YEAR(sales_date) = 2022 AND
+    2=2 AND
+    MONTH(sales_date) = 3
+ORDER BY 
+    sales_date ASC, 
+    product_id ASC,
+    user_id ASC; 
