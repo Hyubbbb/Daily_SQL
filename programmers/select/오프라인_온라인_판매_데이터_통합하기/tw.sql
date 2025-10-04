c02c30c74a3dffaadc7adede58d5b769e8f1b15d
tux2won <114200466+tux2won@users.noreply.github.com>
2025-10-04 17:34:57 +0900

    Refactor SQL query for online sales data

---
+++ programmers/select/오프라인_온라인_판매_데이터_통합하기/tw.sql
@@ -0,0 +1,18 @@
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS 'sales_date',
+    product_id,
+    user_id,
+    sales_amount
+FROM online_sale
+WHERE 1=1
+    AND DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+UNION ALL
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS 'sales_date',
+    product_id,
+    NULL,
+    sales_amount
+FROM offline_sale
+WHERE 1=1
+    AND DATE_FORMAT(sales_date, '%Y-%m') = '2022-03'
+ORDER BY 1, 2, 3
