5514412cdeddc1e95aa052f87954866b70288b71
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-10-04 23:50:09 +0900

    Submit: 이지윤

---
+++ programmers/select/오프라인_온라인_판매_데이터_통합하기/jiyun.sql
@@ -0,0 +1,20 @@
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date,
+    product_id,
+    user_id,
+    sales_amount
+FROM online_sale
+WHERE 1=1
+    AND sales_date LIKE '2022-03%'
+
+UNION
+
+SELECT
+    DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date,
+    product_id,
+    NULL,
+    sales_amount
+FROM offline_sale
+WHERE 1=1
+    AND sales_date LIKE '2022-03%'
+ORDER BY sales_date, product_id, user_id
