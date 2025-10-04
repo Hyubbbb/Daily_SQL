758454993d732bc640fe209427f3f8a7118e0d50
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-10-04 23:11:35 +0900

    Fix: WHERE 1=1 / 테이블 prefix / CTE 대체

--- programmers/select/서울에_위치한_식당_목록_출력하기/jiyun.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/jiyun.sql
@@ -18,3 +18,24 @@ FROM rest_info r1
         ON r1.rest_id = r2.rest_id
 WHERE address LIKE '서울%'
 ORDER BY r2.score DESC, r1.favorites DESC
+
+
+## fix 후
+# WHERE 1=1: 미사용
+# `WHERE address LIKE` - 테이블 prefix 누락 (r1.address 필요)
+# CTE는 좋은 시도지만 이 경우엔 복잡도만 증가하게 되는 것 같습니다 (개인적 생각)
+    
+SELECT 
+    r1.rest_id,
+    r1.rest_name,
+    r1.food_type,
+    r1.favorites,
+    r1.address,
+    ROUND(AVG(review_score), 2) AS score
+FROM rest_info r1
+    JOIN rest_review r2
+        ON r1.rest_id = r2.rest_id
+WHERE 1=1
+    AND r1.address LIKE '서울%'
+GROUP BY r1.rest_id
+ORDER BY 6 DESC, 4 DESC
