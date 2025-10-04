8e62d55961871c9a9b87439cbc5cc2fa5325bcf1
tux2won <114200466+tux2won@users.noreply.github.com>
2025-10-04 16:58:50 +0900

    fix: 컬럼명/테이블명 대문자, WHERE 1=1: 미사용 수정

--- programmers/select/서울에_위치한_식당_목록_출력하기/tw.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/tw.sql
@@ -2,26 +2,47 @@
 -- 1. 서울에 위치한 식당들만 필터링
 -- 2. JOIN 해서 집계 결과 출력
 
-WITH SEOUL_REST AS (
+-- - [x]  대소문자: 컬럼명/테이블명 대문자
+-- [x]  WHERE 1=1: 미사용
+-- [x]  CTE는 좋은 시도지만 이 경우엔 복잡도만 증가하게 되는 것 같습니다 (개인적 생각)
+
+WITH seoul_rest AS (
     SELECT
-        REST_ID,
-        REST_NAME,
-        FOOD_TYPE,
-        FAVORITES,
-        ADDRESS
-    FROM REST_INFO
-    WHERE ADDRESS LIKE '서울%'
+        rest_id,
+        rest_name,
+        food_type,
+        favorites,
+        address
+    FROM rest_info
+    WHERE 1=1
+        address LIKE '서울%'
 )
 
 SELECT
-    s.REST_ID,
-    s.REST_NAME,
-    s.FOOD_TYPE,
-    s.FAVORITES,
-    s.ADDRESS,
-    ROUND(AVG(r.REVIEW_SCORE), 2) AS 'SCORE'
-FROM SEOUL_REST s
-  JOIN REST_REVIEW r
-    USING(REST_ID)
-GROUP BY s.REST_ID
+    s.rest_id,
+    s.rest_name,
+    s.food_type,
+    s.favorites,
+    s.address,
+    ROUND(AVG(r.review_score), 2) AS 'SCORE'
+FROM seoul_rest s
+  JOIN rest_review r
+    USING(rest_id)
+GROUP BY s.rest_id
 ORDER BY 6 DESC, 4 DESC
+
+
+-- CTE 사용 X
+SELECT 
+    r1.rest_id,
+    r1.rest_name,
+    r1.food_type,
+    r1.favorites,
+    r1.address,
+    ROUND(AVG(r2.review_score), 2) AS score
+FROM rest_info AS r1
+    INNER JOIN rest_review AS r2
+        ON r1.rest_id = r2.rest_id
+WHERE r1.address LIKE '서울%'
+GROUP BY r1.rest_id
+ORDER BY score DESC, r1.favorites DESC
