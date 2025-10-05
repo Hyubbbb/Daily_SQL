4ff218dd7866c7d2115a3b908ffffd27fad26e87
Hyubbbb <ekwjsxl200@gmail.com>
2025-10-05 16:20:05 +0900

    Fix: WHERE 1=1 추가

--- programmers/select/서울에_위치한_식당_목록_출력하기/hyub.sql
+++ programmers/select/서울에_위치한_식당_목록_출력하기/hyub.sql
@@ -8,6 +8,7 @@ SELECT
 FROM rest_info AS ri
     JOIN rest_review AS rr
         ON ri.rest_id = rr.rest_id
-WHERE address LIKE '서울%'
+WHERE 1=1
+    AND address LIKE '서울%'
 GROUP BY ri.rest_id, ri.rest_name, ri.food_type, ri.favorites, ri.address
 ORDER BY score DESC, favorites DESC
