8339b6f21dc609d6342e36e506513eef41fc9cad
Jungmin Park <bakjeongmin021009@gmail.com>
2025-10-10 14:31:43 +0900

    Submit : 박정민

---
+++ programmers/select/대장균들의_자식의_수_구하기/jungmin.sql
@@ -0,0 +1,9 @@
+-- 대장균들의 자식의 수 구하기 (박정민)
+SELECT
+    e1.id,
+    COUNT(e2.id) AS child_count
+FROM ecoli_data AS e1
+    LEFT JOIN ecoli_data AS e2
+        ON e1.id = e2.parent_id
+GROUP BY e1.id
+ORDER BY e1.id;
