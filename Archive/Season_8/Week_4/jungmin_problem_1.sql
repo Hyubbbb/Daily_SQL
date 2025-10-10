310e397a8bee4b8599165f7463201322502df754
Jungmin Park <bakjeongmin021009@gmail.com>
2025-10-10 14:41:48 +0900

    Submin: 박정민

---
+++ Archive/Season_8/Week_4/jungmin_problem_1.sql
@@ -0,0 +1,10 @@
+-- programmers / IS NULL : Root 아이템 구하기 (Level 2)
+SELECT
+    info.item_id,
+    info.item_name
+FROM item_info AS info
+    LEFT JOIN item_tree AS tree
+        ON info.item_id = tree.item_id
+WHERE 1=1
+    AND tree.parent_item_id IS NULL
+ORDER BY info.item_id ASC;
