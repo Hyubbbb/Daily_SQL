dccd46a968f5b2bf26d45bf0fb1f4ce7229bb450
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-10-12 01:07:14 +0900

    Fix: AS 대문자 변경, WHERE 2=2 제거

--- programmers/select/오프라인_온라인_판매_데이터_통합하기/hj.sql
+++ programmers/select/오프라인_온라인_판매_데이터_통합하기/hj.sql
@@ -7,19 +7,17 @@ FROM online_sale
 WHERE 
     1=1 AND
     YEAR(sales_date) = 2022 AND
-    2=2 AND
     MONTH(sales_date) = 3
 UNION ALL
 SELECT 
     DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date, 
     product_id, 
-    NULL as user_id, 
+    NULL AS user_id, 
     sales_amount
 FROM offline_sale
 WHERE 
     1=1 AND
     YEAR(sales_date) = 2022 AND
-    2=2 AND
     MONTH(sales_date) = 3
 ORDER BY 
     sales_date ASC, 
