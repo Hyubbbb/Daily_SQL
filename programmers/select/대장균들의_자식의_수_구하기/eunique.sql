cb92766aa2f99fd44143f895aba5dca2008c7e1a
Eunji Lee <jiyomi30@gmail.com>
2025-10-12 16:51:33 +0900

    Submit: 이은지

--- programmers/select/대장균들의_자식의_수_구하기/eunique.sql
+++ programmers/select/대장균들의_자식의_수_구하기/eunique.sql
@@ -7,8 +7,8 @@ SELECT
     # ECOLI_DATA 테이블 => 자식 테이블 지정 계획 
     # CHILD_COUNT 출력 컬럼 생성 
 FROM ECOLI_DATA e1
-LEFT JOIN ECOLI_DATA e2
-    ON e1.ID = e2.PARENT_ID
+    LEFT JOIN ECOLI_DATA e2 # 'JOIN 들여쓰기'
+        ON e1.ID = e2.PARENT_ID
     # 부모 테이블의 ID 와 자식 테이블의 ID 가 같은 행끼리 연결 
     # 자식이 없으면 'NULL'로 표시
 GROUP BY e1.ID
