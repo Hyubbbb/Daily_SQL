db47eeda52202021ea84e61aacca95c663499614
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-10-13 00:43:05 +0900

    Update: table alias 수정

--- programmers/select/대장균들의_자식의_수_구하기/hj.sql
+++ programmers/select/대장균들의_자식의_수_구하기/hj.sql
@@ -1,8 +1,8 @@
 SELECT 
-    p.id AS id,
-    COUNT(c.id) AS child_count
-FROM ecoli_data AS p
-LEFT JOIN ecoli_data AS c
-    ON p.id = c.parent_id
-GROUP BY p.id
-ORDER BY p.id ASC;
+    e1.id AS id,
+    COUNT(e2.id) AS child_count
+FROM ecoli_data AS e1
+LEFT JOIN ecoli_data AS e2
+    ON e1.id = e2.parent_id
+GROUP BY e1.id
+ORDER BY e1.id ASC;
