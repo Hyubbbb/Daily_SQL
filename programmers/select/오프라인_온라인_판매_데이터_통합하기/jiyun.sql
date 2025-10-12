12dc37743e26294fa465f158a0f67d211063ee88
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-10-12 15:04:12 +0900

    Fix: NULL 컬럼 alias 명시 / UNION ALL 사용

--- programmers/select/오프라인_온라인_판매_데이터_통합하기/jiyun.sql
+++ programmers/select/오프라인_온라인_판매_데이터_통합하기/jiyun.sql
@@ -7,12 +7,12 @@ FROM online_sale
 WHERE 1=1
     AND sales_date LIKE '2022-03%'
 
-UNION
+UNION ALL
 
 SELECT
     DATE_FORMAT(sales_date, '%Y-%m-%d') AS sales_date,
     product_id,
-    NULL,
+    NULL AS user_id,
     sales_amount
 FROM offline_sale
 WHERE 1=1
