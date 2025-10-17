d6bbef891879ec9b2357a21d71c48626fd466e3a
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-10-17 17:01:01 +0900

    Fix: 테이블 alias 지정 시 AS 명시

--- programmers/select/대장균들의_자식의_수_구하기/jiyun.sql
+++ programmers/select/대장균들의_자식의_수_구하기/jiyun.sql
@@ -1,8 +1,8 @@
 SELECT 
     e1.id,
     COUNT(e2.id) AS child_count # NULL은 0으로 COUNT됨
-FROM ecoli_data e1
-    LEFT JOIN ecoli_data e2 # LEFT JOIN으로 자식이 없는 id(NULL)도 살리기
+FROM ecoli_data AS e1
+    LEFT JOIN ecoli_data AS e2 # LEFT JOIN으로 자식이 없는 id(NULL)도 살리기
         ON e1.id = e2.parent_id
 GROUP BY e1.id
 ORDER BY e1.id
