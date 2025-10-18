6d646a2f5c6f23babf840e49db70ed2e3a475886
chaewonsong <chaewon1019@ewhain.net>
2025-10-19 00:06:15 +0900

    Submit: 송채원

---
+++ programmers/select/특정_세대의_대장균_찾기/chaewon.sql
@@ -0,0 +1,11 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/301650
+SELECT 
+    e3.id
+FROM ecoli_data AS e1
+    INNER JOIN ecoli_data AS e2
+        ON e1.id = e2.parent_id
+    INNER JOIN ecoli_data AS e3
+        ON e2.id = e3.parent_id
+WHERE 1=1
+    AND e1.parent_id IS NULL
+ORDER BY e3.id ASC;
