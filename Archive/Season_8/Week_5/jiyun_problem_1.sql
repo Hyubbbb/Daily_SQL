27ffc34754db54b9855dd426fbd0032394ed2d3d
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-10-18 14:08:25 +0900

    Week5
    https://leetcode.com/problems/average-selling-price/description/?envType=study-plan-v2&envId=top-sql-50

---
+++ Archive/Season_8/Week_5/jiyun_problem_1.sql
@@ -0,0 +1,17 @@
+-- NULLIF(expression1, expression2)
+-- 동작 원리
+  -- expression1 = expression2 → NULL 반환
+  --expression1 ≠ expression2 → expression1 반환
+SELECT
+    p.product_id,
+    COALESCE(
+      ROUND(
+        SUM(p.price * COALESCE(u.units, 0))
+        / NULLIF(SUM(COALESCE(u.units, 0)), 0)
+      , 2)
+    , 0) AS average_price
+FROM prices p
+LEFT JOIN unitssold u
+  ON  p.product_id = u.product_id
+  AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
