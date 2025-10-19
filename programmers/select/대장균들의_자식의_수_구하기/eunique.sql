4de000d84f1d735403990b376651fcdd6aa9a1b1
Eunji Lee <jiyomi30@gmail.com>
2025-10-19 13:52:49 +0900

    Fix: 컬럼명/테이블명 소문자, alias 정의 시 AS 명시

--- programmers/select/대장균들의_자식의_수_구하기/eunique.sql
+++ programmers/select/대장균들의_자식의_수_구하기/eunique.sql
@@ -1,17 +1,17 @@
 SELECT 
     # 'JOIN' 한 테이블 중 특정 출력 컬럼 선택 
-    e1.ID, 
+    e1.id, 
     # ECOLI_DATA 테이블 => 부모 테이블 지정 계획
     # e1 으로 지정 => ID 컬럼 만 선택 
-    COUNT(e2.ID) AS child_count
+    COUNT(e2.id) AS child_count
     # ECOLI_DATA 테이블 => 자식 테이블 지정 계획 
     # CHILD_COUNT 출력 컬럼 생성 
 FROM ecoli_data AS e1
     LEFT JOIN ecoli_data AS e2 # 'JOIN 들여쓰기'
-        ON e1.ID = e2.parent_id
+        ON e1.id = e2.parent_id
     # 부모 테이블의 ID 와 자식 테이블의 ID 가 같은 행끼리 연결 
     # 자식이 없으면 'NULL'로 표시
-GROUP BY e1.ID
+GROUP BY e1.id
     # 집계 기준 설정 => e1.ID 단위로 묶어서 집계 
-ORDER BY e1.ID;
+ORDER BY e1.id;
     # el.ID 기준으로 오름차순 
