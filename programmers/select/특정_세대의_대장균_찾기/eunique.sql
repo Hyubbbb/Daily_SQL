f82e459b581e77b2a96dd15cab743e37d67d7861
Eunji Lee <jiyomi30@gmail.com>
2025-10-19 13:51:34 +0900

    Update: 컬럼명/테이블명 소문자

--- programmers/select/특정_세대의_대장균_찾기/eunique.sql
+++ programmers/select/특정_세대의_대장균_찾기/eunique.sql
@@ -1,9 +1,9 @@
-SELECT e3.ID
-FROM ECOLI_DATA AS e3
-JOIN ECOLI_DATA AS e2 ON e3.PARENT_ID = e2.ID
-JOIN ECOLI_DATA AS e1 ON e2.PARENT_ID = e1.ID 
+SELECT e3.id
+FROM ecoli_data AS e3
+JOIN ecoli_data AS e2 ON e3.parent_id = e2.id
+JOIN ecoli_data AS e1 ON e2.parent_id = e1.id 
 WHERE 1=1
-    AND e1.PARENT_ID IS NULL 
+    AND e1.parent_id IS NULL 
     -- Convention: allias는 개체 이름 첫글자 / where 1=1
     -- e1: 1세대 e2: 2세대 e3: 3세대 
-ORDER BY e3.ID;
+ORDER BY e3.id;
