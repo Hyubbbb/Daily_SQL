33e36304dde184728df965fc9d7dbf741bd12cdf
tux2won <114200466+tux2won@users.noreply.github.com>
2025-10-19 18:00:17 +0900

    Fix:테이블도 Alias 정의 시 AS 명시

--- programmers/select/대장균들의_자식의_수_구하기/tw.sql
+++ programmers/select/대장균들의_자식의_수_구하기/tw.sql
@@ -4,8 +4,8 @@
 SELECT
     e1.id AS id,
     COUNT(e2.id) AS child_count
-FROM ecoli_data e1
-    LEFT JOIN ecoli_data e2
+FROM ecoli_data AS e1
+    LEFT JOIN ecoli_data AS e2
         ON e1.id = e2.parent_id
 GROUP BY 1
 ORDER BY 1
