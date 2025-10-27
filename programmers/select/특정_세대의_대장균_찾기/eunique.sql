ebd0c97efc505cea6ee8ccd90171ee6d37cef7bb
Eunji Lee <jiyomi30@gmail.com>
2025-10-27 14:59:11 +0900

    Fix: JOIN/ON 들여쓰기

--- programmers/select/특정_세대의_대장균_찾기/eunique.sql
+++ programmers/select/특정_세대의_대장균_찾기/eunique.sql
@@ -1,7 +1,7 @@
 SELECT e3.id
 FROM ecoli_data AS e3
-JOIN ecoli_data AS e2 ON e3.parent_id = e2.id
-JOIN ecoli_data AS e1 ON e2.parent_id = e1.id 
+    JOIN ecoli_data AS e2 ON e3.parent_id = e2.id
+    JOIN ecoli_data AS e1 ON e2.parent_id = e1.id 
 WHERE 1=1
     AND e1.parent_id IS NULL 
     -- Convention: allias는 개체 이름 첫글자 / where 1=1
