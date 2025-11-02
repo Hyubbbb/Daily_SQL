12475916c4c0694e2e41132332110965ce2e22e3
OhJisong <jso2562@ewha.ac.kr>
2025-11-02 13:19:54 +0900

    Submit: 오지송

---
+++ Archive/Season_9/Week_1/jisong_problem_2.sql
@@ -0,0 +1,19 @@
+-- programmers (join) : https://school.programmers.co.kr/learn/courses/30/lessons/59045
+
+SELECT
+    I.ANIMAL_ID,
+    I.ANIMAL_TYPE,
+    O.NAME
+FROM
+    ANIMAL_INS  AS I
+JOIN
+    ANIMAL_OUTS AS O
+    ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE
+    I.SEX_UPON_INTAKE    LIKE 'Intact%'
+    AND (
+        O.SEX_UPON_OUTCOME LIKE 'Spayed%'
+        OR O.SEX_UPON_OUTCOME LIKE 'Neutered%'
+    )
+ORDER BY
+    I.ANIMAL_ID;
