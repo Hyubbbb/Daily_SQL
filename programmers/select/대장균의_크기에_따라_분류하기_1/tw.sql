6a4a6ec02a7fd7a307a662722f58f7748ce144a3
tux2won <114200466+tux2won@users.noreply.github.com>
2025-11-02 22:13:19 +0900

    Fix: 컬럼명(SIZE) 소문자로 변경

--- programmers/select/대장균의_크기에_따라_분류하기_1/tw.sql
+++ programmers/select/대장균의_크기에_따라_분류하기_1/tw.sql
@@ -1,9 +1,9 @@
 SELECT
     id,
     CASE 
-        WHEN size_of_colony <= 100 THEN 'LOW'
-        WHEN 1000 < size_of_colony THEN 'HIGH'
-        ELSE 'MEDIUM'
+        WHEN size_of_colony <= 100 THEN 'low'
+        WHEN 1000 < size_of_colony THEN 'high'
+        ELSE 'medium'
     END AS SIZE
 FROM ecoli_data
 ORDER BY 1
