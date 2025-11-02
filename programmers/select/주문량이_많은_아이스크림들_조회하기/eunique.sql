add172f14de05f8b3e95148a60f1e59b8608c134
Eunji Lee <jiyomi30@gmail.com>
2025-11-02 17:48:39 +0900

    Submit: 이은지

---
+++ programmers/select/주문량이_많은_아이스크림들_조회하기/eunique.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+SELECT
+   f.flavor
+FROM first_half AS f
+    JOIN (
+        SELECT flavor, SUM(total_order) AS july_total
+        FROM july
+        GROUP BY flavor
+    ) AS j
+ON f.flavor = j.flavor
+ORDER BY (f.total_order + j.july_total) DESC
+LIMIT 3;
