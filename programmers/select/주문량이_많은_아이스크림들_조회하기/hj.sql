20c8d47ccac745b61d00db73d5ba27b5f5446549
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-11-02 21:32:17 +0900

    Submit: 이형주

---
+++ programmers/select/주문량이_많은_아이스크림들_조회하기/hj.sql
@@ -0,0 +1,21 @@
+WITH total AS (
+SELECT 
+    flavor, 
+    SUM(total_order) AS total_order
+FROM first_half
+GROUP BY flavor
+
+UNION ALL
+
+SELECT 
+    flavor, 
+    SUM(total_order) AS total_order
+FROM july
+GROUP BY flavor
+)
+
+SELECT flavor
+FROM total
+GROUP BY flavor
+ORDER BY SUM(total_order) DESC
+LIMIT 3;
