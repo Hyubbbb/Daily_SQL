c5e4eb2383b96338d63b83814e201d4f52c6253b
Eunji Lee <jiyomi30@gmail.com>
2025-11-02 23:46:55 +0900

    Update: 이은지 - 고난: JOIN 하는 테이블을 SELECT 과 같은 형식으로 가져와야 하는지 헷갈림. 들여쓰기 헷갈림 - 해결: 예시문 참고. JOIN 안에 SELECT 문을 넣을수도, 테이블을 바로 가져올 수도 있음. 컨벤션 참고.

--- programmers/select/주문량이_많은_아이스크림들_조회하기/eunique.sql
+++ programmers/select/주문량이_많은_아이스크림들_조회하기/eunique.sql
@@ -2,10 +2,11 @@ SELECT
    f.flavor
 FROM first_half AS f
     JOIN (
-        SELECT flavor, SUM(total_order) AS july_total
+        SELECT 
+            flavor, SUM(total_order) AS july_total
         FROM july
         GROUP BY flavor
     ) AS j
-ON f.flavor = j.flavor
+      ON f.flavor = j.flavor
 ORDER BY (f.total_order + j.july_total) DESC
 LIMIT 3; 
