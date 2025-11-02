9632cc4b7d7e550a1b851b858636ef46691d9c56
yundavin <156299922+yundavin@users.noreply.github.com>
2025-11-02 19:22:24 +0900

    Update dabin_problem_1.sql

--- Archive/Season_9/Week_1/dabin_problem_1.sql
+++ Archive/Season_9/Week_1/dabin_problem_1.sql
@@ -1,4 +1,4 @@
-
+-- programmers : https://school.programmers.co.kr/learn/courses/30/lessons/132203
 SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
 FROM DOCTOR
 WHERE MCDP_CD='CS' OR MCDP_CD='GS'
