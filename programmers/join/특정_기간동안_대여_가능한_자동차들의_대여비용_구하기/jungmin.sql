7d12c889b598c6c93dc61d6b0ae181447c624bc7
Jungmin Park <bakjeongmin021009@gmail.com>
2025-11-05 15:58:48 +0900

    Submit: 박정민
    - 고난: 총 3개의 테이블이 존재해서 CTE를 고려할 때 어떠한 정보들을 정하고 CTE를 생성할지가 고민이었다.
    - 해결: 2개의 CTE를 생성하여 분할하여 진행하니 수월하게 쿼리 작성이 가능했던 것 같다.

---
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/jungmin.sql
@@ -0,0 +1,33 @@
+-- 특정 기간동안 대여 가능한 자동차들의 대여비용 구하기 (박정민)
+WITH already_rent AS(
+    SELECT c1.car_id
+    FROM car_rental_company_car AS c1
+        LEFT JOIN car_rental_company_rental_history AS c2
+            ON c1.car_id = c2.car_id
+    WHERE 1=1
+        AND c1.car_type IN ('세단', 'SUV')
+        AND c2.start_date <= '2022-11-30'
+        AND c2.end_date >= '2022-11-01'
+), discount AS(
+    SELECT *
+    FROM car_rental_company_discount_plan
+    WHERE 1=1
+        AND duration_type = '30일 이상'
+)
+
+SELECT
+    c.car_id,
+    c.car_type,
+    FLOOR(c.daily_fee * 30 * (1- (d.discount_rate / 100))) AS fee
+FROM car_rental_company_car AS c
+    LEFT JOIN discount AS d
+        ON c.car_type = d.car_type
+    LEFT JOIN already_rent AS a
+        ON c.car_id = a.car_id
+WHERE 1=1
+    AND c.car_type IN ('세단', 'SUV')
+    AND a.car_id IS NULL
+    AND (c.daily_fee * 30 * (1- (d.discount_rate / 100))) >= 500000
+    AND (c.daily_fee * 30 * (1- (d.discount_rate / 100))) < 2000000
+ORDER BY FEE DESC, car_id DESC;
+    
