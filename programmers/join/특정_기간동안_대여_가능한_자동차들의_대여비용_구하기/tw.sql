a052076cdb2d5c0429d98d3c3b6a0cc7ced29fe5
tux2won <114200466+tux2won@users.noreply.github.com>
2025-11-07 18:05:00 +0900

    Submit: 송태원  - 고난: START_DATE ~ END_DATE 필터링하는 부분이 좀 헷갈렸다. 처음 접근할 때 WHERE START_DATE > '2022-11-30' OR END_DATE < '2022-11-01' 처럼 접근했더니 올바르게 필터링이 안됐음  - 해결: 잘 안쓰는게 좋다고는 하지만 중첩 서브쿼리를 통해 해결했다.

---
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/tw.sql
@@ -0,0 +1,54 @@
+-- 1) 종류 = "세단" or "SUV" | 테이블1, 테이블3
+-- 2) 대여 가능 기간: 2022-11-01 ~ 2022-11-30 | 테이블2
+-- 3) 50만원 <= (30일 대여 금액) < 200만원 | 테이블1, 테이블3
+WITH condition1 AS (
+    SELECT *
+    FROM car_rental_company_car
+    WHERE 1=1
+        AND car_type = "세단"
+        OR car_type = "SUV"
+        
+), condition2 AS (
+    SELECT
+        c1.car_id,
+        c1.car_type,
+        c1.daily_fee,
+        -- c1.options,
+        c2.history_id,
+        c2.start_date,
+        c2.end_date
+    FROM condition1 c1
+        LEFT JOIN car_rental_company_rental_history c2
+            ON c1.car_id = c2.car_id
+    WHERE c1.car_id NOT IN ( 
+        SELECT car_id
+        FROM car_rental_company_rental_history
+        WHERE end_date >= '2022-11-01' AND start_date <= '2022-12-01')
+    GROUP BY c1.car_id 
+    ORDER BY 1
+), condition3 AS (
+    SELECT
+        c2.car_id,
+        c2.car_type,
+        c2.daily_fee,
+        -- c2.history_id,
+        c3.plan_id,
+        -- c3.car_type,
+        c3.duration_type,
+        c3.discount_rate,
+        (daily_fee * (1 - discount_rate / 100) * 30) AS "fee"
+    FROM condition2 c2
+        JOIN car_rental_company_discount_plan c3
+            ON c2.car_type = c3.car_type
+    WHERE 1=1
+        AND duration_type = '30일 이상'
+    ORDER BY 1, 6
+)
+
+SELECT
+    car_id,
+    car_type,
+    ROUND(fee, 0) AS 'fee'
+FROM condition3
+WHERE fee BETWEEN 500000 AND 2000000
+ORDER BY fee DESC, car_type ASC, car_id DESC
