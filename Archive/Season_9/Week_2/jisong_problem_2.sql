7fb622b26eb40cc27d5e507ef4f053a7776be76e
OhJisong <jso2562@ewha.ac.kr>
2025-11-09 17:21:05 +0900

    Submit: 오지송
    Add SQL query to select animals not in animal_ins

---
+++ Archive/Season_9/Week_2/jisong_problem_2.sql
@@ -0,0 +1,12 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT
+    ao.animal_id AS animal_id,
+    ao.name      AS name
+FROM animal_outs AS ao
+LEFT JOIN animal_ins AS ai
+    ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.animal_id IS NULL
+ORDER BY
+    ao.animal_id ASC;
