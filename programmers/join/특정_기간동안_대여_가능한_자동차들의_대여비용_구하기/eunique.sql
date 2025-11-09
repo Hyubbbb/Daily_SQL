61c9874860c94aa468aa5ea234756c7d3c3562cb
Eunji Lee <jiyomi30@gmail.com>
2025-11-09 23:38:44 +0900

    Submit: 이은지
    - 고난: 조건이 다수여서 쿼리 구조 한 번에 떠올리기 어려움
    - 해결: JOIN, WHERE을 기준으로 쿼리 블럭 단위로 생각

--- programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/eunique.sql
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/eunique.sql
@@ -4,12 +4,14 @@ SELECT
     c.car_type,
     FLOOR(c.daily_fee * 30 * (1 - d.discount_rate / 100)) AS fee
 FROM car_rental_company_car c
-JOIN car_rental_company_discount_plan d 
-    ON c.car_type = d.car_type
-    AND d.duration_type = '30일 이상'
-WHERE c.car_type IN ('세단', 'SUV')
+    JOIN car_rental_company_discount_plan d 
+        ON c.car_type = d.car_type
+        AND d.duration_type = '30일 이상'
+WHERE 1=1
+  AND c.car_type IN ('세단', 'SUV')
   AND c.car_id NOT IN (
-        SELECT h.car_id
+        SELECT 
+        h.car_id
         FROM car_rental_company_rental_history h
         WHERE NOT (h.end_Date < '2022-11-01' OR h.start_date > '2022-11-30')
     )
