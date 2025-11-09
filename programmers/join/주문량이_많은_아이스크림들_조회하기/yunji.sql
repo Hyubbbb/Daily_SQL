4386d98c890db33046760c66d108a0cf63c42c19
yunjeee <100953788+yunjeee@users.noreply.github.com>
2025-11-09 23:37:07 +0900

    Fix: #3
    - 예약어, 컬럼/테이블명 대소문자 구분 수정
    - 전체 쿼리 with문을 사용한 CTE 구조로 재구성
    - SELECT문 쿼리 줄바꿈 반영

--- programmers/join/주문량이_많은_아이스크림들_조회하기/yunji.sql
+++ programmers/join/주문량이_많은_아이스크림들_조회하기/yunji.sql
@@ -1,15 +1,22 @@
-SELECT FLAVOR
-FROM (
-    SELECT FLAVOR, SUM(TOTAL_ORDER) AS TOTAL
+# 서브쿼리 대신 with 구문으로 전면 수정
+WITH combined_orders AS (
+    SELECT 
+        flavor, 
+        SUM(total_order) AS total
     FROM (
         # 세로로 concat => 합집합 UNION 활용!
-        SELECT FLAVOR, TOTAL_ORDER FROM FIRST_HALF
+        SELECT 
+            flavor, 
+            total_order
+        FROM first_half
         UNION ALL
-        SELECT FLAVOR, TOTAL_ORDER FROM JULY
-    ) AS JAN_JUL
-    GROUP BY FLAVOR
-    ORDER BY TOTAL DESC
-    # 판다스 head() 역할과 유사
-    LIMIT 3
-    # 서브쿼리 실현 후 alias 붙여주기 필수!
-) AS RES;
+            SELECT flavor, total_order
+            FROM july
+    ) AS jan_jul
+    GROUP BY flavor
+)
+
+SELECT flavor
+FROM combined_orders
+ORDER BY total DESC
+LIMIT 3;
