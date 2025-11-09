d41278a5a825ff43307e687bbface05fcc3866f4
yundavin <156299922+yundavin@users.noreply.github.com>
2025-11-09 14:59:14 +0900

    Submit : 윤다빈
    - 고난 : inner join+group by는 flavor가 여러 행으로 존재하면 합계가 뻥튀기 된다
    - 해결 : 테이블별로 집계하고 그 다음 합쳐서 다시 집계한다

--- programmers/join/주문량이_많은_아이스크림들_조회하기/dabin.sql
+++ programmers/join/주문량이_많은_아이스크림들_조회하기/dabin.sql
@@ -1,7 +1,14 @@
 -- 주문량이 많은 아이스크림들 조회하기 (윤다빈)
-SELECT F.FLAVOR
-FROM FIRST_HALF AS F
-INNER JOIN JULY AS J ON F.FLAVOR = J.FLAVOR
-GROUP BY F.FLAVOR
-ORDER BY SUM(F.TOTAL_ORDER) + SUM(J.TOTAL_ORDER) DESC
+SELECT
+  t.flavor,
+  SUM(t.total_order) AS total_order
+FROM (
+  SELECT f.flavor, f.total_order
+  FROM first_half as f
+  UNION ALL
+  SELECT j.flavor, j.total_order
+  FROM july AS j
+) AS t
+GROUP BY t.flavor
+ORDER BY total_order DESC
 LIMIT 3;
