f4586f64d04cbdc0d333ad35b6a21271c19ea470
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-11-09 23:59:29 +0900

    Submit: 이형주
    - 고난: 두테이블 조인 및 NOT IN으로 문제 해결 시도했으나 성능 저하 문제
    - 해결: 세 테이블 조인으로 변경

---
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/hj.sql
@@ -0,0 +1,19 @@
+SELECT 
+    c1.car_id,
+    c1.car_type,
+    FLOOR(c1.daily_fee * 30 * (1 - COALESCE(c2.discount_rate, 0) / 100)) AS fee
+FROM car_rental_company_car AS c1
+JOIN car_rental_company_discount_plan AS c2
+    ON c1.car_type = c2.car_type
+   AND c2.duration_type = '30일 이상'
+LEFT JOIN car_rental_company_rental_history AS c3
+    ON c1.car_id = c3.car_id
+   AND c3.start_date <= '2022-11-30'
+   AND c3.end_date >= '2022-11-01'
+WHERE c1.car_type IN ('SUV', '세단')
+  AND c3.car_id IS NULL
+  AND (c1.daily_fee * 30 * (1 - COALESCE(c2.discount_rate, 0) / 100)) BETWEEN 500000 AND 1999999
+ORDER BY 
+    fee DESC, 
+    c1.car_type ASC, 
+    c1.car_id DESC;
