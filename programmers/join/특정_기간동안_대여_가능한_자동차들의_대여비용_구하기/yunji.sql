e5aa3bf92367af43fc5257bb7c42c63c504bab3b
yunjeee <100953788+yunjeee@users.noreply.github.com>
2025-11-09 23:10:16 +0900

    Submit: 손윤지
    - 고난: 할인율에 대해서 어떻게 정의해야 할지 정말 고민이 많았다. 제시되어 있는 일반적인 할인 정보를 통해서 바로 계산이 안되니 이를 해소할 방안을 모색했어야 했다. 또한 대여 가능 여부를 골라와야 할 때 (보통은 not null인 경우를 많이 select 했는데 ..) is null인 경우를 찾아와야 하는 걸 스스로 인지하는 것도 시간이 좀 걸렸다. 이와 함께 with 구문을 자주 사용하던 편이 아니라 컨벤션을 최대한 지켜보려고 서브쿼리 없이 작성하려고 노력을 많이 했다 .. 컨벤션 지키는 것도 처음이라 수시로 확인하는 데 어려움이 있었다.
    - 해결: 먼저 할인율 관련해서는 fee라는 별도의 임시 테이블을 만들어 별도의 계산을 중복해서 요하지 않도록 깔끔하게 처리하고자 했다. with 구문의 경우 인터넷 + ai의 도움을 좀 받았다.. with 구문을 쉬운 문제로 몇 번 연습을 따로 해보긴 했지만, 긴 쿼리문을 짜는데 있어서는 아직 원활하지 않았다. 컨벤션 규칙도 노션에 작성되어 있는 풀 규칙과 모범 코드를 최대한 참고하며 지키려고 해보았다 ..!!

---
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/yunji.sql
@@ -0,0 +1,29 @@
+WITH available_cars AS (
+    SELECT c.car_id, c.car_type,
+        # 대여 가능 차량의 요금들을 미리 계산
+        FLOOR(c.daily_fee *30 * (1 - dp.discount_rate / 100)) AS fee
+    
+    FROM car_rental_company_car AS c
+        # 대여기간(30일)에 대한 정보 있는 것들만 조인
+        INNER JOIN car_rental_company_discount_plan AS dp
+            ON dp.car_type = c.car_type
+            AND dp.duration_type = '30일 이상'
+    
+        LEFT JOIN car_rental_company_rental_history AS h
+            ON h.car_id = c.car_id
+            # 대여 가능한 날짜들 조건 걸기
+                AND h.end_date >= '2022-11-01'
+                AND h.start_date <= '2022-11-30'
+    
+    WHERE 1=1
+        AND c.car_type IN ('세단','SUV')
+        AND h.history_id IS NULL # 대여기록 없는 차들 대여하기 위함
+)
+
+# with 구문에서 계산된 결과로 최종 금액 범위 구하기 
+SELECT ac.car_id, ac.car_type, ac.fee
+FROM available_cars AS ac
+WHERE 1=1
+    AND ac.fee >= 500000 # 50만원 이상일 경우
+    AND ac.fee < 2000000 # 200만원 이하일 경우
+ORDER BY ac.fee DESC, ac.car_type ASC, ac.car_id DESC;
