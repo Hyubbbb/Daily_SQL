83aee897585273ef62f0a0ecb64d679c3d2b8a1c
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-11-09 20:30:23 +0900

    Submit: 이지윤
    - 고난: c2.end_date < '2022-11-01' OR c2.start_date > '2022-11-30' 조건으로 시도했을 때, 하나의 대여 이력이라도 대여 가능 범위에 포함되면 대여 불가능 차량으로 제외되지 않았음
    - 해결: 대여 가능 범위를 조건으로 하는 대신, 대여 불가능 차량의 id를 추출하여 NOT EXISTS한 조건을 걸어 대여 불가능 차량을 완전히 제외

---
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/jiyun.sql
@@ -0,0 +1,37 @@
+-- 자동차 종류: 세단, SUV
+-- 2022.11.01~2022.11.30 대여 가능할 것
+-- 30일 간의 대여 금액 50만 이상 200만 미만
+
+WITH can_rental AS (
+    SELECT
+        c1.car_id,
+        c1.car_type,     
+        ROUND(c1.daily_fee * 30 * (1 - c2.discount_rate/100)) AS fee
+    FROM car_rental_company_car AS c1
+        LEFT JOIN car_rental_company_discount_plan AS c2
+            ON c1.car_type = c2.car_type
+    WHERE 1=1
+        AND c1.car_type IN ('세단', 'SUV')
+        AND NOT EXISTS (
+            SELECT 1
+            FROM car_rental_company_rental_history AS c3
+            WHERE c1.car_id = c3.car_id
+              AND (c3.start_date <= '2022-11-30' AND c3.end_date >= '2022-11-01')
+        )
+        AND c2.duration_type = '30일 이상'
+    GROUP BY c1.car_id
+)
+
+
+SELECT
+   car_id,
+   car_type,
+   fee
+FROM can_rental
+WHERE 1=1
+    AND fee >= 500000
+    AND fee < 2000000
+ORDER BY 
+    fee DESC,
+    car_type ASC,
+    car_id DESC
