4b8b0c2367e39d198f59fd302d444618f170db99
yundavin <156299922+yundavin@users.noreply.github.com>
2025-11-09 17:09:14 +0900

    Fix: #3

--- programmers/join/주문량이_많은_아이스크림들_조회하기/dabin.sql
+++ programmers/join/주문량이_많은_아이스크림들_조회하기/dabin.sql
@@ -1,14 +1,22 @@
 -- 주문량이 많은 아이스크림들 조회하기 (윤다빈)
-SELECT
-  t.flavor,
-  SUM(t.total_order) AS total_order
-FROM (
-  SELECT f.flavor, f.total_order
-  FROM first_half as f
+WITH orders AS (
+  SELECT
+    f.flavor,
+    f.total_order
+  FROM first_half AS f
   UNION ALL
-  SELECT j.flavor, j.total_order
+  SELECT
+    j.flavor,
+    j.total_order
   FROM july AS j
-) AS t
-GROUP BY t.flavor
-ORDER BY total_order DESC
+)
+SELECT
+  o.flavor,
+  SUM(o.total_order) AS total_order_sum
+FROM orders AS o
+WHERE 1=1  
+GROUP BY
+  o.flavor
+ORDER BY
+  total_order_sum DESC
 LIMIT 3;
