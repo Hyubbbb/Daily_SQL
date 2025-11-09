8e2381623d66d99693e7ca7c84e32f4396bfb6ef
OhJisong <jso2562@ewha.ac.kr>
2025-11-09 16:58:06 +0900

    fix: 오지송
    콤마(,) 컨벤션 수정

--- programmers/join/주문량이_많은_아이스크림들_조회하기/jisong.sql
+++ programmers/join/주문량이_많은_아이스크림들_조회하기/jisong.sql
@@ -1,15 +1,15 @@
 WITH merged_orders AS (
     SELECT
-        f.flavor
-      , SUM(f.total_order) AS total_order
+        f.flavor ,
+        SUM(f.total_order) AS total_order
     FROM first_half AS f
     GROUP BY f.flavor
 
     UNION ALL
 
     SELECT
-        j.flavor
-      , SUM(j.total_order) AS total_order
+        j.flavor ,
+        SUM(j.total_order) AS total_order
     FROM july AS j
     GROUP BY j.flavor
 )
@@ -19,6 +19,7 @@ FROM merged_orders AS m
 GROUP BY
     m.flavor
 ORDER BY
-    SUM(m.total_order) DESC
-  , m.flavor ASC
+    SUM(m.total_order) DESC ,
+    m.flavor ASC
 LIMIT 3;
+
