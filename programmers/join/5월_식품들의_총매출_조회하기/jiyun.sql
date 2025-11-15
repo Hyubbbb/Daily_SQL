9d60f6374ab27ef17a3a5fe8de113a8d0838415b
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-11-15 19:41:20 +0900

    Submit: 이지윤
    - 고난: 처음에 LEFT JOIN을 썼다가 order 테이블에 있는 제품 모두가 product 테이블에 있는 것이 아님을 확인
    - 해결: JOIN을 써서 해결!

---
+++ programmers/join/5월_식품들의_총매출_조회하기/jiyun.sql
@@ -0,0 +1,11 @@
+SELECT 
+    f1.product_id,
+    f2.product_name,
+    SUM(f2.price * f1.amount) AS total_sales
+FROM food_order f1
+    JOIN food_product f2
+        ON f1.product_id = f2.product_id
+WHERE 1=1
+    AND f1.produce_date LIKE '2022-05%'
+GROUP BY f1.product_id
+ORDER BY total_sales DESC, f1.product_id
