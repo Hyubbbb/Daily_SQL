80691c18ef977b1dac95214a2bff501be9d2e7f6
tux2won <114200466+tux2won@users.noreply.github.com>
2025-11-15 23:08:54 +0900

    Submit: 송태원

---
+++ programmers/join/5월_식품들의_총매출_조회하기/tw.sql
@@ -0,0 +1,12 @@
+
+SELECT
+    fp.product_id,
+    fp.product_name,
+    SUM(fo.amount * fp.price) AS totoal_sales
+FROM FOOD_ORDER AS fo
+    JOIN FOOD_PRODUCT AS fp USING(product_id)
+WHERE 1=1
+    AND YEAR(fo.produce_date) = 2022
+    AND MONTH(fo.produce_date) = 05
+GROUP BY fp.product_id, fp.product_name
+ORDER BY totoal_sales DESC, fp.product_id ASC;
