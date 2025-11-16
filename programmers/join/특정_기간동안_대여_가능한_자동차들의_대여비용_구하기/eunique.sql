386f03ce4ccd2c4315ad6b380475cd1526b5d048
Eunji Lee <jiyomi30@gmail.com>
2025-11-16 23:46:27 +0900

    Style #4

--- programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/eunique.sql
+++ programmers/join/특정_기간동안_대여_가능한_자동차들의_대여비용_구하기/eunique.sql
@@ -1,22 +1,34 @@
 -- 코드를 입력하세요-- 
+WITH can_rental AS (
+    SELECT
+        c1.car_id,
+        c1.car_type,     
+        ROUND(c1.daily_fee * 30 * (1 - c2.discount_rate/100)) AS fee
+    FROM car_rental_company_car AS c1
+        LEFT JOIN car_rental_company_discount_plan AS c2
+            ON c1.car_type = c2.car_type
+    WHERE 1=1
+        AND c1.car_type IN ('세단', 'SUV')
+        AND NOT EXISTS (
+            SELECT 1
+            FROM car_rental_company_rental_history AS c3
+            WHERE c1.car_id = c3.car_id
+              AND (c3.start_date <= '2022-11-30' AND c3.end_date >= '2022-11-01')
+        )
+        AND c2.duration_type = '30일 이상'
+    GROUP BY c1.car_id
+)
+
+
 SELECT
-    c.car_id,
-    c.car_type,
-    FLOOR(c.daily_fee * 30 * (1 - d.discount_rate / 100)) AS fee
-FROM car_rental_company_car c
-    JOIN car_rental_company_discount_plan d 
-        ON c.car_type = d.car_type
-        AND d.duration_type = '30일 이상'
+   car_id,
+   car_type,
+   fee
+FROM can_rental
 WHERE 1=1
-  AND c.car_type IN ('세단', 'SUV')
-  AND c.car_id NOT IN (
-        SELECT 
-        h.car_id
-        FROM car_rental_company_rental_history h
-        WHERE NOT (h.end_Date < '2022-11-01' OR h.start_date > '2022-11-30')
-    )
-  AND FLOOR(c.daily_fee * 30 * (1 - d.discount_rate / 100)) BETWEEN 500000 AND 1999999
-ORDER BY
+    AND fee >= 500000
+    AND fee < 2000000
+ORDER BY 
     fee DESC,
-    c.car_type ASC,
-    c.car_id DESC;
+    car_type ASC,
+    car_id DESC
