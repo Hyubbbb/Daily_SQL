e24b20c3f5342976ad705a4b932ca3e0862b2f22
Eunji Lee <jiyomi30@gmail.com>
2025-11-16 23:58:38 +0900

    Update: 이은지
    - 고난: WHERE 조건문 사용
    - 해결: 이 전 WHERE 문 및 타 쿼리 참고

--- programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
@@ -1,12 +1,11 @@
--- 코드를 입력하세요
-SELECT 
-    fp.product_id as product_id,
-    fp.product_name as product_name,
+SELECT
+    fp.product_id AS product_id,
+    fp.product_name AS product_name,
     SUM(fp.price * fo.amount) AS total_sales
 FROM food_product fp
-    JOIN food_order fo
-        ON fp.product_id = fo.product_id
-WHERE 1=1 
-    AND to_char(fo.produce_date, 'YYYY-MM') = '2022-05'
+JOIN food_order fo
+    ON fp.product_id = fo.product_id
+WHERE fo.produce_date >= '2022-05-01'
+  AND fo.produce_date <  '2022-06-01'
 GROUP BY fp.product_id, fp.product_name
 ORDER BY total_sales DESC, product_id ASC;
