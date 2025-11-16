aa0b4d6f8096b7dad69668462881cfeca772c120
Eunji Lee <jiyomi30@gmail.com>
2025-11-16 23:59:37 +0900

    Update: 이은지
    - 고난: WHERE 문 내용 작성
    - 해결: 기존 쿼리 참고

--- programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
@@ -3,9 +3,10 @@ SELECT
     fp.product_name AS product_name,
     SUM(fp.price * fo.amount) AS total_sales
 FROM food_product fp
-JOIN food_order fo
-    ON fp.product_id = fo.product_id
-WHERE fo.produce_date >= '2022-05-01'
+    JOIN food_order fo
+        ON fp.product_id = fo.product_id
+WHERE 1=1 
+  AND fo.produce_date >= '2022-05-01'
   AND fo.produce_date <  '2022-06-01'
 GROUP BY fp.product_id, fp.product_name
 ORDER BY total_sales DESC, product_id ASC;
