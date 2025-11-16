17c3e308b3c7ce7f5610d8290177495ffa20f578
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-11-16 20:25:57 +0900

    Submit: 이형주

---
+++ programmers/join/5월_식품들의_총매출_조회하기/hj.sql
@@ -0,0 +1,13 @@
+SELECT 
+    f1.product_id, 
+    f2.product_name,
+    SUM(f2.price * f1.amount) AS total_sales
+FROM food_order f1
+  JOIN food_product f2
+    ON f1.product_id = f2.product_id
+WHERE 1=1 
+  AND f1.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY f1.product_id, f2.product_name
+ORDER BY 
+    3 DESC, 
+    1 ASC;
