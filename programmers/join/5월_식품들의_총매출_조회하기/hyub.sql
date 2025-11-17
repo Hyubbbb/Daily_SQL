62a10a3914bdd6bed21c6033c29fb2cc6cb77f78
Hyubbbb <hyub@fnfcorp.com>
2025-11-17 18:52:03 +0900

    Submit: 김협
    - 고난: `amount` 컬럼을 확인하지 못하고, `SUM(price)` 를 쿼리해서 오답이 나왔다
    - 해결: 주어진 문제와 테이블을 다시 읽고, 개선 후 정답 처리됨 !

---
+++ programmers/join/5월_식품들의_총매출_조회하기/hyub.sql
@@ -0,0 +1,11 @@
+SELECT
+    fp.product_id AS product_id,
+    fp.product_name AS product_name,
+    SUM(fp.price * fo.amount) AS total_sales
+FROM food_order AS fo
+    LEFT JOIN food_product AS fp
+        ON fo.product_id = fp.product_id
+WHERE 1=1
+    AND fo.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY fp.product_id, fp.product_name
+ORDER BY total_sales DESC, fp.product_id
