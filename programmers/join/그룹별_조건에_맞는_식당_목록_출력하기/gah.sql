dc56cfe5ee187f818489ce1ae3dd07dba0155a50
gah508 <109083740+gah508@users.noreply.github.com>
2025-11-19 15:49:35 +0900

    Submit: 안가현
    - 고난: 최다 리뷰어가 여러명일 경우를 고려한 CTE
    - 해결: [멤버벌 리뷰 카운트 -> 최다 리뷰를 작성한 멤버] 구조로 구성

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/gah.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/gah.sql
@@ -1,11 +1,11 @@
-WITH review_cnt AS (  -- 회원별 리뷰 개수
+WITH review_cnt AS (   -- 회원별 리뷰 개수
     SELECT
         member_id,
         COUNT(*) AS cnt
     FROM rest_review
     GROUP BY member_id
 ),
-max_reviewer AS (  -- 리뷰를 가장 많이 작성한 회원
+max_reviewer AS (   -- 리뷰를 가장 많이 작성한 회원
     SELECT
         member_id
     FROM review_cnt
@@ -20,11 +20,11 @@ SELECT
     p.member_name,
     r.review_text,
     DATE_FORMAT(r.review_date, '%Y-%m-%d') AS review_date
-FROM max_reviewer AS m  -- (1) 제일 많이 작성한 리뷰어
+FROM max_reviewer AS m   -- (1) 제일 많이 작성한 리뷰어
     JOIN rest_review AS r
-        ON m.member_id = r.member_id  -- (2) 위 리뷰어의 리뷰
+        ON m.member_id = r.member_id   -- (2) 위 리뷰어의 리뷰
     JOIN member_profile AS p
-        ON m.member_id = p.member_id  -- (3) 위 리뷰어의 프로필
+        ON m.member_id = p.member_id   -- (3) 위 리뷰어의 프로필
 ORDER BY
     review_date ASC,
     r.review_text ASC;
