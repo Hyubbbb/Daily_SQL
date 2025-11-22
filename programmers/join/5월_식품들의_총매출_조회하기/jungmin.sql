c672f5161f073668c84922e4e651191776ee829d
Jungmin Park <bakjeongmin021009@gmail.com>
2025-11-22 16:29:18 +0900

    Style: #5

--- programmers/join/5월_식품들의_총매출_조회하기/jungmin.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/jungmin.sql
@@ -1,18 +1,31 @@
 -- 5월 식품들의 총매출 조회하기 (박정민)
-WITH total_amount AS(
-    SELECT 
-        product_id,
-        SUM(amount) AS total_amount
-    FROM food_order
-    WHERE 1=1
-        AND produce_date BETWEEN '2022-05-01' AND '2022-05-31'
-    GROUP BY product_id
-) 
 SELECT
-    f.product_id,
-    f.product_name,
-    f.price * t.total_amount AS total_sales
-FROM food_product AS f
-    JOIN total_amount AS t
-        ON f.product_id = t.product_id
-ORDER BY total_sales DESC, f.product_id ASC;
+    f1.product_id,
+    f1.product_name,
+    SUM(f1.price * f2.amount) AS total_price
+FROM food_product AS f1
+    JOIN food_order AS f2
+        ON f1.product_id = f2.product_id
+WHERE 1=1
+    AND f2.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY f1.product_id
+ORDER BY total_price DESC, f1.product_id ASC;
+        
+-- WITH total_amount AS(
+--     SELECT 
+--         product_id,
+--         SUM(amount) AS total_amount
+--     FROM food_order
+--     WHERE 1=1
+--         AND produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+--     GROUP BY product_id
+-- ) 
+-- SELECT
+--     f.product_id,
+--     f.product_name,
+--     f.price * t.total_amount AS total_sales
+-- FROM food_product AS f
+--     JOIN total_amount AS t
+--         ON f.product_id = t.product_id
+-- ORDER BY total_sales DESC, f.product_id ASC;
+
