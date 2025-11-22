26c4085181f8aff573d7954be17a6f01e9731f80
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2025-11-22 21:46:12 +0900

    Style: #5

--- programmers/join/5월_식품들의_총매출_조회하기/boo.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/boo.sql
@@ -1,13 +1,13 @@
 SELECT
     f1.product_id,
-    product_name,
+    f1.product_name,
     SUM(price*amount) AS total_sales
 FROM food_product AS f1
     JOIN food_order AS f2
         ON f1.product_id = f2.product_id
 WHERE 1=1
-    AND DATE_FORMAT(produce_date, '%Y-%m') = '2022-05'
-GROUP BY f1.product_id
+    AND f2.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY f1.product_id, f1.product_name
 ORDER BY
     total_sales DESC,
-    f1.product_id;
+    f1.product_id ASC;
