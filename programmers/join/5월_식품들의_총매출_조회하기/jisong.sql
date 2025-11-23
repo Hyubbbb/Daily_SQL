67c9877610b8d4b62db7bdb23aea60365eeed871
OhJisong <jso2562@ewha.ac.kr>
2025-11-24 00:26:12 +0900

    Style: #5

--- programmers/join/5월_식품들의_총매출_조회하기/jisong.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/jisong.sql
@@ -1,15 +1,15 @@
 SELECT
-    fp.product_id   AS product_id,
-    fp.product_name AS product_name,
-    SUM(fo.amount * fp.price) AS total_sales
-FROM food_product AS fp
-    JOIN food_order AS fo
-        ON fo.product_id = fp.product_id
+    fp1.product_id   AS product_id,
+    fp1.product_name AS product_name,
+    SUM(fo1.amount * fp1.price) AS total_sales
+FROM food_product AS fp1
+    JOIN food_order AS fo1
+        ON fo1.product_id = fp1.product_id
 WHERE 1=1
-    AND fo.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+    AND fo1.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
 GROUP BY
-    fp.product_id,
-    fp.product_name
+    fp1.product_id,
+    fp1.product_name
 ORDER BY
     total_sales DESC,
-    fp.product_id ASC;
+    fp1.product_id ASC;
