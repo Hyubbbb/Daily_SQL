537325885eb2c95580a0c91da84025c1b870834e
Eunji Lee <jiyomi30@gmail.com>
2025-11-23 23:36:05 +0900

    Fix: 이은지 #5

--- programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/eunique.sql
@@ -1,12 +1,11 @@
 SELECT
-    fp.product_id AS product_id,
-    fp.product_name AS product_name,
-    SUM(fp.price * fo.amount) AS total_sales
-FROM food_product fp
-    JOIN food_order fo
-        ON fp.product_id = fo.product_id
+    f1.product_id AS product_id,
+    f2.product_name AS product_name,
+    SUM(f1.price * f2.amount) AS total_sales
+FROM food_product AS f1
+    JOIN food_order AS f2
+        ON f1.product_id = f2.product_id
 WHERE 1=1 
-  AND fo.produce_date >= '2022-05-01'
-  AND fo.produce_date <  '2022-06-01'
-GROUP BY fp.product_id, fp.product_name
+  AND f1.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY f1.product_id, f2.product_name
 ORDER BY total_sales DESC, product_id ASC;
