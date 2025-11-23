618abbfc3dd0f3c43fce7f72ef3b1807c0da6983
yunjeee <100953788+yunjeee@users.noreply.github.com>
2025-11-23 23:43:40 +0900

    Style: #5
    - alias 에서 as 누락된 것 추가
    - CTE 구조 없이 쿼리 재정리
    - GROUP BY에 누락된 컬럼들 추가

--- programmers/join/5월_식품들의_총매출_조회하기/yunji.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/yunji.sql
@@ -1,19 +1,16 @@
-WITH order_2022_05 AS (
-    SELECT product_id,
-        amount, 
-        produce_date
-    FROM food_order
-    WHERE produce_date 
-        BETWEEN '2022-05-01' 
-        AND '2022-05-31'
-)
-
-SELECT p.product_id,
-    p.product_name,
-    SUM(p.price * o.amount) AS total_sales    
-FROM food_product p
-JOIN order_2022_05 o
+SELECT
+    p.product_id AS product_id,
+    p.product_name AS product_name,
+    SUM(p.price * o.amount) AS total_sales
+FROM food_product AS p
+JOIN food_order AS o
     ON p.product_id = o.product_id
-GROUP BY p.product_name
-ORDER BY total_sales DESC,
-    p.product_id
+WHERE o.produce_date 
+    BETWEEN '2022-05-01' 
+    AND '2022-05-31'
+GROUP BY
+    p.product_id,
+    p.product_name
+ORDER BY
+    total_sales DESC,
+    p.product_id;
