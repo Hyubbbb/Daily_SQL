1341be9cb8fb0a30706f33193982d3c114c485ba
yunjeee <100953788+yunjeee@users.noreply.github.com>
2025-11-23 23:09:50 +0900

    Submit: 손윤지
    - 고난: 최대 리뷰 개수를 가진 멤버를 어떻게 뽑아서 해당 멤버의 리뷰들만 조회할지, max(count)의 정보를 어떻게 저장해놓고 다른 테이블과 조인해야 할지 고민이 많았음.
    - 해결: 서브쿼리를 최소화 하기 위해 해당 정보를 저장하기 위한 테이블을 여럿 생성하여 해결 .. 좋은 방법인진 모르겠다.

---
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/yunji.sql
@@ -0,0 +1,30 @@
+WITH review_cnt AS (
+    SELECT
+        member_id,
+        COUNT(*) AS cnt
+    FROM rest_review
+    GROUP BY member_id
+),
+max_cnt_num AS (
+    SELECT MAX(cnt) AS max_cnt
+    FROM review_cnt 
+),
+top_member AS (
+    SELECT member_id
+    FROM review_cnt rc
+    JOIN max_cnt_num mc
+        ON rc.cnt = mc.max_cnt
+)
+
+SELECT 
+    mp.member_name,
+    rr.review_text,
+    DATE_FORMAT(rr.review_date, '%Y-%m-%d') AS review_date
+FROM member_profile mp
+JOIN rest_review rr
+    ON mp.member_id = rr.member_id
+JOIN top_member tm
+    ON rr.member_id = tm.member_id
+ORDER BY
+    rr.review_date,
+    rr.review_text
