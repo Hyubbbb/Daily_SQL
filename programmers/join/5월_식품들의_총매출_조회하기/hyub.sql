b64b1947bed34991c1870199fd5131780ba8a4c5
Hyubbbb <ekwjsxl200@gmail.com>
2025-11-23 16:31:05 +0900

    Style: #5
    - LEFT JOIN을 JOIN으로 사용해야 하지 않을까에 대해서도 리뷰 주셨는데, 이 부분은 조금 더 고민해보면 좋을 것 같습니다 ! (food_order 테이블에 기록되고 있는 product_id 값 중 food_product 테이블에서 관리되지 못하고 있는 정보가 있진 않은지 점검될 수 있는 쿼리가 필요하지 않을까 생각했습니다 !)

--- programmers/join/5월_식품들의_총매출_조회하기/hyub.sql
+++ programmers/join/5월_식품들의_총매출_조회하기/hyub.sql
@@ -1,11 +1,11 @@
 SELECT
-    fp.product_id AS product_id,
-    fp.product_name AS product_name,
-    SUM(fp.price * fo.amount) AS total_sales
-FROM food_order AS fo
-    LEFT JOIN food_product AS fp
-        ON fo.product_id = fp.product_id
+    f1.product_id AS product_id,
+    f1.product_name AS product_name,
+    SUM(f1.price * f2.amount) AS total_sales
+FROM food_order AS f2
+    LEFT JOIN food_product AS f1
+        ON f2.product_id = f1.product_id
 WHERE 1=1
-    AND fo.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
-GROUP BY fp.product_id, fp.product_name
-ORDER BY total_sales DESC, fp.product_id
+    AND f2.produce_date BETWEEN '2022-05-01' AND '2022-05-31'
+GROUP BY f1.product_id, f1.product_name
+ORDER BY total_sales DESC, f1.product_id
