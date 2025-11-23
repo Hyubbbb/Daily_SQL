50500f29b7510423dffc7520321a1c185f56559d
yunjeee <100953788+yunjeee@users.noreply.github.com>
2025-11-23 23:46:26 +0900

    Update: alias AS 추가

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/yunji.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/yunji.sql
@@ -11,8 +11,8 @@ max_cnt_num AS (
 ),
 top_member AS (
     SELECT member_id
-    FROM review_cnt rc
-    JOIN max_cnt_num mc
+    FROM review_cnt AS rc
+    JOIN max_cnt_num AS mc
         ON rc.cnt = mc.max_cnt
 )
 
@@ -20,10 +20,10 @@ SELECT
     mp.member_name,
     rr.review_text,
     DATE_FORMAT(rr.review_date, '%Y-%m-%d') AS review_date
-FROM member_profile mp
-JOIN rest_review rr
+FROM member_profile AS mp
+JOIN rest_review AS rr
     ON mp.member_id = rr.member_id
-JOIN top_member tm
+JOIN top_member AS tm
     ON rr.member_id = tm.member_id
 ORDER BY
     rr.review_date,
