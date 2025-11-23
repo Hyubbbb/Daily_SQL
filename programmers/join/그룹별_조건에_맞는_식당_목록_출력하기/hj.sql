c92346c1cfe5610ba707a18511099e6aad657e52
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2025-11-23 17:18:04 +0900

    Submit: 이형주
    - 고난: 가장 많은 리뷰를 찾을때, 개수를 내림차순 정렬 후 첫번째 행을 선택하려 했으나 최대 리뷰 수가 같을 경우 문제 발생  
    - 해결: rank를 활용하여 동일한 리뷰 수가 나올 경우도 처리 가능

---
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/hj.sql
@@ -0,0 +1,21 @@
+WITH cnt_review AS (
+    SELECT
+        member_id,
+        RANK() OVER(ORDER BY COUNT(*) DESC) AS rnk
+    FROM rest_review
+    GROUP BY member_id
+)
+
+SELECT 
+    m.member_name, 
+    r.review_text,
+    DATE_FORMAT(r.review_date, '%Y-%m-%d') AS review_date
+FROM rest_review AS r
+    JOIN cnt_review AS c
+        ON r.member_id = c.member_id
+    JOIN member_profile AS m
+        ON r.member_id = m.member_id
+WHERE c.rnk = 1
+ORDER BY 
+    3, 
+    1;
