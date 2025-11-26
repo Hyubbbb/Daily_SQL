93f2ac5a9e0723478fde8de4e3676477a55ed0a2
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-11-26 15:29:15 +0900

    Submit: 이지윤
    - 고난: 큰 어려움 없이 해결
    - 해결: -

---
+++ programmers/join/보호소에서_중성화한_동물/jiyun.sql
@@ -0,0 +1,11 @@
+SELECT
+    ai.animal_id,
+    ai.animal_type,
+    ai.name
+FROM animal_ins AS ai
+    JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.sex_upon_intake LIKE 'Intact%'
+    AND ao.sex_upon_outcome NOT LIKE 'Intact%'
+ORDER BY ai.animal_id
