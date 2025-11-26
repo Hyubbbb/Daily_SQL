588a53aa4e97c692532d2b12184cef3a7592b0a9
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2025-11-26 15:39:18 +0900

    Style: #6
    Alias 일관성 있게 지정 (테이블, 집계함수)

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/jiyun.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/jiyun.sql
@@ -15,14 +15,14 @@ most_reviewer AS (
 )
 
 SELECT
-    m2.member_name,
-    r.review_text,
-    DATE_FORMAT(r.review_date, '%Y-%m-%d') AS review_date
-FROM rest_review AS r
-    JOIN most_reviewer m1 
-        ON r.member_id = m1.member_id
-    JOIN member_profile AS m2
-        ON m1.member_id = m2.member_id
+    mp.member_name,
+    rr.review_text,
+    DATE_FORMAT(rr.review_date, '%Y-%m-%d') AS review_date
+FROM rest_review AS rr
+    JOIN most_reviewer mr 
+        ON rr.member_id = mr.member_id
+    JOIN member_profile AS mp
+        ON mr.member_id = mp.member_id
 WHERE 1=1
     AND rn = 1 -- 리뷰 수 내림차순 기준 rank가 1인 회원
-ORDER BY r.review_date, r.review_text
+ORDER BY rr.review_date, rr.review_text
