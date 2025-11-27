bf184ec3309121439cdfc153b6ca0197fbbfb42c
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2025-11-27 15:40:45 +0900

    Style: #6

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/boo.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/boo.sql
@@ -30,18 +30,18 @@ WITH rnk_review AS (
 max_member AS (
     SELECT member_id
     FROM rnk_review
-    WHERE rnk=1
+    WHERE 1=1
+        AND rnk=1
 )
 
 SELECT
     member_name,
     review_text,
     DATE_FORMAT(review_date, '%Y-%m-%d') AS review_date
-FROM rest_review AS r
-    JOIN max_member AS m1
-        ON r.member_id = m1.member_id
-    JOIN member_profile AS m2
-        ON m2.member_id = r.member_id
+FROM rest_review AS rr
+    JOIN max_member AS mm
+        ON rr.member_id = mm.member_id
+    JOIN member_profile AS mp
+        ON mp.member_id = rr.member_id
 ORDER BY 
-    review_date,
-    review_text;
+    rr.review_date, rr.review_text;
