4924543b82811e6e28509550882db084430f1ca7
tux2won <114200466+tux2won@users.noreply.github.com>
2025-11-28 00:23:50 +0900

    Submit: 송태원

---
+++ programmers/join/보호소에서_중성화한_동물/tw.sql
@@ -0,0 +1,13 @@
+-- 코드를 입력하세요
+SELECT 
+    ai.animal_id,
+    ai.animal_type,
+    ai.name
+FROM animal_ins ai
+    JOIN animal_outs ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.sex_upon_intake LIKE 'Intact%'
+    AND ao.sex_upon_outccome NOT LIKE 'Intact%'
+ORDER BY 
+    ai.animal_id ASC;
