625ced7c9ad39bb34b365df863eb8366b410f153
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2025-11-30 02:19:08 +0900

    Style: #6
    FROM - JOIN - ON 들여쓰기 수정

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/yunji.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/yunji.sql
@@ -12,8 +12,8 @@ max_cnt_num AS (
 top_member AS (
     SELECT member_id
     FROM review_cnt AS rc
-    JOIN max_cnt_num AS mc
-        ON rc.cnt = mc.max_cnt
+        JOIN max_cnt_num AS mc
+            ON rc.cnt = mc.max_cnt
 )
 
 SELECT 
@@ -21,10 +21,10 @@ SELECT
     rr.review_text,
     DATE_FORMAT(rr.review_date, '%Y-%m-%d') AS review_date
 FROM member_profile AS mp
-JOIN rest_review AS rr
-    ON mp.member_id = rr.member_id
-JOIN top_member AS tm
-    ON rr.member_id = tm.member_id
+    JOIN rest_review AS rr
+        ON mp.member_id = rr.member_id
+    JOIN top_member AS tm
+        ON rr.member_id = tm.member_id
 ORDER BY
     rr.review_date,
     rr.review_text
