707867edd813f19b6fda2b267ab433fad1c9da10
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2025-11-30 02:41:30 +0900

    Submit: 손윤지
    - 고난: CTE를 써야 할지 말지 고민, 컨벤션이 틀린 곳은 없는지 검토를 여러번 진행하였다.
    - 해결: CTE 없이도 간단한 쿼리로 작성 가능했고, 1=1 하는 걸 빼먹을 뻔했지만 다행히 넣어주었다.

---
+++ programmers/join/보호소에서_중성화한_동물/yunji.sql
@@ -0,0 +1,10 @@
+SELECT
+    o.animal_id,
+    o.animal_type,
+    o.name
+FROM animal_outs AS o
+    JOIN animal_ins AS i
+        ON o.animal_id = i.animal_id
+WHERE 1=1
+    AND o.sex_upon_outcome NOT LIKE 'Intact%'
+    AND i.sex_upon_intake LIKE 'Intact%'
