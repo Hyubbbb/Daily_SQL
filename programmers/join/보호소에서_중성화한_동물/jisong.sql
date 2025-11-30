77798ec6fa31dcfceb44035b28908dcfd1e0bfab
OhJisong <jso2562@ewha.ac.kr>
2025-11-30 22:58:24 +0900

    Submit: 오지송
    Submit: 오지송
    - 고난: Intact/Spayed/Neutered 조건을 WHERE절에 어떻게 나눠서 걸어야 할지, AND/OR 우선순위 때문에 원하는 동물만 안 나오는 문제가 있었음
    
    - 해결: ANIMAL_INS는 입소 시점, ANIMAL_OUTS는 퇴소 시점 컬럼이라는 걸 다시 정리하고 Intact 조건과 Spayed/Neutered 조건을 괄호로 분리해서 AND/OR 조건을 명확히 하여 원하는 결과만 조회되도록 수정함

---
+++ programmers/join/보호소에서_중성화한_동물/jisong.sql
@@ -0,0 +1,17 @@
+SELECT
+    I.ANIMAL_ID,
+    I.ANIMAL_TYPE,
+    O.NAME
+FROM
+    ANIMAL_INS  AS I
+JOIN
+    ANIMAL_OUTS AS O
+    ON O.ANIMAL_ID = I.ANIMAL_ID
+WHERE
+    I.SEX_UPON_INTAKE    LIKE 'Intact%'
+    AND (
+        O.SEX_UPON_OUTCOME LIKE 'Spayed%'
+        OR O.SEX_UPON_OUTCOME LIKE 'Neutered%'
+    )
+ORDER BY
+    I.ANIMAL_ID;
