6bb310a60f4cb8f18b79cfd5ab0756e0d0c69a02
OhJisong <jso2562@ewha.ac.kr>
2025-11-30 22:50:59 +0900

    fix: #6

--- programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/jisong.sql
+++ programmers/join/그룹별_조건에_맞는_식당_목록_출력하기/jisong.sql
@@ -1,19 +1,28 @@
+WITH review_count AS (
+    SELECT
+        member_id,
+        COUNT(*) AS review_cnt
+    FROM rest_review
+    GROUP BY
+        member_id
+),
+max_count AS (
+    SELECT
+        MAX(review_cnt) AS max_review_cnt
+    FROM review_count
+)
 SELECT
-    mp.member_name       AS member_name,
-    rr.review_text       AS review_text,
-    TO_CHAR(rr.review_date, 'YYYY-MM-DD') AS review_date
+    mp.member_name                         AS member_name,
+    rr.review_text                         AS review_text,
+    TO_CHAR(rr.review_date, 'YYYY-MM-DD')  AS review_date
 FROM member_profile AS mp
 JOIN rest_review AS rr
     ON rr.member_id = mp.member_id
+JOIN review_count AS rc
+    ON rc.member_id = mp.member_id
+JOIN max_count AS mc
+    ON rc.review_cnt = mc.max_review_cnt
 WHERE 1=1
-    AND rr.member_id = (
-        SELECT
-            member_id
-        FROM rest_review
-        GROUP BY member_id
-        ORDER BY COUNT(*) DESC
-        FETCH FIRST 1 ROWS ONLY
-    )
 ORDER BY
     rr.review_date ASC,
     rr.review_text ASC;
