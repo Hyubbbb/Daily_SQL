d80a6441b6e35f1d5195c85fbcf7818d38712d2f
Eunji Lee <jiyomi30@gmail.com>
2025-11-30 09:49:28 +0900

    Submit: 이은지

---
+++ programmers/join/보호소에서_중성화한_동물/eunique.sql
@@ -0,0 +1,12 @@
+SELECT
+    ai.animal_id,
+    ai.animal_type,
+    ai.name
+FROM animal_ins AS ai
+    JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.sex_upon_intake LIKE 'Intact%'
+    AND (ao.sex_upon_outcome LIKE 'Spayed%' 
+    OR ao.sex_upon_outcome LIKE 'Neutered%')
+ORDER BY ai.animal_id;
