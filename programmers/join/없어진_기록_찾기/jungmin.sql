f17f6739eb3c717893571ce4fbb8403f2d0d6d53
Jungmin Park <bakjeongmin021009@gmail.com>
2025-12-02 09:56:49 +0900

    Submit: 박정민
    - 고난: 큰 어려움 없이 변화한 쿼리 인벤션을 적용함.
    - 해결: -

---
+++ programmers/join/없어진_기록_찾기/jungmin.sql
@@ -0,0 +1,10 @@
+-- 없어진 기록 찾기 (박정민)
+SELECT
+    ao.animal_id,
+    ao.name
+FROM animal_outs AS ao
+    LEFT JOIN animal_ins AS ai
+        ON ao.animal_id = ai.animal_id
+WHERE 1=1
+    AND ai.animal_id IS NULL
+ORDER BY ao.animal_id;
