a5304029a4cefc4c9b607d196c77a1dc3c434ff0
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2025-12-04 11:42:42 +0900

    Style: #6

--- programmers/join/보호소에서_중성화한_동물/yunji.sql
+++ programmers/join/보호소에서_중성화한_동물/yunji.sql
@@ -1,11 +1,11 @@
 SELECT
-    o.animal_id,
-    o.animal_type,
-    o.name
-FROM animal_outs AS o
-    JOIN animal_ins AS i
-        ON o.animal_id = i.animal_id
+    ao.animal_id,
+    ao.animal_type,
+    ao.name
+FROM animal_outs AS ao
+    JOIN animal_ins AS ai
+        ON ao.animal_id = ai.animal_id
 WHERE 1=1
-    AND o.sex_upon_outcome NOT LIKE 'Intact%'
-    AND i.sex_upon_intake LIKE 'Intact%'
-ORDER BY o.animal_id
+    AND ao.sex_upon_outcome NOT LIKE 'Intact%'
+    AND ai.sex_upon_intake LIKE 'Intact%'
+ORDER BY ao.animal_id
