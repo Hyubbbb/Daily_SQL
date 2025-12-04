cb2b5fc78a6aceba234305def38cc6bfb2c05ac9
yundabin <156299922+yundavin@users.noreply.github.com>
2025-12-04 18:58:25 +0900

    Style: #7

--- programmers/join/보호소에서_중성화한_동물/dabin.sql
+++ programmers/join/보호소에서_중성화한_동물/dabin.sql
@@ -1,12 +1,11 @@
 -- 보호소에서 중성화한 동물 (윤다빈)
 SELECT
-    a.animal_id,
-    a.animal_type,
-    a.name
-FROM animal_ins AS a
-    JOIN animal_outs AS b
-        ON a.animal_id = b.animal_id
-WHERE 1 = 1
-    AND a.sex_upon_intake LIKE 'Intact%'
-    AND b.sex_upon_outcome NOT LIKE 'Intact%'
-ORDER BY a.animal_id;
+    ai.animal_id   AS animal_id,
+    ai.animal_type AS animal_type,
+    ai.name        AS name
+FROM animal_ins AS ai
+JOIN animal_outs AS ao
+    ON ai.animal_id = ao.animal_id
+WHERE ai.sex_upon_intake LIKE 'Intact%'
+  AND ao.sex_upon_outcome NOT LIKE 'Intact%'
+ORDER BY ai.animal_id;
