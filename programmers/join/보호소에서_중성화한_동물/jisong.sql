5de50e56581cb23472079a6ccd7706b4812559ca
OhJisong <jso2562@ewha.ac.kr>
2025-12-07 21:08:42 +0900

    Style: #7

--- programmers/join/보호소에서_중성화한_동물/jisong.sql
+++ programmers/join/보호소에서_중성화한_동물/jisong.sql
@@ -1,11 +1,14 @@
-SELECT
-    ai.animal_id,
-    ai.animal_type,
-    ai.name
-FROM animal_ins AS ai
-    JOIN animal_outs AS ao
-        ON ai.animal_id = ao.animal_id
-            AND ai.sex_upon_intake != ao.sex_upon_outcome -- WHERE 절에서 Intact만 남긴 후, 이 조건을 통해 필터링 가능
-WHERE 1=1
-    AND ai.sex_upon_intake IN ('Intact Male', 'Intact Female') -- 보호소 입소 당시, Intact인 개체만 남김
-ORDER BY animal_id
+SELECT 
+    I.ANIMAL_ID, 
+    I.ANIMAL_TYPE, 
+    I.NAME
+FROM ANIMAL_INS AS I
+    JOIN ANIMAL_OUTS AS O
+        ON I.ANIMAL_ID = O.ANIMAL_ID
+    
+    WHERE I.SEX_UPON_INTAKE LIKE 'Intact%'
+
+    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%'
+    OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
+
+ORDER BY I.ANIMAL_ID
