faef91e98ba44cc4e6012cd7ddf089f722ed0be0
Eunji Lee <jiyomi30@gmail.com>
2025-12-07 23:13:58 +0900

    Style #7

--- programmers/join/보호소에서_중성화한_동물/eunique.sql
+++ programmers/join/보호소에서_중성화한_동물/eunique.sql
@@ -7,6 +7,6 @@ FROM animal_ins AS ai
         ON ai.animal_id = ao.animal_id
 WHERE 1=1
     AND ai.sex_upon_intake LIKE 'Intact%'
-    AND (ao.sex_upon_outcome LIKE 'Spayed%' 
-    OR ao.sex_upon_outcome LIKE 'Neutered%')
+        AND (ao.sex_upon_outcome LIKE 'Spayed%' 
+        OR ao.sex_upon_outcome LIKE 'Neutered%')
 ORDER BY ai.animal_id;
