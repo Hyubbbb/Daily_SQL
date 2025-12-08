37e144c58e317b82b2e4b9342eae0202475ba486
OhJisong <jso2562@ewha.ac.kr>
2025-12-08 20:01:54 +0900

    Style: #7

--- programmers/join/보호소에서_중성화한_동물/jisong.sql
+++ programmers/join/보호소에서_중성화한_동물/jisong.sql
@@ -1,14 +1,15 @@
-SELECT 
-    I.ANIMAL_ID, 
-    I.ANIMAL_TYPE, 
-    I.NAME
-FROM ANIMAL_INS AS I
-    JOIN ANIMAL_OUTS AS O
-        ON I.ANIMAL_ID = O.ANIMAL_ID
-    
-    WHERE I.SEX_UPON_INTAKE LIKE 'Intact%'
-
-    AND (O.SEX_UPON_OUTCOME LIKE 'Spayed%'
-    OR O.SEX_UPON_OUTCOME LIKE 'Neutered%')
-
-ORDER BY I.ANIMAL_ID
+SELECT
+    i.animal_id      AS animal_id,
+    i.animal_type    AS animal_type,
+    o.name           AS name
+FROM animal_ins AS i
+    JOIN animal_outs AS o
+        ON o.animal_id = i.animal_id
+WHERE 1=1
+    AND i.sex_upon_intake LIKE 'Intact%'
+    AND (
+        o.sex_upon_outcome LIKE 'Spayed%'
+        OR o.sex_upon_outcome LIKE 'Neutered%'
+    )
+ORDER BY
+    i.animal_id ASC;
