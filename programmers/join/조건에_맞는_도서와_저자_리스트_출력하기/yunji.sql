2a18fa0f9d2a7b435778d6f9d9400258d7ec326c
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-01-08 01:04:52 +0900

    Submit: 손윤지
    - 고난: date_format 양식이 안 맞아서 초반에 헤메었다.
    - 해결: 인터넷에서 규칙 찾아서 해결하였다!

---
+++ programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/yunji.sql
@@ -0,0 +1,9 @@
+SELECT
+    b.book_id,
+    a.author_name,
+    DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
+FROM book AS b
+JOIN author AS a
+    ON b.author_id = a.author_id
+WHERE category = '경제'
+ORDER BY published_date
