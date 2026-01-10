0375381d7bca3e3b56736f034fe42319514bc5cd
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-01-10 18:14:34 +0900

    Submit: 이예진
    - 고난 및 역경: 데이트 포맷 학습 부족으로 출력 형식에서 오류 발생
    - 해결: 변환 방법 학습 후 해결

---
+++ programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/yehjin.sql
@@ -0,0 +1,8 @@
+SELECT b.book_id,
+       a.author_name,
+       DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
+FROM book AS b
+    JOIN author AS a
+        ON b.author_id = a.author_id
+WHERE b.category = '경제'
+ORDER BY b.published_date
