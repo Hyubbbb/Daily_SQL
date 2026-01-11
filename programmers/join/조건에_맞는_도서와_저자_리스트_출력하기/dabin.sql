c1209cc684357a27469fba8c43513ebbffeecc6e
yundabin <156299922+yundavin@users.noreply.github.com>
2026-01-11 16:55:33 +0900

    Submit: 윤다빈

---
+++ programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/dabin.sql
@@ -0,0 +1,11 @@
+-- 조건에 맞는 도서와 저자 리스트 출력하기(윤다빈)
+SELECT
+  b.book_id,
+  a.author_name,
+  DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
+FROM book AS b
+  INNER JOIN author AS a
+    ON b.author_id = a.author_id
+WHERE 1=1
+  AND b.category = '경제'
+ORDER BY published_date;
