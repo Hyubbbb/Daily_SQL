e3f0acdee8110109c8f3854dfb151c6bfd112bf9
gah508 <109083740+gah508@users.noreply.github.com>
2026-01-11 21:12:51 +0900

    Submit: 안가현
    - 고난: 없음!
    - 해결: 없음!

---
+++ programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/gah.sql
@@ -0,0 +1,10 @@
+SELECT
+    b.book_id,
+    a.author_name,
+    DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
+FROM book AS b
+    JOIN author AS a
+        ON b.author_id = a.author_id
+WHERE 1=1
+    AND b.category = '경제'
+ORDER BY published_date;
