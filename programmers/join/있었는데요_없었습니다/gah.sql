fb59e014b82d841979b688c043a6cd0b6c9a712a
gah508 <109083740+gah508@users.noreply.github.com>
2026-01-13 21:08:15 +0900

    Submit: 안가현

---
+++ programmers/join/있었는데요_없었습니다/gah.sql
@@ -0,0 +1,8 @@
+SELECT
+    ai.animal_id,
+    ai.name
+FROM animal_ins AS ai
+    JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE ai.datetime > ao.datetime
+ORDER BY ai.datetime;
