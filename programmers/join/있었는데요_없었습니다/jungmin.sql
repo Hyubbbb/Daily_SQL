47fb7dad34f24f2c4952692dac06c00b1100a160
Jungmin Park <bakjeongmin021009@gmail.com>
2026-01-15 14:18:32 +0900

    Submit: 박정민
    - 고난: 단순히 datetime에 존재하는 시간값들도 부등호가 되는지 정확히 알지 못했다.
    - 해결: 단순히 실행을 해보니 되는 것을 확인했고, 자세히 알기 위해 제미나이한테 물어봤다.

---
+++ programmers/join/있었는데요_없었습니다/jungmin.sql
@@ -0,0 +1,9 @@
+SELECT
+    ai.animal_id,
+    ai.name
+FROM animal_ins AS ai
+    JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.datetime > ao.datetime
+ORDER BY ai.datetime;
