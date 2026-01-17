bcce47830ff0bfa888c8815ecfa827e340f4a5f5
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-01-17 11:00:02 +0900

    Style: #9

--- programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/yunji.sql
+++ programmers/join/조건에_맞는_도서와_저자_리스트_출력하기/yunji.sql
@@ -3,7 +3,8 @@ SELECT
     a.author_name,
     DATE_FORMAT(b.published_date, '%Y-%m-%d') AS published_date
 FROM book AS b
-JOIN author AS a
-    ON b.author_id = a.author_id
-WHERE category = '경제'
+    JOIN author AS a
+        ON b.author_id = a.author_id
+WHERE 1=1
+    AND category = '경제'
 ORDER BY published_date
