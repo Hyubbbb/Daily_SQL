02cd6355c79ac488647f470e0b1d114023c49a78
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-01-18 21:53:52 +0900

    Submit: 이예진

--- Archive/Season_10/Week_2/yehjin_problem_1.sql
+++ Archive/Season_10/Week_2/yehjin_problem_1.sql
@@ -1,7 +1,7 @@
 #https://school.programmers.co.kr/learn/courses/30/lessons/59414
 SELECT
-ai.animal_id,
-ai.name,
-DATE_FORMAT(ai.datetime, '%Y-%m-%d') AS 날짜
+  ai.animal_id,
+  ai.name,
+  DATE_FORMAT(ai.datetime, '%Y-%m-%d') AS 날짜
 FROM animal_ins AS ai
 ORDER BY animal_id
