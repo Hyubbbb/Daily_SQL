d98497e5662048e2c4cd88e48762c1d5953702dc
yundabin <156299922+yundavin@users.noreply.github.com>
2026-01-18 16:01:55 +0900

    Update: 윤다빈

--- Archive/Season_10/Week_2/dabin_problem_1.sql
+++ Archive/Season_10/Week_2/dabin_problem_1.sql
@@ -1,12 +1,15 @@
 -- 3월에 태어난 여성 회원 목록 출력하기(윤다빈)
 SELECT
-    member_id,
-    member_name,
-    gender,
-    DATE_FORMAT(STR_TO_DATE(date_of_birth, '%Y-%m-%d'), '%Y-%m-%d') AS date_of_birth
-FROM member_profile
-WHERE
-    gender = 'W'
-    AND SUBSTR(date_of_birth, 6, 2) = '03'
-    AND tlno IS NOT NULL
-ORDER BY member_id ASC;
+    mp.member_id,
+    mp.member_name,
+    mp.gender,
+    DATE_FORMAT(
+        STR_TO_DATE(mp.date_of_birth, '%Y-%m-%d'),
+        '%Y-%m-%d'
+    ) AS date_of_birth
+FROM member_profile AS mp
+WHERE 1=1
+    AND mp.gender = 'W'
+    AND SUBSTR(mp.date_of_birth, 6, 2) = '03'
+    AND mp.tlno IS NOT NULL
+ORDER BY mp.member_id ASC;
