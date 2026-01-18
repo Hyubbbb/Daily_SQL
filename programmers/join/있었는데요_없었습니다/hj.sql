940ac8fb412bbd771cdbe013730f112fb5f291e3
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2026-01-18 22:17:17 +0900

    Submit: 이형주

---
+++ programmers/join/있었는데요_없었습니다/hj.sql
@@ -0,0 +1,7 @@
+SELECT ai.animal_id, ai.name
+FROM animal_ins AS ai
+    LEFT JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ai.datetime > ao.datetime
+ORDER BY ai.datetime;
