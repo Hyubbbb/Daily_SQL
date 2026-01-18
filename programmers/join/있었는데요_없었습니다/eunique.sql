cbcf9dce1942bf3e1a78273b41b95385cf5537e7
Eunji Lee <jiyomi30@gmail.com>
2026-01-18 16:36:29 +0900

    Submit: 이은지
    고난: DATETIME을 함수 처리해야 하는지 헷갈림
    해결: 컬럼에 있으므로 컬럼명 불러오듯이 처리

--- programmers/join/있었는데요_없었습니다/eunique.sql
+++ programmers/join/있었는데요_없었습니다/eunique.sql
@@ -1,9 +1,9 @@
 -- 코드를 입력하세요
 SELECT
-    i.animal_id,
-    i.name
-FROM animal_ins i
-    JOIN animal_outs o
-        ON i.animal_id = o.animal_id
-WHERE o.datetime < i.datetime
-ORDER BY i.datetime;
+    ai.animal_id,
+    ai.name
+FROM animal_ins AS ai
+    JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE ao.datetime < ai.datetime
+ORDER BY ai.datetime;
