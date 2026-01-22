078f54c793f95b31325ba8d2a81d33d0637f5fc4
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-01-22 19:11:42 +0900

    Style: #10

--- programmers/join/있었는데요_없었습니다/boo.sql
+++ programmers/join/있었는데요_없었습니다/boo.sql
@@ -2,7 +2,7 @@ SELECT
     ai.animal_id,
     ai.name
 FROM animal_ins AS ai
-    JOIN animal_outs as ao
+    JOIN animal_outs AS ao
         ON ai.animal_id = ao.animal_id
 WHERE 1=1
     AND ai.datetime > ao.datetime
