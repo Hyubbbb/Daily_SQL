71c49d79ed0921f3effd1de42cc403016f88f0cd
celinalab <pogang505799@ewhain.net>
2026-01-22 21:30:46 +0900

    Style: #10

--- programmers/join/있었는데요_없었습니다/hyunjin.sql
+++ programmers/join/있었는데요_없었습니다/hyunjin.sql
@@ -1,7 +1,7 @@
 SELECT ai.animal_id,
        ai.name
-FROM animal_ins as ai
-  JOIN animal_outs as ao
+FROM animal_ins AS ai
+  JOIN animal_outs AS ao
     ON ai.animal_id = ao.animal_id
 WHERE 1=1
   AND ai.datetime > ao.datetime
