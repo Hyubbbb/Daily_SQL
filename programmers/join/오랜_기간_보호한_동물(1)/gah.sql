aec6ceca89eafbcb87d39f0d829f75644b174746
gah508 <109083740+gah508@users.noreply.github.com>
2026-01-25 22:01:43 +0900

    Submit: 안가현

---
+++ programmers/join/오랜_기간_보호한_동물(1)/gah.sql
@@ -0,0 +1,10 @@
+SELECT
+    ai.name,
+    ai.datetime
+FROM animal_ins AS ai
+    LEFT JOIN animal_outs AS ao
+        ON ai.animal_id = ao.animal_id
+WHERE 1=1
+    AND ao.animal_id IS NULL
+ORDER BY ai.datetime
+LIMIT 3;
