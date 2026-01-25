531ec61bb23b0d94b49c6127a57db650bf4a03eb
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2026-01-25 23:32:56 +0900

    Style: #10

--- programmers/join/있었는데요_없었습니다/hj.sql
+++ programmers/join/있었는데요_없었습니다/hj.sql
@@ -2,7 +2,7 @@ SELECT
     ai.animal_id, 
     ai.name
 FROM animal_ins AS ai
-    LEFT JOIN animal_outs AS ao
+    JOIN animal_outs AS ao
         ON ai.animal_id = ao.animal_id
 WHERE 1=1
     AND ai.datetime > ao.datetime
