e3d63dbf1c21a2a06dcf0a09dc6f562b7cf85e87
Hyubbbb <hyub@fnfcorp.com>
2026-01-28 18:21:25 +0900

    Submit: 김협
    - 고난: user_info 테이블 내의 user_id는 PK라서 중복이 없음을 순간 잊어 먹음
    - 해결: PK라는 점을 감안해서 불필요한 DISTINCT 사용을 줄였음

---
+++ programmers/join/상품을_구매한_회원_비율_구하기/hyub.sql
@@ -0,0 +1,16 @@
+WITH members_2021 AS (
+    SELECT user_id
+    FROM user_info
+    WHERE 1=1
+        AND YEAR(joined) = '2021'
+)   
+SELECT
+    YEAR(sales_date) AS year,
+    MONTH(sales_date) AS month,
+    COUNT(DISTINCT user_id) AS purchased_users,
+    ROUND(COUNT(DISTINCT user_id) / (SELECT COUNT(user_id) FROM members_2021), 1) AS purchased_ratio
+FROM online_sale
+WHERE 1=1
+    AND user_id IN (SELECT user_id FROM members_2021)
+GROUP BY YEAR(sales_date), MONTH(sales_date)
+ORDER BY year, month
