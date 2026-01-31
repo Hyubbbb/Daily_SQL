55e35ef7a5fd5566ad6ed11c32384d60722ba73f
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-01-31 17:02:54 +0900

    Submit: 이지윤
    - 고난: DISTINCT와 서브쿼리 사용을 지양하려다보니 더 복잡해지는 문제
    - 해결: DISTINCT는 사용하되, WITH 문을 통해 가독성 확보

---
+++ programmers/join/상품을_구매한_회원_비율_구하기/jiyun.sql
@@ -0,0 +1,18 @@
+WITH join_2021 AS ( -- 2021에 가입한 회원
+    SELECT
+        user_id
+    FROM user_info
+    WHERE 1=1
+        AND joined LIKE '2021%'
+)
+
+SELECT
+    YEAR(os.sales_date) AS year,
+    MONTH(os.sales_date) AS month,
+    COUNT(DISTINCT j.user_id) AS purchased_users,
+    ROUND(COUNT(DISTINCT j.user_id) / (SELECT COUNT(user_id) FROM join_2021) , 1) AS puchased_ratio
+FROM join_2021 AS j
+    JOIN online_sale AS os
+        ON j.user_id = os.user_id
+GROUP BY year, month
+ORDER BY year, month
