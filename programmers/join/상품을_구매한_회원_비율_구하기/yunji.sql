2b4958bfac965a477980d5d672ead51185b572a4
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-02-01 23:59:20 +0900

    Submit: 손윤지
    - 고난: 월별 집계 시 2021년 가입 회원 수를 고정하기 어려웠음. 또한 조인 2번 진행해야 하는 발상을 떠올리지 못했음
    - 해결: WITH CTE로 2021년 가입 회원과 전체 회원 수를 분리해서 계산하고, 조인 2번 하는 경우도 있음을 알게 되어 적용함

---
+++ programmers/join/상품을_구매한_회원_비율_구하기/yunji.sql
@@ -0,0 +1,28 @@
+WITH join_2021 AS (
+    SELECT user_id
+    FROM user_info
+    WHERE 1=1
+        AND YEAR(joined) = 2021
+),
+total_2021 AS (
+    SELECT
+        COUNT(*) AS total_users
+    FROM join_2021
+)
+
+SELECT
+    YEAR(os.sales_date)  AS year, 
+    MONTH(os.sales_date) AS month, 
+    COUNT(DISTINCT os.user_id) AS purchased_users, 
+    ROUND(COUNT(DISTINCT os.user_id) / t.total_users, 1) AS purchased_ratio
+FROM online_sale AS os
+  JOIN join_2021 AS j
+      ON os.user_id = j.user_id
+  CROSS JOIN total_2021 AS t
+GROUP BY
+    year, 
+    month, 
+    t.total_users
+ORDER BY
+    year ASC, 
+    month ASC;
