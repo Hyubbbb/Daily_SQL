271f4f7a43f1e35410f516306c231af4fd254b40
gah508 <109083740+gah508@users.noreply.github.com>
2026-02-02 00:18:35 +0900

    Update: 함수 수정
    - 고난: 한 달에 여러 번 구매해도 한 명으로 계산해야 한다는 점
    - 해결: DISTINCT 사용으로 중복 구매 제거

--- programmers/join/상품을_구매한_회원_비율_구하기/gah.sql
+++ programmers/join/상품을_구매한_회원_비율_구하기/gah.sql
@@ -2,8 +2,9 @@ WITH joined_2021 AS (
     SELECT
         user_id
     FROM user_info
-    WHERE joined >= '2021-01-01'
-      AND joined <  '2022-01-01'
+    WHERE 1=1
+        AND joined >= '2021-01-01'
+        AND joined < '2022-01-01'
 ),
 
 total_joined AS (
@@ -14,19 +15,19 @@ total_joined AS (
 
 monthly_purchasers AS (
     SELECT
-        DATE_FORMAT(os.sales_date, '%Y-%m') AS ym,
+        DATE_FORMAT(os.sales_date, '%Y-%m-01') AS ym,
         COUNT(DISTINCT os.user_id) AS purchased_users
     FROM online_sale AS os
-        JOIN joined_2021 AS j
+        JOIN joined_2021 j
             ON j.user_id = os.user_id
-    GROUP BY DATE_FORMAT(os.sales_date, '%Y-%m')
+    GROUP BY DATE_FORMAT(os.sales_date, '%Y-%m-01')
 )
-
+    
 SELECT
     YEAR(mp.ym) AS year,
     MONTH(mp.ym) AS month,
     mp.purchased_users,
-    ROUND(mp.purchased_users / NULLIF(tj.total_users, 0), 1) AS purchased_ratio
+    ROUND(mp.purchased_users / tj.total_users, 1) AS purchased_ratio
 FROM monthly_purchasers AS mp
-    CROSS JOIN total_joined tj
+    CROSS JOIN total_joined AS tj
 ORDER BY year, month;
