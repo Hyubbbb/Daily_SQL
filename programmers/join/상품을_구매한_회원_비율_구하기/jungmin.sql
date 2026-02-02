73c5bf0620c4b490e77b71a437ed4c08100c5f85
Jungmin Park <bakjeongmin021009@gmail.com>
2026-02-02 14:46:45 +0900

    Fix: #12

--- programmers/join/상품을_구매한_회원_비율_구하기/jungmin.sql
+++ programmers/join/상품을_구매한_회원_비율_구하기/jungmin.sql
@@ -6,15 +6,15 @@ WITH cnt AS (
 )
 
 SELECT
-    DATE_FORMAT(o.sales_date, '%Y') AS year,
-    DATE_FORMAT(o.sales_date, '%m') AS month,
-    COUNT(DISTINCT o.user_id) AS purchased_users,
-    ROUND(COUNT(DISTINCT o.user_id) / c.cnt, 1) AS purchased_ratio
-FROM user_info AS u
-    JOIN online_sale AS o
-        ON u.user_id = o.user_id
+    YEAR(os.sales_date) AS year,
+    MONTH(os.sales_date) AS month,
+    COUNT(DISTINCT os.user_id) AS purchased_users,
+    ROUND(COUNT(DISTINCT os.user_id) / c.cnt, 1) AS purchased_ratio
+FROM user_info AS ui
+    JOIN online_sale AS os
+        ON ui.user_id = os.user_id
     CROSS JOIN cnt AS c
 WHERE 1=1
-    AND u.joined BETWEEN '2021-01-01' AND '2021-12-31'
+    AND ui.joined BETWEEN '2021-01-01' AND '2021-12-31'
 GROUP BY year, month
-ORDER BY year, month;
+ORDER BY year, month
