380879355b4c616d58875b55efce838507774e23
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-02-02 20:33:33 +0900

    Style: #12

--- programmers/join/상품을_구매한_회원_비율_구하기/boo.sql
+++ programmers/join/상품을_구매한_회원_비율_구하기/boo.sql
@@ -1,20 +1,27 @@
--- 2021년에 가입한 회원
+-- 2021년에 가입한 회원 목록
 WITH join_2021 AS (
     SELECT
         user_id
     FROM user_info
     WHERE 1=1
         AND YEAR(joined) = 2021
+),
+-- 2021년에 가입한 회원 수
+cnt_2021 AS (
+    SELECT
+        COUNT(user_id) AS cnt
+    FROM join_2021
 )
 
 SELECT
     YEAR(o.sales_date) AS year,
     MONTH(o.sales_date) AS month,
     COUNT(DISTINCT o.user_id) AS purchased_users,
-    ROUND(COUNT(DISTINCT o.user_id) / (SELECT COUNT(*) FROM join_2021), 1) AS puchased_ratio
+    ROUND(COUNT(DISTINCT o.user_id) / c.cnt, 1) AS puchased_ratio
 FROM join_2021 AS j
     JOIN online_sale AS o
         ON j.user_id = o.user_id
+    CROSS JOIN cnt_2021 AS c
 GROUP BY
     YEAR(o.sales_date), 
     MONTH(o.sales_date)
