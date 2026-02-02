273d9751c6ef4293968225919df8e79a12fb1a37
yundabin <156299922+yundavin@users.noreply.github.com>
2026-02-02 13:26:07 +0900

    Update: CTE로 수정

--- programmers/join/상품을_구매한_회원_비율_구하기/dabin.sql
+++ programmers/join/상품을_구매한_회원_비율_구하기/dabin.sql
@@ -1,17 +1,21 @@
 -- 상품을 구매한 회원 비율 구하기
+WITH base AS (
+    SELECT COUNT(*) AS total_2021_users
+    FROM user_info
+    WHERE YEAR(joined) = 2021
+)
 SELECT
-    YEAR(os.sales_date) AS YEAR,
-    MONTH(os.sales_date) AS MONTH,
-    COUNT(DISTINCT ui.user_id) AS PURCHASED_USERS,
+    YEAR(os.sales_date)  AS year,
+    MONTH(os.sales_date) AS month,
+    COUNT(DISTINCT ui.user_id) AS purchased_users,
     ROUND(
-        COUNT(DISTINCT ui.user_id) /
-        (SELECT COUNT(*) 
-         FROM user_info
-         WHERE YEAR(joined) = 2021),
-        1) AS PUCHASED_RATIO
+        COUNT(DISTINCT ui.user_id) / base.total_2021_users,
+        1
+    ) AS purchased_ratio
 FROM online_sale AS os
-INNER JOIN user_info AS ui
-    ON ui.user_id = os.user_id
-WHERE YEAR(ui.JOINED) = 2021
-GROUP BY YEAR, MONTH
-ORDER BY YEAR, MONTH;
+JOIN user_info AS ui
+  ON ui.user_id = os.user_id
+CROSS JOIN base
+WHERE YEAR(ui.joined) = 2021
+GROUP BY YEAR(os.sales_date), MONTH(os.sales_date), base.total_2021_users
+ORDER BY year, month;
