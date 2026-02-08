2fac51cf46e741852a02c84db6765c9f45d0c917
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-02-08 19:22:22 +0900

    Submit: 이예진
    고난: 문제를 제대로 보지않아 '상품코드'를 product_id로 착각함.
    해결: 문제와 테이블 구조, 예시를 다시 살펴보고 해결함.

---
+++ programmers/join/상품_별_오프라인_매출_구하기/yehjin.sql
@@ -0,0 +1,8 @@
+SELECT
+p.product_code,
+SUM(p.price*os.sales_amount) AS sales
+FROM offline_sale AS os
+    JOIN product AS p
+        ON os.product_id=p.product_id
+GROUP BY p.product_code
+ORDER BY sales DESC, p.product_code
