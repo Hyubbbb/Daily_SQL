cd0e832ac733087c733fb6e6d32e2d7c79fb8dd7
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2026-02-08 22:22:12 +0900

    Style: #12

--- programmers/join/상품을_구매한_회원_비율_구하기/hj.sql
+++ programmers/join/상품을_구매한_회원_비율_구하기/hj.sql
@@ -4,15 +4,21 @@ WITH user_2021 AS (
     FROM user_info
     WHERE 1=1
         AND YEAR(joined) = 2021
+), 
+cnt_2021 AS (
+    SELECT 
+        COUNT(user_id) AS cnt
+    FROM user_2021
 )
-
+    
 SELECT
     YEAR(os.sales_date) AS year,
     MONTH(os.sales_date) AS month,
     COUNT(DISTINCT u.user_id) AS purchased_users,
-    ROUND(COUNT(DISTINCT u.user_id) / (SELECT COUNT(user_id) FROM user_2021) , 1) AS puchased_ratio
+    ROUND(COUNT(DISTINCT u.user_id) / c.cnt , 1) AS puchased_ratio
 FROM user_2021 AS u
     JOIN online_sale AS os
         ON u.user_id = os.user_id
+    CROSS JOIN cnt_2021 AS c
 GROUP BY year, month
 ORDER BY year, month;
