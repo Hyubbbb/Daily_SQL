d280df430d7e82aa5fecacc929f04894e2b02d74
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-02-08 23:16:57 +0900

    Submit: 손윤지
    - 고난: 없음 !
    - 해결: 없음 !

---
+++ programmers/join/상품_별_오프라인_매출_구하기/yunji.sql
@@ -0,0 +1,9 @@
+SELECT 
+    p.product_code,
+    SUM(os.sales_amount) * p.price AS sales
+FROM product AS p
+    JOIN offline_sale AS os
+        ON p.product_id = os.product_id
+ORDER BY 
+    sales DESC,
+    product_id;
