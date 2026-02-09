385f75627481245cb1710151ddf519cb31d9b788
yundabin <156299922+yundavin@users.noreply.github.com>
2026-02-09 13:10:12 +0900

    Update: JOIN 들여쓰기

--- programmers/join/상품_별_오프라인_매출_구하기/dabin.sql
+++ programmers/join/상품_별_오프라인_매출_구하기/dabin.sql
@@ -3,7 +3,7 @@ SELECT
     p.product_code,
     SUM(p.price*os.sales_amount) AS sales
 FROM product AS p
-INNER JOIN offline_sale AS os
-    ON p.product_id = os.product_id
+    INNER JOIN offline_sale AS os
+        ON p.product_id = os.product_id
 GROUP BY p.product_code
 ORDER BY sales DESC, product_code;
