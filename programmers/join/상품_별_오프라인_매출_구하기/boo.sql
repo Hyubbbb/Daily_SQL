2e40e9915457b1a3592299a4f632d52c2a212b4e
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-02-13 11:20:48 +0900

    Style: #13

--- programmers/join/상품_별_오프라인_매출_구하기/boo.sql
+++ programmers/join/상품_별_오프라인_매출_구하기/boo.sql
@@ -1,8 +1,8 @@
 SELECT
     p.product_code,
-    SUM(p.price * of.sales_amount) AS sales
+    SUM(p.price * os.sales_amount) AS sales
 FROM product AS p
-    JOIN offline_sale AS of
-        ON p.product_id = of.product_id
+    JOIN offline_sale AS os
+        ON p.product_id = os.product_id
 GROUP BY p.product_code
 ORDER BY sales DESC, p.product_code;
