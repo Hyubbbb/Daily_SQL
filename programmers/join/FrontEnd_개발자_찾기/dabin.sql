22f964d7f8dae6ee18cd93b86598023ab6c35a4d
yundabin <156299922+yundavin@users.noreply.github.com>
2026-02-14 14:58:50 +0900

    Submit: 윤다빈
    - 고난: skill_code와 code를 비교하기 위한 연산자
    - 해결: 비트 연산자를 사용

---
+++ programmers/join/FrontEnd_개발자_찾기/dabin.sql
@@ -0,0 +1,11 @@
+-- FrontEnd 개발자 찾기 (윤다빈)
+SELECT
+    DISTINCT(d.id),
+    d.email,
+    d.first_name,
+    d.last_name
+FROM developers AS d
+JOIN skillcodes AS s
+    ON d.skill_code & s.code != 0
+WHERE s.category = 'Front End'
+ORDER BY d.id;
