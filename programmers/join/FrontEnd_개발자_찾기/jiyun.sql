fbd9ae280afefd32047638827797a01e10572e20
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-02-15 12:58:25 +0900

    Submit: 이지윤
    - 고난: bit 연산이 기억이 안 남
    - 해결: GPT와 구글링으로 공부 후 해결

---
+++ programmers/join/FrontEnd_개발자_찾기/jiyun.sql
@@ -0,0 +1,16 @@
+ -- &: 비트 AND 연산자
+ -- 두 숫자를 2진수로 바꿔서 같은 자리끼리 비교한 다음
+ -- 둘 다 1이면 → 1
+ -- 하나라도 0이면 → 0
+
+SELECT
+    DISTINCT d.id,
+    d.email,
+    d.first_name,
+    d.last_name
+FROM skillcodes AS s
+    JOIN developers AS d
+        ON (d.SKILL_CODE & s.CODE) > 0
+WHERE 1=1
+    AND s.category = 'Front End'
+ORDER BY d.id
