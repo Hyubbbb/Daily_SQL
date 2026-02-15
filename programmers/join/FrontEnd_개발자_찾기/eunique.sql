5219e391d6ceb9097b1b77373f3b4da11c0bb974
Eunji Lee <jiyomi30@gmail.com>
2026-02-16 05:16:45 +0900

    Submit: 이은지
    - 고난: ON 부분 쿼리 작성
    - 해결: 컬럼 선언 별로 조건문 작성하지 않고 컬럼을 &으로 연결해서 작성 가능

---
+++ programmers/join/FrontEnd_개발자_찾기/eunique.sql
@@ -0,0 +1,12 @@
+-- 코드를 작성해주세요
+SELECT DISTINCT
+    d.id,
+    d.email,
+    d.first_name,
+    d.last_name
+FROM developers AS d
+    JOIN skillcodes AS s
+        ON d.skill_code & s.code > 0
+WHERE 1=1
+    AND s.category = 'Front End'
+ORDER BY d.id;
