29396ff01714a0a0316305310146c61b094d225e
gah508 <109083740+gah508@users.noreply.github.com>
2026-02-26 01:23:27 +0900

    Style: #14

--- programmers/join/FrontEnd_개발자_찾기/gah.sql
+++ programmers/join/FrontEnd_개발자_찾기/gah.sql
@@ -1,10 +1,11 @@
 SELECT DISTINCT
     d.id,
     d.email,
-    e.first_name,
+    d.first_name,
     d.last_name
 FROM developers AS d
     JOIN skillcodes AS s
         ON d.skill_code & s.code > 0
-WHERE s.category = 'Front End'
+WHERE 1=1
+    AND s.category = 'Front End'
 ORDER BY d.id;
