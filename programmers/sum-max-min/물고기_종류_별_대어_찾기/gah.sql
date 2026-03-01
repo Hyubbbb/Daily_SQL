bedbd76e659c8f3b59f0c87aebe7100fe0ab9fb7
gah508 <109083740+gah508@users.noreply.github.com>
2026-03-01 16:00:46 +0900

    Update: NOT NULL 조건 추가

--- programmers/sum-max-min/물고기_종류_별_대어_찾기/gah.sql
+++ programmers/sum-max-min/물고기_종류_별_대어_찾기/gah.sql
@@ -1,24 +1,23 @@
 WITH fish_rank AS (
-  SELECT
-    id,
-    fish_type,
-    length,
+    SELECT
+        id,
+        fish_type,
+        length,
     ROW_NUMBER() OVER (
-      PARTITION BY fish_type
-      ORDER BY length DESC
+        PARTITION BY fish_type
+        ORDER BY length DESC
     ) AS rn
-  FROM
-    fish_info
-  WHERE 1=1
-    AND length IS NOT NULL
+    FROM fish_info
+    WHERE 1=1
+        AND length IS NOT NULL
 )
 
 SELECT
-  fr.id,
-  fni.fish_name,
-  fr.length
+    fr.id,
+    fni.fish_name,
+    fr.length
 FROM fish_rank AS fr
-    JOIN FISH_NAME_INFO AS fni
+    JOIN fish_name_info AS fni
         ON fr.fish_type = fni.fish_type
 WHERE 1=1
     AND fr.rn = 1
