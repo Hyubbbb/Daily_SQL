2240ea6672c4518c7f87650649387c8b12e9db30
Junho Lee <100112445+Junoritto@users.noreply.github.com>
2026-03-02 18:37:28 +0900

    Style: #15

--- programmers/sum-max-min/물고기_종류_별_대어_찾기/junho.sql
+++ programmers/sum-max-min/물고기_종류_별_대어_찾기/junho.sql
@@ -5,14 +5,17 @@ WITH fish_ranked AS (
         fi.length,
         ROW_NUMBER() OVER (
             PARTITION BY fi.fish_type
-            ORDER BY fi.length DESC, fi.id ASC
+            ORDER BY fi.length DESC, fi.id
         ) AS ranking
     FROM fish_info fi
-    JOIN fish_name_info fni
-        ON fi.fish_type = fni.fish_type
+        JOIN fish_name_info fni
+            ON fi.fish_type = fni.fish_type
 )
 
-SELECT id, fish_name, length
+SELECT 
+    id, 
+    fish_name, 
+    length
 FROM fish_ranked
 WHERE 1=1
   AND ranking = 1
