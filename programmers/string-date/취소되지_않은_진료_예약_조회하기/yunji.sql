4cc5ad67992afd3897a938b2d2b41172ac4ab6ac
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-03-04 01:03:50 +0900

    Submit: 손윤지
    - 고난: 문제 조건을 잘못 이해해서 동일한 환자의 예약 및 취소 내역이 있는 걸로 판단했다. 동일 테이블 조인해서 차집합 써야 하나 열심히 고민했다.
    - 해결: 혹시나 싶어 위의 조건 처리 없이 제출했는데 정답이었다 .. 예시 보니 취소된 내역이 같은 pt_no가 아니어서 해당 조건 고려하지 않아도 됨을 깨달았다..!

---
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/yunji.sql
@@ -0,0 +1,19 @@
+SELECT 
+    a.apnt_no,
+    p.pt_name,
+    a.pt_no,
+    a.mcdp_cd,
+    d.dr_name,
+    a.apnt_ymd
+FROM appointment AS a
+    JOIN patient AS p
+        ON a.pt_no = p.pt_no
+    JOIN doctor AS d
+        ON a.mddr_id = d.dr_id
+WHERE 1=1
+    AND a.mcdp_cd = 'CS'
+    AND YEAR(a.apnt_ymd) = '2022'
+    AND MONTH(a.apnt_ymd) = '04'
+    AND DAY(a.apnt_ymd) = '13'
+    AND a.apnt_cncl_yn = 'N'
+ORDER BY a.apnt_ymd;
