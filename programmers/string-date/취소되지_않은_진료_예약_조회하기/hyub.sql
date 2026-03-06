eecc7d9c68f335a850d3b26af72eb8e8209ec43a
Hyubbbb <hyub@fnfcorp.com>
2026-03-06 09:53:06 +0900

    Update: TIMESTAMP에 맞는 기간 조건으로 수정

--- programmers/string-date/취소되지_않은_진료_예약_조회하기/hyub.sql
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/hyub.sql
@@ -11,7 +11,8 @@ FROM appointment AS a
     LEFT JOIN doctor AS d
         ON a.mddr_id = d.dr_id
 WHERE 1=1
-    AND a.apnt_ymd BETWEEN '2022-04-13' AND '2022-04-14' -- 2022년 4월 1일만 필터링
+    AND a.apnt_ymd >= '2022-04-13' 
+    AND a.apnt_ymd <  '2022-04-14' -- 해당 컬럼이 타임스탬프일 때에는 BETWEEN보다는 이런 식으로 조건을 거는 게 명확하다고 함
     AND a.apnt_cncl_yn = 'N'
     AND a.mcdp_cd = 'CS' -- 흉부외과
 ORDER BY a.apnt_ymd
