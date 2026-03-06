e29cb08d32891416aa2a286e2c62a987256a38dc
yundabin <156299922+yundavin@users.noreply.github.com>
2026-03-06 22:49:23 +0900

    Update: JOIN 들여쓰기, WHERE 1=1

--- programmers/is-null/ROOT_아이템_구하기/dabin.sql
+++ programmers/is-null/ROOT_아이템_구하기/dabin.sql
@@ -3,8 +3,9 @@ SELECT
     ii.item_id,
     ii.item_name
 FROM item_info AS ii
-INNER JOIN item_tree AS it
-    ON ii.item_id = it.item_id
-WHERE it.parent_item_id IS NULL
+    INNER JOIN item_tree AS it
+        ON ii.item_id = it.item_id
+WHERE 1=1
+    AND it.parent_item_id IS NULL
 ORDER BY item_id
 ;
