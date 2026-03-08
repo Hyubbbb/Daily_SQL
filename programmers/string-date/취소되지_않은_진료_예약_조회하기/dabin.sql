6b117a4f900e76e017b19c987ed616789f958065
yundabin <156299922+yundavin@users.noreply.github.com>
2026-03-08 23:31:08 +0900

    Submit: 윤다빈
    - 고난: 테이블 3개를 합쳐야 함
    - 해결: 키를 찾고, inner join 시행

---
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/dabin.sql
@@ -0,0 +1,16 @@
+-- 취소되지 않은 진료예약 조회하기
+SELECT 
+    ap.apnt_no, 
+    p.pt_name, 
+    ap.pt_no, 
+    ap.mcdp_cd, 
+    d.dr_name, 
+    ap.apnt_ymd
+FROM appointment AS ap
+    INNER JOIN doctor AS d ON d.dr_id = ap.mddr_id
+    INNER JOIN patient AS p ON p.pt_no = ap.pt_no
+WHERE 1=1
+    AND ap.apnt_cncl_ymd IS NULL 
+    AND ap.mcdp_cd = 'CS' 
+    AND ap.apnt_ymd LIKE '2022-04-13%'
+ORDER BY ap.apnt_ymd
