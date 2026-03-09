80ca6680bbdb41faeaa537f68e8fd623a1047815
Jungmin Park <bakjeongmin021009@gmail.com>
2026-03-09 16:35:01 +0900

    Submit: 박정민

---
+++ Archive/Season_11/Week_2/jungmin_problem_2.sql
@@ -0,0 +1,9 @@
+-- https://solvesql.com/problems/group-by/
+SELECT
+  quartet,
+  ROUND(AVG(x), 2) AS x_mean,
+  ROUND(VARIANCE(x), 2) AS x_var,
+  ROUND(AVG(y), 2) AS y_mean,
+  ROUND(VARIANCE(y), 2) AS y_var
+FROM points
+GROUP BY quartet
