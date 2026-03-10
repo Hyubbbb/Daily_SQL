d195bfd049d0afead472499cd458fc507ac9d1c0
Hyubbbb <hyub@fnfcorp.com>
2026-03-10 09:10:34 +0900

    Submit: 김협
    - 고난:
      1. CASE WHEN에서 7일 이상을 첫 줄에 입력해서 뒷부분의 WHEN이 계속 걸리지 않았다
      2. 두 번째 JOIN ON 조건에 car_type을 빼 먹음
      3. JOIN ON 조건 내에 CASE WHEN 조건을 쓸 수 있었는지 불확실했음
    - 해결:
      1. 한동안 헤매다가 해결
      2. 하나의 history_id당 출력 결과가 3개씩 나오는 걸 보고 눈치채고 수정
      3. 쿼리 작성해보고 JOIN 잘 되는 것 확인 후 진행

---
+++ programmers/string-date/자동차_대여_기록_별_대여_금액_구하기/hyub.sql
@@ -0,0 +1,19 @@
+SELECT
+    rh.history_id,
+    ROUND(
+        (DATEDIFF(rh.end_date, rh.start_date) + 1) * c.daily_fee * (1 - COALESCE(dp.discount_rate, 0) / 100)
+        , 0) AS fee
+FROM car_rental_company_rental_history AS rh
+    JOIN car_rental_company_car AS c
+        ON rh.car_id = c.car_id
+    LEFT JOIN car_rental_company_discount_plan AS dp
+        ON dp.car_type = c.car_type
+            AND dp.duration_type = (CASE
+                                        WHEN (DATEDIFF(rh.end_date, rh.start_date) + 1) >= 90 THEN '90일 이상'
+                                        WHEN (DATEDIFF(rh.end_date, rh.start_date) + 1) >= 30 THEN '30일 이상'
+                                        WHEN (DATEDIFF(rh.end_date, rh.start_date) + 1) >= 7  THEN '7일 이상'
+                                    END
+                                   )
+WHERE 1=1
+    AND c.car_type = '트럭'
+ORDER BY fee DESC, history_id DESC
