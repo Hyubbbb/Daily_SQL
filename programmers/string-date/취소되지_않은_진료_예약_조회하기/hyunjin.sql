e2c9bc909c222ca17da9d0c3f89fac2149212945
celinalab <pogang505799@ewhain.net>
2026-03-14 17:17:13 +0900

    Style: #16

--- programmers/string-date/취소되지_않은_진료_예약_조회하기/hyunjin.sql
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/hyunjin.sql
@@ -1,19 +1,19 @@
 -- 코드를 입력하세요
 SELECT
-   ap.apnt_no,
+   a.apnt_no,
    p.pt_name,
-   ap.pt_no,
-   ap.mcdp_cd,
+   a.pt_no,
+   a.mcdp_cd,
    d.dr_name,
-   ap.apnt_ymd
-FROM appointment AS ap
+   a.apnt_ymd
+FROM appointment AS a
   JOIN patient AS p
-    ON p.pt_no = ap.pt_no
+    ON p.pt_no = a.pt_no
   JOIN doctor AS d
-    ON d.dr_id = ap.mddr_id
+    ON d.dr_id = a.mddr_id
 WHERE 1=1
-   AND ap.mcdp_cd = 'CS'
-   AND ap.apnt_cncl_yn = 'N'
-   AND ap.apnt_ymd >= '2022-04-13'
-   AND ap.apnt_ymd < '2022-04-14'
-ORDER BY ap.apnt_ymd
+   AND a.mcdp_cd = 'CS'
+   AND a.apnt_cncl_yn = 'N'
+   AND a.apnt_ymd >= '2022-04-13'
+   AND a.apnt_ymd < '2022-04-14'
+ORDER BY a.apnt_ymd
