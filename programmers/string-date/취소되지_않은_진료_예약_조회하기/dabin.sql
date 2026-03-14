dea11a74be9037402d34cc5d59a7153f3b2de69c
yundabin <156299922+yundavin@users.noreply.github.com>
2026-03-14 15:41:25 +0900

    Style: #16

--- programmers/string-date/취소되지_않은_진료_예약_조회하기/dabin.sql
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/dabin.sql
@@ -7,10 +7,13 @@ SELECT
     d.dr_name, 
     ap.apnt_ymd
 FROM appointment AS ap
-    INNER JOIN doctor AS d ON d.dr_id = ap.mddr_id
-    INNER JOIN patient AS p ON p.pt_no = ap.pt_no
+    INNER JOIN doctor AS d 
+        oN d.dr_id = ap.mddr_id
+    INNER JOIN patient AS p 
+        ON p.pt_no = ap.pt_no
 WHERE 1=1
     AND ap.apnt_cncl_ymd IS NULL 
     AND ap.mcdp_cd = 'CS' 
-    AND ap.apnt_ymd LIKE '2022-04-13%'
+    AND ap.apnt_ymd >= '2022-04-13'
+    AND ap.apnt_ymd < '2022-04-14'
 ORDER BY ap.apnt_ymd
