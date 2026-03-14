cc1ceff7b577a467a796becf7c2d047522c9572e
itshoya <taipaominam@gmail.com>
2026-03-14 16:16:06 +0900

    Style: #16

--- programmers/string-date/취소되지_않은_진료_예약_조회하기/hyunho.sql
+++ programmers/string-date/취소되지_않은_진료_예약_조회하기/hyunho.sql
@@ -6,12 +6,13 @@ SELECT
     d.dr_name,
     a.apnt_ymd
 FROM appointment AS a
-    JOIN patient AS p
+    LEFT JOIN patient AS p
         ON a.pt_no = p.pt_no
-    JOIN doctor AS d
+    LEFT JOIN doctor AS d
         ON a.mddr_id = d.dr_id
 WHERE 1=1
-    AND DATE(a.apnt_ymd) = '2022-04-13'
+    AND a.apnt_ymd >= '2022-04-13'
+    AND a.apnt_ymd < '2022-04-14'
     AND a.apnt_cncl_yn = 'N'
     AND a.mcdp_cd = 'CS'
 ORDER BY a.apnt_ymd;
