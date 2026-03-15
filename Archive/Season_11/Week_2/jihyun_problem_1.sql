e6669bc118c80950815422a0335eca910fea4552
HYUNN <zeonnii721@gmail.com>
2026-03-15 17:56:34 +0900

    Submit: 윤지현

---
+++ Archive/Season_11/Week_2/jihyun_problem_1.sql
@@ -0,0 +1,6 @@
+# --https://school.programmers.co.kr/learn/courses/30/lessons/59040
+SELECT ANIMAL_TYPE, COUNT(DISTINCT ANIMAL_TYPE) count
+FROM ANIMAL_INS
+WHERE ANIMAL_TYPE IN ('Cat', 'Dog')
+GROUP BY ANIMAL_TYPE
+ORDER BY ANIMAL_TYPE
