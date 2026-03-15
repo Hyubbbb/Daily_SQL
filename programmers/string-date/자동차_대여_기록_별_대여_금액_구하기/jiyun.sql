a370d542658f50bd51555340c6c0ae24f454758a
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-03-15 15:45:12 +0900

    Submit: 이지윤
    - 고난: CTE를 어떻게 활용할지 고민
    - 해결: 
      - CTE로 대여 기간 타입을 분류하고, 메인 쿼리에서 할인율과 조인하여 해결
      - 할인 적용이 안 되는 경우가 제외되지 않도록 LEFT JOIN 활용

---
+++ programmers/string-date/자동차_대여_기록_별_대여_금액_구하기/jiyun.sql
@@ -0,0 +1,25 @@
+WITH history_duration AS ( -- 대여 기간 & 타입
+    SELECT
+        crcrh.history_id,
+        crcc.car_type,
+        crcc.daily_fee,
+        DATEDIFF(end_date, start_date) + 1 AS duration,
+        CASE
+            WHEN DATEDIFF(end_date, start_date) + 1 BETWEEN 7 AND 29 THEN '7일 이상'
+            WHEN DATEDIFF(end_date, start_date) + 1 BETWEEN 30 AND 89 THEN '30일 이상'
+            WHEN DATEDIFF(end_date, start_date) + 1 >= 90 THEN '90일 이상'
+            ELSE NULL
+        END AS duration_type 
+    FROM car_rental_company_rental_history AS crcrh
+        INNER JOIN car_rental_company_car AS crcc
+            ON crcrh.car_id = crcc.car_id
+)
+
+SELECT
+    hd.history_id,
+    ROUND(hd.duration * hd.daily_fee * (1-IFNULL(discount_rate, 0)/100)) AS fee
+FROM history_duration AS hd
+    LEFT JOIN car_rental_company_discount_plan As crcdp
+        ON hd.car_type = crcdp.car_type AND hd.duration_type = crcdp.duration_type
+WHERE hd.car_type = '트럭'
+ORDER BY fee DESC, hd.history_id DESC
