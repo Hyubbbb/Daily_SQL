cae7573f001d5de7115cdbb104e60f3838c7ee5c
HYUNN <zeonnii721@gmail.com>
2026-03-15 21:39:16 +0900

    Submit: 윤지현

---
+++ programmers/string-date/자동차_대여_기록_별_대여_금액_구하기/jihyun.sql
@@ -0,0 +1,14 @@
+SELECT history_id,  FLOOR(c.daily_fee * (100-IFNULL(d.discount_rate, 0))/100 * (DATEDIFF(h.end_date, h.start_date)+1)) AS fee
+FROM car_rental_company_rental_history AS h
+	LEFT JOIN car_rental_company_car AS c
+	ON h.car_id = c.car_id
+	LEFT JOIN car_rental_company_discount_plan d
+	ON c.car_type = d.car_type
+	AND d.duration_type = CASE
+		WHEN DATEDIFF(h.end_date, h.start_date) + 1 >= 90 THEN '90일 이상'
+		WHEN DATEDIFF(h.end_date, h.start_date) + 1 >= 30 THEN '30일 이상'
+		WHEN DATEDIFF(h.end_date, h.start_date) + 1 >= 7 THEN '7일 이상'
+		ELSE NULL
+	END
+WHERE c.car_type = '트럭'
+ORDER BY fee DESC, history_id DESC
