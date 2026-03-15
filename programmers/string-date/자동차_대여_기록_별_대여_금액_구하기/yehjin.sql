e43d2374c5086cdb31f9db52b4c081d9448bfc83
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-03-15 21:56:19 +0900

    Submit: 이예진
    고난: 자동차 대여 기간에 따른 할인율을 적용하여 대여 금액을 계산하는 SQL 구현에 어려움을 겪음.
    해결: CTE를 활용해 대여 기간을 먼저 계산하고, CASE문을 통해 기간별 할인 구간을 구분하여 해결함.

---
+++ programmers/string-date/자동차_대여_기록_별_대여_금액_구하기/yehjin.sql
@@ -0,0 +1,27 @@
+WITH truck AS (
+    SELECT 
+        crh.history_id,
+        cc.daily_fee,
+        cc.car_type,
+        DATEDIFF(crh.end_date, crh.start_date) + 1 AS period,
+        CASE
+            WHEN DATEDIFF(crh.end_date, crh.start_date) + 1 >= 90 THEN '90일 이상'
+            WHEN DATEDIFF(crh.end_date, crh.start_date) + 1 >= 30 THEN '30일 이상'
+            WHEN DATEDIFF(crh.end_date, crh.start_date) + 1 >= 7 THEN '7일 이상'
+            ELSE NULL
+        END AS duration_type
+    FROM car_rental_company_rental_history AS crh
+    JOIN car_rental_company_car AS cc
+        ON crh.car_id = cc.car_id
+    WHERE 1=1
+        AND cc.car_type = '트럭'
+)
+
+SELECT
+    t.history_id,
+    ROUND(t.daily_fee * t.period * (100 - IFNULL(cdp.discount_rate,0)) / 100) AS fee
+FROM truck AS t
+    LEFT JOIN car_rental_company_discount_plan AS cdp
+        ON t.car_type = cdp.car_type
+        AND t.duration_type = cdp.duration_type
+ORDER BY fee DESC, history_id DESC;
