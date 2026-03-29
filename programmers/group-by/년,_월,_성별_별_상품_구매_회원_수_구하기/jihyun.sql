b3d173be487bfffd4efcfc701d44dc511f090e6f
HYUNN <zeonnii721@gmail.com>
2026-03-29 23:15:58 +0900

    Submit: 윤지현

---
+++ programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/jihyun.sql
@@ -0,0 +1,14 @@
+SELECT
+    year(os.sales_date) AS year
+  , month(os.sales_date) AS month
+  , ui.gender
+  , COUNT(DISTINCT os.user_id) AS users
+FROM online_sale os
+JOIN user_info ui
+  ON os.user_id = ui.user_id
+WHERE 1 = 1
+  AND ui.gender IS NOT NULL
+GROUP BY
+    year, month, gender
+ORDER BY
+    year, month, gender;
