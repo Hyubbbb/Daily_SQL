69dae74f9c4c827ba880f9654c466038c13fb1fe
yundabin <156299922+yundavin@users.noreply.github.com>
2026-03-29 23:30:47 +0900

    Submit: 윤다빈
    고난: group by를 무엇을 기준으로 하는지
    해결: 연,월,성별으로 group by 지정

---
+++ programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/dabin.sql
@@ -0,0 +1,17 @@
+SELECT
+    YEAR(os.sales_date) AS year,
+    MONTH(os.sales_date) AS month,
+    ui.gender,
+    COUNT(DISTINCT os.user_id) AS user_count
+FROM user_info AS ui
+INNER JOIN online_sale AS os
+    ON ui.user_id = os.user_id
+WHERE ui.gender IS NOT NULL
+GROUP BY
+    YEAR(os.sales_date),
+    MONTH(os.sales_date),
+    ui.gender
+ORDER BY
+    year,
+    month,
+    ui.gender;
