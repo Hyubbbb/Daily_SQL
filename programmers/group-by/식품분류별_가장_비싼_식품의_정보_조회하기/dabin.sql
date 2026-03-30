9b1020491ab144c879049314f776ab144ee17838
yundabin <156299922+yundavin@users.noreply.github.com>
2026-03-30 16:18:26 +0900

    Submit: 윤다빈
    - 고난: 가격의 최댓값을 구하면 그 행도 구해져서 with을 사용하지 않아도 될 것이라고 생각
    - 해결: 윈도우 함수에서 각 행의 순위를 구하고 그룹별 최대값을 가진 행을 뽑는다

---
+++ programmers/group-by/식품분류별_가장_비싼_식품의_정보_조회하기/dabin.sql
@@ -0,0 +1,17 @@
+WITH max_price_cte AS (
+    SELECT
+        category,
+        MAX(price) AS max_price
+    FROM food_product
+    WHERE category IN ('과자', '국', '김치', '식용유')
+    GROUP BY category
+)
+SELECT
+    p.category,
+    p.price AS max_price,
+    p.product_name
+FROM food_product p
+  JOIN max_price_cte m
+    ON p.category = m.category
+    AND p.price = m.max_price
+ORDER BY p.price DESC;
