c0661260ab80fb1ab9302100f3142764a6c8c35a
Jungmin Park <bakjeongmin021009@gmail.com>
2026-03-30 15:09:12 +0900

    Update: WHERE절 1=1 추가
    해당 코드의 경우 서브쿼리가 CTE보다 좋아보인다.

--- Archive/Season_11/Week_5/jungmin_problem_2.sql
+++ Archive/Season_11/Week_5/jungmin_problem_2.sql
@@ -8,8 +8,10 @@ SELECT t.*
 FROM tips AS t
   INNER JOIN avg_bill AS a
     ON 1=1
-WHERE t.total_bill > a.avg_bill;
+WHERE 1=1
+    AND t.total_bill > a.avg_bill;
 
 -- SELECT *
 -- FROM tips
--- WHERE total_bill > (SELECT AVG(total_bill) FROM tips);
+-- WHERE 1=1
+--    AND total_bill > (SELECT AVG(total_bill) FROM tips);
