9e1357bf02b3b9d0132dc5086902ff5c36a4d4b6
Jungmin Park <bakjeongmin021009@gmail.com>
2026-03-30 14:43:52 +0900

    Submit: 박정민
    - 고난: CTE없이 단순히 SELECT절에 MAX만 넣고 진행하였을 때 틀린 결과라고 나와서 이후 어떻게 해결해야하는지 고민이 길었음
    - 해결: 단순히 MAX만 사용한다면 price만 변하고 해당 행이 변하는 게 아니라는 것을 알고나서 WITH절을 이용한 CTE를 이용하여 JOIN으로 max의 값을 출력하였음

---
+++ programmers/group-by/식품분류별_가장_비싼_식품의_정보_조회하기/jungmin.sql
@@ -0,0 +1,20 @@
+WITH max_price AS (
+    SELECT
+        category,
+        MAX(price) AS max_price
+    FROM food_product
+    WHERE 1=1
+        AND category IN ('과자', '국', '김치', '식용유')
+    GROUP BY category
+)
+
+SELECT
+    f.category,
+    m.max_price,
+    f.product_name
+FROM food_product AS f
+    JOIN max_price AS m
+        ON f.category = m.category
+WHERE 1=1
+    AND f.price = m.max_price
+ORDER BY m.max_price DESC;
