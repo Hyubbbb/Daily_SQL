933293a2d8b35560c25b6ce020bb099eb8cea8dc
seoyeoning <ssseoyeon95@naver.com>
2026-04-02 15:56:38 +0900

    Style: #19

--- programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/seoyeon.sql
+++ programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/seoyeon.sql
@@ -4,9 +4,9 @@ SELECT
     gender,
     COUNT(DISTINCT os.user_id) AS users
 FROM online_sale AS os
-    LEFT JOIN user_info AS ui
+    INNER JOIN user_info AS ui
         ON os.user_id = ui.user_id
-WHERE 1 = 1
+WHERE 1=1
     AND ui.gender IS NOT NULL
 GROUP BY YEAR(os.sales_date), MONTH(os.sales_date), ui.gender
 ORDER BY year, month, ui.gender;
