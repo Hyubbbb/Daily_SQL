75c5a91969858766415e98d6279decea74169ed5
HYUNN <zeonnii721@gmail.com>
2026-04-03 13:32:17 +0900

    Style: #19

--- programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/jihyun.sql
+++ programmers/group-by/년,_월,_성별_별_상품_구매_회원_수_구하기/jihyun.sql
@@ -4,11 +4,9 @@ SELECT
     ui.gender,
     COUNT(DISTINCT os.user_id) AS users
 FROM online_sale os
-JOIN user_info ui
-  ON os.user_id = ui.user_id
+    JOIN user_info ui
+        ON os.user_id = ui.user_id
 WHERE 1 = 1
   AND ui.gender IS NOT NULL
-GROUP BY
-    year, month, gender
-ORDER BY
-    year, month, gender;
+GROUP BY year, month, gender
+ORDER BY year, month, gender;
