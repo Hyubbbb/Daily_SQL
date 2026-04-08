2667f53f9d9d016e1c3eb1a241aa844f22995623
yundabin <156299922+yundavin@users.noreply.github.com>
2026-04-08 10:48:18 +0900

    Submit: 윤다빈

---
+++ programmers/group-by/저자_별_카테고리_별_매출액_집계하기/dabin.sql
@@ -0,0 +1,20 @@
+-- 저자 별 카테고리 별 매출액 집계하기
+SELECT
+    a.author_id,
+    a.author_name,
+    b.category,
+    SUM(b.price * bs.sales) AS total_sales
+FROM book AS b
+    JOIN author AS a
+        ON b.author_id = a.author_id
+    JOIN book_sales AS bs
+        ON b.book_id = bs.book_id
+WHERE bs.sales_date >= '2022-01-01'
+  AND bs.sales_date < '2022-02-01'
+GROUP BY
+    b.author_id,
+    a.author_name,
+    b.category
+ORDER BY
+    b.author_id,
+    b.category DESC;
