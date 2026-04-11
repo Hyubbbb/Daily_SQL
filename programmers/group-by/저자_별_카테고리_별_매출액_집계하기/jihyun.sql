2f94c7aff6dc2d9bc2f098738fabaabffa1a93b8
HYUNN <zeonnii721@gmail.com>
2026-04-11 22:15:02 +0900

    Submit: 윤지현

---
+++ programmers/group-by/저자_별_카테고리_별_매출액_집계하기/jihyun.sql
@@ -0,0 +1,17 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/144856
+
+SELECT 
+    author.author_id, 
+    author.author_name, 
+    book.category, 
+    SUM(book.price*bsales.sales) AS sales
+FROM book
+    JOIN author
+        ON book.author_id = author.author_id
+    JOIN book_sales AS bsales
+        ON book.book_id = bsales.book_id
+WHERE 1=1
+    AND sales_date > '2021-12-31'
+    AND sales_date < '2022-02-01'
+GROUP BY author_id, category
+ORDER BY author_id, category DESC
