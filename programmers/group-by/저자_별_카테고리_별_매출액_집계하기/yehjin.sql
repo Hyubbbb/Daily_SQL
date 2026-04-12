88c92117485a013c3e41753cbff67914d411a328
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-04-12 14:43:40 +0900

    Submit: 이예진
    고난: 날짜 조건에서 특정 연도의 특정 달만 필터링하는 방법 헷갈림
    해결: between으로 1월1일부터 1월 31일까지 필터링함

---
+++ programmers/group-by/저자_별_카테고리_별_매출액_집계하기/yehjin.sql
@@ -0,0 +1,14 @@
+SELECT
+    b.author_id,
+    a.author_name,
+    b.category,
+    SUM(bs.sales*b.price) AS total_sales
+FROM book AS b
+    JOIN author AS a
+        ON b.author_id=a.author_id
+    JOIN book_sales AS bs
+        ON b.book_id=bs.book_id
+WHERE 1=1
+    AND bs.sales_date BETWEEN '2022-01-01' AND '2022-01-31'
+GROUP BY b.author_id, b.category
+ORDER BY b.author_id, b.category DESC;
