a3ead7f9cb3e22a50530a5da033f69acb3c858f8
Jungmin Park <bakjeongmin021009@gmail.com>
2026-04-15 20:33:30 +0900

    Submit: 박정민

---
+++ Archive/Season_11/Week_7/jungmin_problem_1.sql
@@ -0,0 +1,9 @@
+-- https://solvesql.com/problems/find-christmas-games/
+SELECT 
+    game_id,
+    name,
+    year
+FROM games
+WHERE 1=1
+    AND name LIKE '%Christmas%'
+    OR name LIKE '%Santa%'
