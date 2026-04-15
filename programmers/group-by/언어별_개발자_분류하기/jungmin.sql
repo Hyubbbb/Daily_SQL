3c40dc6fc3563b624e3bd0e1556478668ba3e496
Jungmin Park <bakjeongmin021009@gmail.com>
2026-04-15 20:26:15 +0900

    Submit: 박정민
    - 고난: GROUP BY와 비트연산자로 해결하려하니 방식이 떠오르지 않고, 너무 복잡하게 생각하게 되었음.
    - 해결: 차라리 WITH CTE를 사용하여 따로 분류하고, grade_table을 만들어 진행하니 오히려 편하게 작성할 수 있었음.

---
+++ programmers/group-by/언어별_개발자_분류하기/jungmin.sql
@@ -0,0 +1,44 @@
+WITH frontend AS (
+    SELECT SUM(code) AS frontend
+    FROM skillcodes
+    WHERE 1=1
+        AND category = 'front end'
+),
+python AS (
+    SELECT code AS python
+    FROM skillcodes
+    WHERE 1=1
+        AND name = 'python'
+),
+csharp AS (
+    SELECT code AS csharp
+    FROM skillcodes
+    WHERE 1=1
+        AND name = 'c#'
+),
+grade_table AS (
+    SELECT 
+        CASE
+            WHEN (d.skill_code & f.frontend !=0) AND (d.skill_code & p.python !=0) THEN 'A'
+            WHEN (d.skill_code & c.csharp !=0) THEN 'B'
+            WHEN (d.skill_code & f.frontend !=0) THEN 'C'
+        END AS grade,
+        d.id,
+        d.email
+    FROM developers AS d
+        JOIN frontend AS f
+            ON 1=1
+        JOIN python AS p
+            ON 1=1
+        JOIN csharp AS c
+            ON 1=1
+)
+
+SELECT
+    grade,
+    id,
+    email
+FROM grade_table
+WHERE 1=1
+    AND grade IS NOT NULL
+ORDER BY grade, id;
