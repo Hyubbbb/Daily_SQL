5105fd4f12f6207fa571ee253f803c0ed9f1c000
Hyubbbb <hyub@fnfcorp.com>
2026-04-16 14:58:47 +0900

    Submit: 김협
    - 고난: `CASE WHEN`에서 NULL인 값을 `WHERE`절에서 필터링할 수가 없음
      - 이유: 명령어 실행 순서에 의해 `WHERE` 이후에 `SELECT`가 실행됨
    - 해결: CTE로 만들었음

---
+++ programmers/group-by/언어별_개발자_분류하기/hyub.sql
@@ -0,0 +1,19 @@
+WITH dev_grade AS (
+    SELECT 
+        CASE 
+            WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End')) AND (skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')) THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') THEN 'C'
+        END AS grade,
+        id,
+        email
+    FROM developers
+)
+SELECT
+    grade,
+    id,
+    email
+FROM dev_grade
+WHERE 1=1
+    AND grade IS NOT NULL
+ORDER BY grade, id
