294f90507f4cecaf0124a258b683f9defd89b086
HYUNN <zeonnii721@gmail.com>
2026-04-17 15:12:11 +0900

    Style: #21

--- programmers/group-by/저자_별_카테고리_별_매출액_집계하기/jihyun.sql
+++ programmers/group-by/저자_별_카테고리_별_매출액_집계하기/jihyun.sql
@@ -1,15 +1,15 @@
 SELECT 
-    author.author_id, 
-    author.author_name, 
-    book.category, 
-    SUM(book.price*bsales.sales) AS sales
-FROM book
-    JOIN author
-        ON book.author_id = author.author_id
-    JOIN book_sales AS bsales
-        ON book.book_id = bsales.book_id
+    a.author_id, 
+    a.author_name, 
+    b.category, 
+    SUM(b.price*bs.sales) AS sales
+FROM book AS b
+    JOIN author AS a
+        ON b.author_id = a.author_id
+    JOIN book_sales AS bs
+        ON b.book_id = bs.book_id
 WHERE 1=1
     AND sales_date > '2021-12-31'
     AND sales_date < '2022-02-01'
-GROUP BY author_id, category
-ORDER BY author_id, category DESC
+GROUP BY a.author_id, b.category
+ORDER BY a.author_id, b.category DESC
