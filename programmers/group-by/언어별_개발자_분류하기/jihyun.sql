ae526496ba827badc87ef9df8ea97f2fd2338f2d
HYUNN <zeonnii721@gmail.com>
2026-04-18 20:27:19 +0900

    Submit: 윤지현

--- programmers/group-by/언어별_개발자_분류하기/jihyun.sql
+++ programmers/group-by/언어별_개발자_분류하기/jihyun.sql
@@ -1,12 +1,8 @@
 SELECT
     CASE
-        WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category='Front End')) > 0
-            AND (skill_code & (SELECT code FROM skillcodes WHERE name='Python')) > 0
-            THEN 'A'
-        WHEN (skill_code & (SELECT code FROM skillcodes WHERE name='C#')) > 0
-            THEN 'B'
-        WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category='Front End')) > 0
-            THEN 'C'
+        WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category='Front End')) > 0 AND (skill_code & (SELECT code FROM skillcodes WHERE name='Python')) > 0 THEN 'A'
+        WHEN (skill_code & (SELECT code FROM skillcodes WHERE name='C#')) > 0 THEN 'B'
+        WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category='Front End')) > 0 THEN 'C'
     END AS grade,
     id,
     email
