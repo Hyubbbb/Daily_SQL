6ed8566be30e0ec1425f04b77d0629b8ed143484
celinalab <pogang505799@ewhain.net>
2026-04-19 18:30:07 +0900

    Submit: 임현진
    고난: casewhen이 어려웠음
    해결: 제미나이와 함께 해결 ~!

---
+++ programmers/group-by/언어별_개발자_분류하기/hyunjin.sql
@@ -0,0 +1,22 @@
+WITH graded AS (
+    SELECT 
+        CASE 
+            WHEN (SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End')) 
+                 AND (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'Python')) THEN 'A'
+            WHEN (SKILL_CODE & (SELECT CODE FROM SKILLCODES WHERE NAME = 'C#')) THEN 'B'
+            WHEN (SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End')) THEN 'C'
+            ELSE NULL 
+        END AS 
+        GRADE,
+        ID, 
+        EMAIL
+    FROM DEVELOPERS
+)
+
+SELECT 
+   g.grade,
+   g.id,
+   g.email
+FROM graded AS g
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE ASC, ID ASC
