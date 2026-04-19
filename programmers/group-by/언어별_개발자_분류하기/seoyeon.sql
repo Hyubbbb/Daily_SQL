1858d3b010a33a357fd9b3193b64b9e0fe58c2de
seoyeoning <ssseoyeon95@naver.com>
2026-04-19 23:08:08 +0900

    Submit: 김서연
    - 고난: SKILL_CODE가 비트마스크 형태라 특정 언어 보유 여부를 일반 비교로 판단할 수 없어 조건 설정에서 혼란이 생김
    - 해결: 각 스킬 코드를 조회한 뒤 비트연산(&)을 사용하여 해당 스킬 포함 여부를 정확히 판별하고, CASE문으로 등급을 구분함

---
+++ programmers/group-by/언어별_개발자_분류하기/seoyeon.sql
@@ -0,0 +1,41 @@
+WITH frontend_skill AS (
+  SELECT
+    SUM(sc.code) AS frontend_code
+  FROM skillcodes AS sc
+  WHERE sc.category = 'Front End'
+),
+python_skill AS (
+  SELECT
+    sc.code AS python_code
+  FROM skillcodes AS sc
+  WHERE sc.name = 'Python'
+),
+csharp_skill AS (
+  SELECT
+    sc.code AS csharp_code
+  FROM skillcodes AS sc
+  WHERE sc.name = 'C#'
+),
+developer_grade AS (
+  SELECT
+    CASE
+      WHEN (d.skill_code & fs.frontend_code) > 0
+        AND (d.skill_code & ps.python_code) > 0 THEN 'A'
+      WHEN (d.skill_code & cs.csharp_code) > 0 THEN 'B'
+      WHEN (d.skill_code & fs.frontend_code) > 0 THEN 'C'
+    END AS grade,
+    d.id AS id,
+    d.email AS email
+  FROM developers AS d
+    CROSS JOIN frontend_skill AS fs
+    CROSS JOIN python_skill AS ps
+    CROSS JOIN csharp_skill AS cs
+)
+
+SELECT
+  dg.grade AS grade,
+  dg.id AS id,
+  dg.email AS email
+FROM developer_grade AS dg
+WHERE dg.grade IS NOT NULL
+ORDER BY grade, id;
