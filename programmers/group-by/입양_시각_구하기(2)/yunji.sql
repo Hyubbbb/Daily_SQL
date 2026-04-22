83cd12ea5982b892b8fb7801f68594abbfafd4b5
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-04-22 16:23:33 +0900

    Submit: 손윤지
    - 고난: count 값이 0인 hour 행을 어떻게 추가해야 할지 고민이 정말 많았다.
    - 해결: Recursive CTE 구조를 처음 알게 되었다 ! SQL에서도 재귀를 쓸 수 있는지 몰랐다 .. hours 테이블을 별도로 만들어 조건 일치하는 것만 join하여 완성했다 :)

---
+++ programmers/group-by/입양_시각_구하기(2)/yunji.sql
@@ -0,0 +1,19 @@
+WITH RECURSIVE hours AS (
+    -- 최초 1회 실행: 0이라는 값을 hour이라는 컬럼에 삽입
+    SELECT 0 AS hour
+    
+    UNION ALL
+    
+    -- 이후 반복해서 23시까지 행 새로 생성 로직
+    SELECT hour + 1
+    FROM hours
+    WHERE hour < 23
+)
+SELECT
+    h.hour AS hour,
+    COUNT(a.ANIMAL_ID) AS count
+FROM hours AS h
+    LEFT JOIN animal_outs AS a
+        ON HOUR(a.DATETIME) = h.hour
+GROUP BY h.hour
+ORDER BY h.hour;
