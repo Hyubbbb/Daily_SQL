7d2f8c1bfe419dfba839d1545da4ff39a4dc593a
Hyubbbb <hyub@fnfcorp.com>
2026-04-23 13:41:02 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/hyub.sql
+++ programmers/group-by/언어별_개발자_분류하기/hyub.sql
@@ -1,9 +1,13 @@
 WITH dev_grade AS (
     SELECT 
         CASE 
-            WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End')) AND (skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')) THEN 'A'
-            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') THEN 'B'
-            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') THEN 'C'
+            WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End'))
+                AND (skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')) 
+                THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') 
+                THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') 
+                THEN 'C'
         END AS grade,
         id,
         email
