9281b1e86be7799776adcf2d175f9b27f8530cc4
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-04-24 15:22:48 +0900

    S

---
+++ programmers/group-by/입양_시각_구하기(2)/boo.sql
@@ -0,0 +1,19 @@
+WITH RECURSIVE hours AS(
+    SELECT 0 AS hour
+    
+    UNION ALL 
+    
+    SELECT hour + 1
+    FROM hours
+    WHERE 1=1
+        AND hour < 23
+)
+
+SELECT
+    h.hour,
+    COUNT(ao.DATETIME) AS count
+FROM hours AS h
+    LEFT JOIN ANIMAL_OUTS AS ao
+        ON h.hour = HOUR(ao.datetime)
+GROUP BY h.hour
+ORDER BY h.hour;
