24436454c8a15c3d1238a6e69440ac32d9463956
yundabin <156299922+yundavin@users.noreply.github.com>
2026-04-26 11:46:40 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/dabin.sql
+++ programmers/group-by/언어별_개발자_분류하기/dabin.sql
@@ -19,33 +19,29 @@ csharp_skill AS (
     FROM skillcodes
     WHERE 1 = 1
       AND name = 'C#'
+),
+classified AS (
+    SELECT
+        CASE
+            WHEN d.skill_code & f.frontend_code > 0
+             AND d.skill_code & p.python_code > 0 THEN 'A'
+            WHEN d.skill_code & c.csharp_code > 0 THEN 'B'
+            WHEN d.skill_code & f.frontend_code > 0 THEN 'C'
+        END AS grade,
+        d.id,
+        d.email
+    FROM developers AS d
+    CROSS JOIN frontend_skill AS f
+    CROSS JOIN python_skill AS p
+    CROSS JOIN csharp_skill AS c
 )
-
 SELECT
-    CASE
-        WHEN developers.skill_code & frontend_skill.frontend_code > 0
-         AND developers.skill_code & python_skill.python_code > 0 THEN 'A'
-        WHEN developers.skill_code & csharp_skill.csharp_code > 0 THEN 'B'
-        WHEN developers.skill_code & frontend_skill.frontend_code > 0 THEN 'C'
-    END AS grade,
-    developers.id,
-    developers.email
-FROM developers
-CROSS JOIN frontend_skill
-CROSS JOIN python_skill
-CROSS JOIN csharp_skill
+    grade,
+    id,
+    email
+FROM classified
 WHERE 1 = 1
-  AND (
-        developers.skill_code & frontend_skill.frontend_code > 0
-     OR developers.skill_code & python_skill.python_code > 0
-     OR developers.skill_code & csharp_skill.csharp_code > 0
-  )
-  AND CASE
-          WHEN developers.skill_code & frontend_skill.frontend_code > 0
-           AND developers.skill_code & python_skill.python_code > 0 THEN 'A'
-          WHEN developers.skill_code & csharp_skill.csharp_code > 0 THEN 'B'
-          WHEN developers.skill_code & frontend_skill.frontend_code > 0 THEN 'C'
-      END IS NOT NULL
+  AND grade IS NOT NULL
 ORDER BY
     grade,
-    developers.id;
+    id;
