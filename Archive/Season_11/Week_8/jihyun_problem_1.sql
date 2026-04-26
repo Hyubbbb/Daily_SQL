e310b5d8c11cc11aeaae66aa83a9d739ae4308da
HYUNN <zeonnii721@gmail.com>
2026-04-26 21:08:41 +0900

    Submit: 윤지현

--- Archive/Season_11/Week_8/jihyun_problem_1.sql
+++ Archive/Season_11/Week_8/jihyun_problem_1.sql
@@ -1,11 +1,18 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132201
+
 SELECT 
-    PT_NAME,
-    PT_NO,
-    GEND_cd,
+    pt_name,
+    pt_no,
+    gend_cd,
     age,
-    NVL(tlno, 'NONE')
+    CASE 
+        WHEN tlno IS NOT NULL
+            THEN tlno
+        WHEN tlno IS NULL
+            THEN 'NONE'
+        END AS tlno
 FROM patient
 WHERE 1=1
-    AND age <= 12 
-    AND gend_cd = 'W'   
+    AND age <= 12
+    AND gend_cd = 'W'
 ORDER BY age DESC, pt_name
