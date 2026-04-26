4c1ac2468660ff87f638cd2c5e0bf2e1cefbb1eb
yundabin <156299922+yundavin@users.noreply.github.com>
2026-04-26 11:53:49 +0900

    Submit: 윤다빈
    - 고난: 시간대에 입양 건수가 발생하지 않은 경우 0으로 표시해야함
    - 해결: 기준 테이블을 만든 뒤 left join을 통해 데이터를 붙임

---
+++ programmers/group-by/입양_시각_구하기(2)/dabin.sql
@@ -0,0 +1,19 @@
+WITH recursive hours AS (
+    SELECT 0 AS hour
+    UNION ALL
+    SELECT hour + 1
+    FROM hours
+    WHERE 1 = 1
+      AND hour < 23
+)
+SELECT
+    h.hour,
+    COUNT(o.datetime) AS count
+FROM hours AS h
+LEFT JOIN animal_outs AS o
+    ON HOUR(o.datetime) = h.hour
+WHERE 1 = 1
+GROUP BY
+    h.hour
+ORDER BY
+    h.hour;
