014e0e894e899168394adf147a7ea515857b0d1b
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-04-27 08:44:56 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/boo.sql
+++ programmers/group-by/언어별_개발자_분류하기/boo.sql
@@ -1,24 +1,21 @@
-WITH dev_skill AS(
-    SELECT 
-        d.id,
-        GROUP_CONCAT(s.name) AS name,
-        GROUP_CONCAT(s.category) AS category
-    FROM developers AS d
-        JOIN skillcodes AS s
-            ON d.skill_code & s.code = s.code
-    GROUP BY d.id
+WITH dev_skill AS (
+    SELECT
+        CASE
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0
+                AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') != 0 THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0 THEN 'C'
+        END AS grade,
+        id,
+        email
+    FROM developers
 )
 
 SELECT
-    CASE
-        WHEN ds.category LIKE '%Front End%' AND ds.name LIKE '%Python%' THEN 'A'
-        WHEN ds.name LIKE '%C#%' THEN 'B'
-        WHEN ds.category LIKE '%Front End%' THEN 'C'
-    END AS grade,
-    ds.id,
-    d.email
-FROM dev_skill AS ds
-    JOIN developers AS d
-        ON ds.id = d.id
-HAVING grade IS NOT NULL
-ORDER BY grade, d.id;
+    grade,
+    id,
+    email
+FROM dev_skill
+WHERE 1=1
+    AND grade IS NOT NULL
+ORDER BY grade, id;
