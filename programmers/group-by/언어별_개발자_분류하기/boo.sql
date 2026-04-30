6391c4983dc17bfe8b0cc3d9076d90a9980a8948
BooGyeom Kim <153355571+b00kkk@users.noreply.github.com>
2026-04-30 16:46:16 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/boo.sql
+++ programmers/group-by/언어별_개발자_분류하기/boo.sql
@@ -2,9 +2,12 @@ WITH dev_skill AS (
     SELECT
         CASE
             WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0
-                AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') != 0 THEN 'A'
-            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') THEN 'B'
-            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0 THEN 'C'
+                 AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') != 0 
+                 THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') 
+                 THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0 
+                 THEN 'C'
         END AS grade,
         id,
         email
