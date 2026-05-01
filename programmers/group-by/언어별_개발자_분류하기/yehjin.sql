a7dcd7bb758a335b4004e1f264dc87a9f0ca7646
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-05-01 15:30:43 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/yehjin.sql
+++ programmers/group-by/언어별_개발자_분류하기/yehjin.sql
@@ -1,11 +1,23 @@
+WITH dev_grade AS (
+    SELECT 
+        CASE 
+            WHEN (skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End'))
+                AND (skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')) 
+                THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') 
+                THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') 
+                THEN 'C'
+        END AS grade,
+        id,
+        email
+    FROM developers
+)
 SELECT
-    CASE
-        WHEN d.skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') > 0 AND d.skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') > 0 THEN 'A'
-        WHEN d.skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') > 0 THEN 'B'
-        WHEN d.skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') > 0 THEN 'C'
-    END AS grade,
-    d.id,
-    d.email
-FROM developers AS d
-HAVING grade IS NOT NULL
-ORDER BY grade, id;
+    grade,
+    id,
+    email
+FROM dev_grade
+WHERE 1=1
+    AND grade IS NOT NULL
+ORDER BY grade, id
