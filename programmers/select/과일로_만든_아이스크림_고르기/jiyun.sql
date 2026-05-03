e4ed00bda146096cb6129717185e809d35fde4ef
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-05-03 17:01:08 +0900

    Submit: 이지윤
    - 고난: 어려움 없이 해결
    - 해결: -

---
+++ programmers/select/과일로_만든_아이스크림_고르기/jiyun.sql
@@ -0,0 +1,8 @@
+SELECT fh.flavor
+FROM first_half AS fh
+    INNER JOIN icecream_info AS ii
+        ON fh.flavor = ii.flavor
+WHERE 1=1
+    AND fh.total_order > 3000
+    AND ii.ingredient_type = 'fruit_based'
+ORDER BY fh.total_order DESC
