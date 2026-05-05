3e7933af6532ef71cd1cc1e374231c2a4e90faa3
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-05-05 16:53:04 +0900

    Style: #22

--- programmers/group-by/언어별_개발자_분류하기/jiyun.sql
+++ programmers/group-by/언어별_개발자_분류하기/jiyun.sql
@@ -2,9 +2,12 @@ WITH dev_grade AS (
     SELECT
         CASE
             WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0
-                AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') != 0 THEN 'A'
-            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') != 0 THEN 'B'
-            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0 THEN 'C'
+                AND skill_code & (SELECT code FROM skillcodes WHERE name = 'Python') != 0 
+                THEN 'A'
+            WHEN skill_code & (SELECT code FROM skillcodes WHERE name = 'C#') != 0 
+                THEN 'B'
+            WHEN skill_code & (SELECT SUM(code) FROM skillcodes WHERE category = 'Front End') != 0 
+                THEN 'C'
         END AS grade,
         id,
         email
