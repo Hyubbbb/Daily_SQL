2bb675ee63e863b32bf098c4081a8355d48f9987
HYUNN <zeonnii721@gmail.com>
2026-05-06 10:42:40 +0900

    Submit: 윤지현
    - 고난: id를 기준으로 JOIN 수행한 뒤, 하위 아이템의 name을 어떻게 조인할지 고민
    - 해결: CTE로 하위 아이템 정보와 parent_item_id를 먼저 정리한 뒤, item_info와 한 번 더 JOIN

---
+++ programmers/select/업그레이드_된_아이템_구하기/jihyun.sql
@@ -0,0 +1,19 @@
+WITH tree AS (
+    SELECT tree.item_id,
+            info.item_name,
+            tree.parent_item_id,
+            info.rarity
+    FROM item_tree AS tree
+    JOIN item_info AS info
+        ON tree.item_id = info.item_id
+        )
+        
+SELECT tree.item_id,
+        tree.item_name,
+        tree.rarity
+FROM item_info AS info
+JOIN tree
+    ON info.item_id = tree.parent_item_id
+WHERE 1=1
+    AND info.rarity = 'RARE'
+ORDER BY tree.item_id DESC
