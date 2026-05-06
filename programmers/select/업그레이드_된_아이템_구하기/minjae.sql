0f5e34b46770d55f751b1ea3faab969118c2732b
mmminjaekim <minjaekim00@snu.ac.kr>
2026-05-06 18:42:09 +0900

    Submit: 김민재
    - 고난: CTE(WITH) 문법을 이용한 구조를 처음 접해, 설계에 어려움.
    - 해결: WITH절 문법 학습.

---
+++ programmers/select/업그레이드_된_아이템_구하기/minjae.sql
@@ -0,0 +1,19 @@
+WITH rare_item AS (
+    SELECT
+        item_id
+    FROM item_info
+    WHERE 1=1
+        AND rarity = 'RARE'
+)
+
+SELECT
+    info.item_id,
+    info.item_name,
+    info.rarity
+FROM item_info AS info
+    INNER JOIN item_tree AS tree
+        ON info.item_id = tree.item_id
+    INNER JOIN rare_item AS rare
+        ON tree.parent_item_id = rare.item_id
+WHERE 1=1
+ORDER BY info.item_id DESC;
