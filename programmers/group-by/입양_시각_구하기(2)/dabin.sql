31489b34a4fab933b3c28afafc817e2e25789101
yundabin <156299922+yundavin@users.noreply.github.com>
2026-05-10 23:04:37 +0900

    Style: #23

--- programmers/group-by/입양_시각_구하기(2)/dabin.sql
+++ programmers/group-by/입양_시각_구하기(2)/dabin.sql
@@ -1,18 +1,23 @@
-WITH recursive hours AS (
-    SELECT 0 AS hour
+-- 시간대별 입양 건수 구하기
+WITH RECURSIVE hours AS (
+    SELECT 
+        0 AS hour
     UNION ALL
-    SELECT hour + 1
-    FROM hours
-    WHERE 1 = 1
+    SELECT 
+        hour + 1
+    FROM 
+        hours
+    WHERE 1=1
       AND hour < 23
 )
+
 SELECT
     h.hour,
     COUNT(o.datetime) AS count
 FROM hours AS h
-LEFT JOIN animal_outs AS o
-    ON HOUR(o.datetime) = h.hour
-WHERE 1 = 1
+    LEFT JOIN animal_outs AS o
+        ON HOUR(o.datetime) = h.hour
+    WHERE 1=1
 GROUP BY
     h.hour
 ORDER BY
