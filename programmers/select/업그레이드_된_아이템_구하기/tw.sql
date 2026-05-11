1c101be50ad18a898e0702ca2af953ac605579db
tux2won <114200466+tux2won@users.noreply.github.com>
2026-05-11 21:14:13 +0900

    Submit: 송태원

---
+++ programmers/select/업그레이드_된_아이템_구하기/tw.sql
@@ -0,0 +1,13 @@
+SELECT
+    a.ITEM_ID,
+    b.ITEM_NAME,
+    b.RARITY
+FROM ITEM_TREE AS item1
+    JOIN ITEM_INFO itme2
+        ON item1.ITEM_ID=item2.ITEM_ID
+WHERE 1=1
+    AND PARENT_ITEM_ID IN (
+        SELECT ITEM_ID 
+        FROM ITEM_INFO
+        WHERE RARITY = 'RARE') 
+ORDER BY a.ITEM_ID DESC
