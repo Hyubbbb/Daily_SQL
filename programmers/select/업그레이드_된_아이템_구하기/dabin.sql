af4f1ab50323d007f916b96f0238ddf30f8d76e0
yundabin <156299922+yundavin@users.noreply.github.com>
2026-05-14 16:48:47 +0900

    Update: JOIN 들여쓰기

--- programmers/select/업그레이드_된_아이템_구하기/dabin.sql
+++ programmers/select/업그레이드_된_아이템_구하기/dabin.sql
@@ -1,25 +1,16 @@
 -- 업그레이드 된 아이템 구하기 (윤다빈)
 WITH rare_items AS (
-    SELECT 
-        item_id
-    FROM 
-        item_info
-    WHERE 1=1
-      AND rarity = 'RARE'
+    SELECT item_id
+    FROM item_info
+    WHERE 1 = 1
+        AND rarity = 'RARE'
 )
 
-SELECT 
-    info.item_id, 
-    info.item_name, 
-    info.rarity
-FROM 
-    item_info AS info 
-JOIN 
-    item_tree AS tree
-    ON info.item_id = tree.item_id
-JOIN 
-    rare_items AS rare
-    ON tree.parent_item_id = rare.item_id
-WHERE 1=1
-ORDER BY 
-    info.item_id DESC;
+SELECT info.item_id, info.item_name, info.rarity
+FROM item_info AS info
+    INNER JOIN item_tree AS tree
+        ON info.item_id = tree.item_id
+    INNER JOIN rare_items AS rare
+        ON tree.parent_item_id = rare.item_id
+WHERE 1 = 1
+ORDER BY info.item_id DESC;
