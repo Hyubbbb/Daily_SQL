808fad4b9c2be246d19f9267f6c935d048aa1f76
tux2won <114200466+tux2won@users.noreply.github.com>
2026-05-17 10:04:43 +0900

    Submit: 송태원
    - 고난: 기존 풀이에서는 IF문을 SELECT 조회하는 방식으로 풀이, 가독성x
    - 해결: CASE WHEN과 MAX문을 통해 가독성 UP

---
+++ programmers/group-by/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기/tw.sql
@@ -0,0 +1,13 @@
+SELECT
+    car_id,
+    CASE
+        WHEN MAX(
+            start_date <= '2022-10-16'
+            AND end_date >= '2022-10-16'
+        ) = 1
+        THEN '대여중'
+        ELSE '대여 가능'
+    END AS availability
+FROM car_rental_company_rental_history
+GROUP BY car_id
+ORDER BY car_id DESC
