6549e1c7628f4dd6fbb0c3d1a6d7d5fcae15bfa4
Jiyun Lee <166379348+jyun0618@users.noreply.github.com>
2026-05-17 20:47:54 +0900

    Submit: 이지윤
    - 고난: 
      - MAX() 계산을 CTE에서 하고 메인 쿼리에서 CASE WHEN을 적용했으나 불필요한 CTE 같다는 생각이 듦 
      - MAX() 내부 CASE WHEN에도 indentation을 적용하면 오히려 가독성이 떨어지는 것 같아.. 미적용함
    - 해결: MAX()와 CASE WHEN을 하나의 쿼리에서 계산하여 불필요한 CTE 제거

---
+++ programmers/group-by/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기/jiyun.sql
@@ -0,0 +1,10 @@
+SELECT
+    car_id,
+    CASE    
+        WHEN MAX(CASE WHEN '2022-10-16' BETWEEN start_date AND end_date THEN 1 ELSE 0 END) = 1 
+            THEN '대여중'
+        ELSE '대여 가능'
+    END AS availability
+FROM car_rental_company_rental_history
+GROUP BY car_id
+ORDER BY car_id DESC
