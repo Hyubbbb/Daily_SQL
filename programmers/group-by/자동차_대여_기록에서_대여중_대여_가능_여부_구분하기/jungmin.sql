f90d8fd6a086395299cc897cf2ad5780890e92b6
Jungmin Park <bakjeongmin021009@gmail.com>
2026-05-21 15:14:27 +0900

    Style: #25

--- programmers/group-by/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기/jungmin.sql
+++ programmers/group-by/자동차_대여_기록에서_대여중_대여_가능_여부_구분하기/jungmin.sql
@@ -1,11 +1,10 @@
 SELECT
     car_id,
-    MAX(CASE
-        WHEN start_date > '2022-10-16'
-            OR end_date < '2022-10-16'
-            THEN '대여 가능'
-        ELSE '대여중'
-    END) AS availability
+    CASE 
+        WHEN MAX('2022-10-16' BETWEEN start_date AND end_date) = 1
+            THEN '대여중'
+        ELSE '대여 가능'
+    END AS availability
 FROM car_rental_company_rental_history
 GROUP BY car_id
-ORDER BY car_id DESC;
+ORDER BY car_id DESC
