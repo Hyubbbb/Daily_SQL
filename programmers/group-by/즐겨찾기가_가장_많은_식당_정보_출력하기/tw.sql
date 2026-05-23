de080806c5bebbdc23b1b2379f9ad0dca7c406f9
tux2won <114200466+tux2won@users.noreply.github.com>
2026-05-23 16:23:26 +0900

    Submit: 송태원

---
+++ programmers/group-by/즐겨찾기가_가장_많은_식당_정보_출력하기/tw.sql
@@ -0,0 +1,18 @@
+WITH favorites_info AS (
+    SELECT
+        food_type,
+        MAX(favorites) AS max_favorites
+    FROM rest_info
+    GROUP BY food_type
+    )
+
+SELECT
+    rest.food_type,
+    rest.rest_id,
+    rest.rest_name,
+    rest.favorites
+FROM rest_info AS rest
+    JOIN favorites_info AS fav
+        ON rest.favorites = fav.max_favorites
+        AND rest.food_type = fav.food_type
+ORDER BY rest.food_type DESC
