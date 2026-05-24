930c1b721a99317dd8f3c430be9c6c2378f1b5da
HYUNN <zeonnii721@gmail.com>
2026-05-24 20:12:57 +0900

    Submit: 윤지현
    고난: group by 이후 최대 favorites인 식당만 남기는 방법을 찾지 못함
    해결: WHERE절로 group by 수행한 뒤 favorites가 최대인 식당 필터링

---
+++ programmers/group-by/즐겨찾기가_가장_많은_식당_정보_출력하기/jihyun.sql
@@ -0,0 +1,11 @@
+SELECT food_type, 
+       rest_id, 
+       rest_name, 
+       favorites
+FROM rest_info 
+WHERE 1=1
+    AND (food_type, favorites) IN (
+            SELECT food_type, MAX(favorites)
+            FROM rest_info
+            GROUP BY food_type)
+ORDER BY food_type DESC
