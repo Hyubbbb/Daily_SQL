ac072fd345048bf2bcf8acde9a38641716498659
yundabin <156299922+yundavin@users.noreply.github.com>
2026-05-25 16:52:17 +0900

    Submit: 윤다
    - 고난: 음식 종류별 favorites를 구하고 원 테이블과 합치는 방법
    - 해결: with에서 음식 종류별 favorites를 구하기 이후에 합친다

---
+++ programmers/group-by/즐겨찾기가_가장_많은_식당_정보_출력하기/dabin.sql
@@ -0,0 +1,18 @@
+WITH max_favorites_by_food_type AS (
+    SELECT
+        food_type,
+        MAX(favorites) AS max_favorites
+    FROM rest_info
+    GROUP BY food_type
+)
+
+SELECT
+    ri.food_type,
+    ri.rest_id,
+    ri.rest_name,
+    ri.favorites
+FROM rest_info AS ri
+INNER JOIN max_favorites_by_food_type AS mf
+    ON ri.food_type = mf.food_type
+    AND ri.favorites = mf.max_favorites
+ORDER BY ri.food_type DESC;
