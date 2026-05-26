4019da2c517aadc6da86b9ea35534471eb92eff3
Jungmin Park <bakjeongmin021009@gmail.com>
2026-05-26 17:00:25 +0900

    Submit: 박정민
    - 고난: 세대를 구함에 있어서 JOIN을 계속 진행하여 진행하기에는 메모리가 너무 커보였음
    - 해결: WITH RECURSIVE를 이용하여 세대를 구하는 CTE를 구한 후 JOIN하여 해당 문제를 해결하였음

---
+++ programmers/select/멸종위기의_대장균_찾기/jungmin.sql
@@ -0,0 +1,31 @@
+-- parent_id가 NULL이면 1세대
+-- parent_id에 없으면 자식 존재X
+WITH RECURSIVE gen_table AS (
+    SELECT      -- 1세대 찾기
+        id,
+        parent_id,
+        1 AS generation
+    FROM ecoli_data
+    WHERE parent_id IS NULL 
+    
+    UNION ALL
+    
+    SELECT
+        ecoli.id,
+        ecoli.parent_id,
+        gen.generation + 1
+    FROM ecoli_data AS ecoli
+        JOIN gen_table AS gen
+            ON ecoli.parent_id = gen.id
+)
+
+SELECT
+    COUNT(*) AS count,
+    gen.generation
+FROM gen_table AS gen
+    LEFT JOIN ecoli_data AS ch
+        ON gen.id = ch.parent_id
+WHERE 1=1
+    AND ch.id IS NULL
+GROUP BY gen.generation
+ORDER BY gen.generation
