d811300096a80120e86e18b0a1b9df1c9d41450e
Jungmin Park <bakjeongmin021009@gmail.com>
2026-05-26 17:01:32 +0900

    Submit: 박정민

---
+++ Archive/Season_12/Week_4/jungmin_problem_2.sql
@@ -0,0 +1,10 @@
+-- https://solvesql.com/problems/settled-sellers-2/
+SELECT
+    seller_id,
+    COUNT(DISTINCT order_id) AS orders
+FROM olist_order_items_dataset
+WHERE 1=1
+    AND price >= 50
+GROUP BY seller_id
+HAVING COUNT(DISTINCT order_id) >= 100
+ORDER BY orders DESC;
