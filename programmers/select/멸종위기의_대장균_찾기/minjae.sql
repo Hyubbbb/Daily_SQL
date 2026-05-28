54fdb90ac6d21afdb5de3a0a6cae29df80d037dd
mmminjaekim <minjaekim00@snu.ac.kr>
2026-05-28 17:36:59 +0900

    Submit: 김민재
    - 고난: 자식 없는 개체를 찾기 위해 NOT IN과 IS NOT NULL을 썼으나 이중 부정 구조라 가독성 떨어지는 느낌을 받음.
    - 해결: LEFT JOIN을 사용해 전면 수정.

---
+++ programmers/select/멸종위기의_대장균_찾기/minjae.sql
@@ -0,0 +1,28 @@
+WITH RECURSIVE generation_data AS (
+    SELECT 
+        id, 
+        parent_id, 
+        1 AS generation
+    FROM ecoli_data
+    WHERE parent_id IS NULL
+
+    UNION ALL
+
+    SELECT 
+        child.id, 
+        child.parent_id, 
+        parent.generation + 1 AS generation
+    FROM ecoli_data AS child
+        INNER JOIN generation_data AS parent
+            ON child.parent_id = parent.id
+)
+
+SELECT 
+    COUNT(gen.id) AS count,
+    gen.generation
+FROM generation_data AS gen 
+    LEFT JOIN ecoli_data AS parent_check 
+        ON gen.id = parent_check.parent_id 
+WHERE parent_check.parent_id IS NULL
+GROUP BY gen.generation
+ORDER BY gen.generation ASC;
