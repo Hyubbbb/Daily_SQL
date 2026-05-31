3007c913f71af5e007bc0a8993efa6eb27565fd3
tux2won <114200466+tux2won@users.noreply.github.com>
2026-05-31 21:38:28 +0900

    Submit: 송태원
    - 고난: UNION과 재귀 개념이 오랜만
    - 해결: 이전 풀이 이해 후, 가독성 엉망인 이전 쿼리 컨벤션 및 Alias명 수정

---
+++ programmers/select/멸종위기의_대장균_찾기/tw.sql
@@ -0,0 +1,45 @@
+-- WITH RECURSIVE: CTE 안에서 자기 자신을 참조 가능함
+
+WITH RECURSIVE gen_info AS (
+    SELECT
+        ecoli.id AS id,
+        ecoli.parent_id AS parent_id,
+        1 AS generation
+    FROM ecoli_data AS ecoli
+    WHERE 1=1
+        AND ecoli.parent_id IS NULL
+
+    UNION ALL
+  
+-- 그 다음 세대부터는 +1
+  
+    SELECT
+        child.id AS id,
+        child.parent_id AS parent_id,
+        gen.generation + 1 AS generation
+    FROM ecoli_data AS child
+        JOIN gen_info AS gen
+            ON child.parent_id = gen.id
+),
+  
+child_count AS (
+    SELECT
+        parent.id AS id,
+        COUNT(child.id) AS child_count
+    FROM ecoli_data AS parent
+        JOIN ecoli_data AS child
+            ON parent.id = child.parent_id
+    GROUP BY parent.id
+)
+
+-- 최종 구하는 단계
+SELECT
+    COUNT(*) AS count,
+    gen.generation AS generation
+FROM gen_info AS gen
+    JOIN child_count AS child_count
+        ON gen.id = child_count.id
+WHERE 1=1
+    AND child_count.child_count = 0
+GROUP BY generation
+ORDER BY generation
