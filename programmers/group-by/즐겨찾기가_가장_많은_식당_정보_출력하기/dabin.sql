1f204e66260c66a42ba33d0712ba59f7b86df161
yundabin <156299922+yundavin@users.noreply.github.com>
2026-05-31 20:50:06 +0900

    Style: #26

--- programmers/group-by/즐겨찾기가_가장_많은_식당_정보_출력하기/dabin.sql
+++ programmers/group-by/즐겨찾기가_가장_많은_식당_정보_출력하기/dabin.sql
@@ -14,5 +14,5 @@ SELECT
 FROM rest_info AS ri
 INNER JOIN max_favorites_by_food_type AS mf
     ON ri.food_type = mf.food_type
-    AND ri.favorites = mf.max_favorites
-ORDER BY ri.food_type DESC;
+        AND ri.favorites = mf.max_favorites
+ORDER BY ri.food_type DESC
