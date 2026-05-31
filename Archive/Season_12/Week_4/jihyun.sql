8b89e532d64fd0479b65cea4a81a57030238ee63
HYUNN <zeonnii721@gmail.com>
2026-05-31 21:16:25 +0900

    Submit: 윤지현
    고난: 주소 이름에 '강원도'가 들어가는 행만 추출하는 방법이 헷갈림
    해결: LIKE 연산자를 활용해서 '강원도'로 시작하는 주소명만 추출하도록 설정

---
+++ Archive/Season_12/Week_4/jihyun.sql
@@ -0,0 +1,9 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/131112
+SELECT 
+    factory_id, 
+    factory_name, 
+    address
+FROM food_factory AS f_factory
+WHERE 1=1
+    AND address LIKE '강원도%'
+ORDER BY factory_id
