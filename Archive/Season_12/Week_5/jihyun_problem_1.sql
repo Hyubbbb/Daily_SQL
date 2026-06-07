0f013cc48f23926b169b460da6eb1a76c0b6ec6c
HYUNN <zeonnii721@gmail.com>
2026-06-07 20:29:20 +0900

    Submit: 윤지현

---
+++ Archive/Season_12/Week_5/jihyun_problem_1.sql
@@ -0,0 +1,9 @@
+--https://school.programmers.co.kr/learn/courses/30/lessons/59041
+SELECT name, 
+       COUNT(*) AS count
+FROM animal_ins
+WHERE 1=1
+    AND name IS NOT NULL
+GROUP BY name
+HAVING COUNT(*) > 1
+ORDER BY name
