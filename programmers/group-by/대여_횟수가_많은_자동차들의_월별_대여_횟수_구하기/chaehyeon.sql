4825394f9e81161d40df783a5d01a083ba9f5422
chae117 <lee6460e@gmail.com>
2026-06-07 18:07:04 +0900

    Submit: 이채현
    -고난: 서브쿼리를 만들지 않고 해결하고 싶었지만 쿼리가 더 복잡해짐
    -해결: CTE로 5회 이상인 car_id를 구하고 join해서 해결

---
+++ programmers/group-by/대여_횟수가_많은_자동차들의_월별_대여_횟수_구하기/chaehyeon.sql
@@ -0,0 +1,19 @@
+WITH child AS(
+SELECT 
+    car_id
+FROM car_rental_company_rental_history
+WHERE start_date BETWEEN '2022-08-01' AND '2022-10-31'
+GROUP BY car_id
+HAVING COUNT(*) >= 5
+)
+
+SELECT 
+    MONTH(p.start_date) AS month,
+    p.car_id,
+    count(*) AS records
+FROM car_rental_company_rental_history AS p
+    JOIN child AS c
+        ON p.car_id = c.car_id
+WHERE p.start_date BETWEEN '2022-08-01' AND '2022-10-31'
+GROUP BY MONTH(p.start_date), car_id
+ORDER BY month, car_id DESC;
