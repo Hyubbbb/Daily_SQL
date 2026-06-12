1913298fe42756014f14c02c840e7f9145d819d1
yundabin <156299922+yundavin@users.noreply.github.com>
2026-06-12 18:01:09 +0900

    Submit: 윤다빈

---
+++ programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/dabin.sql
@@ -0,0 +1,14 @@
+-- 조건에 맞는 사용자와 총 거래금액 조회하기
+SELECT 
+  board.writer_id AS user_id,
+  user.nickname,
+  SUM(board.price) AS total_sales
+FROM used_goods_board AS board
+LEFT JOIN used_goods_user AS user
+    ON board.writer_id = user.user_id
+WHERE board.status='DONE'
+GROUP BY 
+  board.writer_id,
+  user.nickname
+HAVING SUM(board.price) >= 700000
+ORDER BY total_sales ASC
