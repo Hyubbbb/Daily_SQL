703354579c60ff7393ed23af688e63935639838d
HYUNN <zeonnii721@gmail.com>
2026-06-13 20:30:50 +0900

    Style: #28

--- programmers/group-by/대여_횟수가_많은_자동차들의_월별_대여_횟수_구하기/jihyun.sql
+++ programmers/group-by/대여_횟수가_많은_자동차들의_월별_대여_횟수_구하기/jihyun.sql
@@ -1,16 +1,18 @@
 WITH rental_num AS (
-    SELECT car_id,
-           COUNT(*) AS records
+    SELECT 
+        car_id,
+        COUNT(*) AS records
     FROM car_rental_company_rental_history
     WHERE 1=1
-        AND start_date BETWEEN '2022-08-01'AND '2022-10-31'
+        AND start_date BETWEEN '2022-08-01' AND '2022-10-31'
     GROUP BY car_id
     HAVING COUNT(*) >= 5
 )
 
-SELECT MONTH(history.start_date) AS month,
-       car_id,
-       COUNT(*) AS records
+SELECT 
+    MONTH(history.start_date) AS month,
+    car_id,
+    COUNT(*) AS records
 FROM car_rental_company_rental_history AS history
 WHERE 1=1
     AND history.car_id IN (
