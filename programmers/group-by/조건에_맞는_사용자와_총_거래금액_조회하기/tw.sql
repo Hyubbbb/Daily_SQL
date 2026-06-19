920698030d65beabc3e28cf32443135f1732fa81
tux2won <114200466+tux2won@users.noreply.github.com>
2026-06-19 14:46:04 +0900

    Style: #29

--- programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/tw.sql
+++ programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/tw.sql
@@ -1,14 +1,29 @@
 -- 완료된(WHERE = "DONE") 중고 거래의 총금액이 70만 원 이상(HAVING)인 사람
 
+-- SELECT
+--     user.user_id,
+--     user.nickname,
+--     SUM(price) AS total_price
+-- FROM used_goods_board AS board
+--     JOIN used_goods_user AS user
+--         ON board.writer_id = user.user_id
+-- WHERE 1=1
+--     AND status = "DONE"
+-- GROUP BY board.writer_id
+-- HAVING total_price >= 700000
+-- ORDER BY total_price
+
 SELECT
     user.user_id,
     user.nickname,
-    SUM(price) AS total_price
+    SUM(board.price) AS total_sales
 FROM used_goods_board AS board
     JOIN used_goods_user AS user
         ON board.writer_id = user.user_id
 WHERE 1=1
-    AND status = "DONE"
-GROUP BY board.writer_id
-HAVING total_price >= 700000
-ORDER BY total_price
+    AND board.status = 'DONE'
+GROUP BY 
+    user.user_id,
+    user.nickname
+HAVING SUM(board.price) >= 700000
+ORDER BY total_sales
