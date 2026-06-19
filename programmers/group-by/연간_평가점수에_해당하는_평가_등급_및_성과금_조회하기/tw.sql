180d68ad9c6279b406fddfc766a708cf052937f1
tux2won <114200466+tux2won@users.noreply.github.com>
2026-06-19 15:21:14 +0900

    Submit: 송태원
    - 고난: HR_DEPARTMENT 테이블의 사용 여부로 꽤 고민
    - 해결: 결론은 사용 안해도 문제에서 요구하는 칼럼 추출 가능

---
+++ programmers/group-by/연간_평가점수에_해당하는_평가_등급_및_성과금_조회하기/tw.sql
@@ -0,0 +1,27 @@
+WITH score_tbl AS (
+    SELECT 
+        emp_no,
+        CASE
+            WHEN AVG(score) >= 96 THEN 'S'
+            WHEN AVG(score) >= 90 THEN 'A'
+            WHEN AVG(score) >= 80 THEN 'B'
+            ELSE 'C'
+        END AS grade
+    FROM hr_grade
+    GROUP BY emp_no
+)
+
+SELECT 
+    employee.emp_no, 
+    employee.emp_name, 
+    score_tbl.grade,
+    CASE
+        WHEN score_tbl.grade = 'S' THEN employee.sal * 0.2
+        WHEN score_tbl.grade = 'A' THEN employee.sal * 0.15
+        WHEN score_tbl.grade = 'B' THEN employee.sal * 0.1
+        ELSE 0
+    END AS bonus
+FROM hr_employees AS employee 
+    LEFT JOIN score_tbl 
+        ON employee.emp_no = score_tbl.emp_no
+ORDER BY employee.emp_no
