e1ff7f8e7e012af7d1be03f55b04d463a1da7bb1
chae117 <lee6460e@gmail.com>
2026-06-21 09:13:55 +0900

    Style: #29

--- programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/chaehyeon.sql
+++ programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/chaehyeon.sql
@@ -5,7 +5,8 @@ SELECT
 FROM used_goods_user AS user
     JOIN used_goods_board AS board
         ON user.user_id = board.writer_id
-WHERE board.status = 'DONE'
+WHERE 1=1
+    AND board.status = 'DONE'
 GROUP BY user.user_id, user.nickname
 HAVING SUM(board.price) >= 700000
 ORDER BY total_sales;
