f7f7be63513e3dd665348fe1c9747150868604e7
HYUNN <zeonnii721@gmail.com>
2026-06-21 23:38:12 +0900

    Submit: 윤지현

---
+++ programmers/group-by/연간_평가점수에_해당하는_평가_등급_및_성과금_조회하기/jihyun.sql
@@ -0,0 +1,32 @@
+WITH employee_avg_grade AS (
+    SELECT
+        emp.emp_no,
+        emp.emp_name,
+        emp.sal,
+        AVG(g.score) AS avg_score
+    FROM hr_employees AS emp
+        INNER JOIN hr_grade AS g
+            ON emp.emp_no = g.emp_no
+    GROUP BY
+        emp.emp_no,
+        emp.emp_name,
+        emp.sal
+)
+
+SELECT
+    emp_no,
+    emp_name,
+    CASE
+        WHEN avg_score >= 96 THEN 'S'
+        WHEN avg_score >= 90 THEN 'A'
+        WHEN avg_score >= 80 THEN 'B'
+        ELSE 'C'
+    END AS grade,
+    CASE
+        WHEN avg_score >= 96 THEN sal * 0.2
+        WHEN avg_score >= 90 THEN sal * 0.15
+        WHEN avg_score >= 80 THEN sal * 0.1
+        ELSE 0
+    END AS bonus
+FROM employee_avg_grade
+ORDER BY emp_no
