ffc18a039f48729af3d95ff91cb1ef1da05dc949
HYUNN <zeonnii721@gmail.com>
2026-06-21 23:33:09 +0900

    Style: #29

--- programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/jihyun.sql
+++ programmers/group-by/조건에_맞는_사용자와_총_거래금액_조회하기/jihyun.sql
@@ -1,19 +1,21 @@
-WITH price AS (
+WITH completed_sales AS (
     SELECT 
-        writer_id, 
-        price
-    FROM used_goods_board
+        ugb.writer_id, 
+        ugb.price
+    FROM used_goods_board AS ugb
     WHERE 1=1
-        AND status = 'DONE'
+        AND ugb.status = 'DONE'
 )
 
 SELECT 
-    user_id,
-    user.nickname,
-    SUM(price) AS total_sales
-FROM used_goods_user AS user
-    LEFT JOIN price
-        ON user.user_id = price.writer_id
-GROUP BY user_id
-HAVING SUM(price) >= 700000 
-ORDER BY total_sales
+    ugu.user_id,
+    ugu.nickname,
+    SUM(cs.price) AS total_sales
+FROM used_goods_user AS ugu
+INNER JOIN completed_sales AS cs
+    ON ugu.user_id = cs.writer_id
+GROUP BY 
+    ugu.user_id,
+    ugu.nickname
+HAVING SUM(cs.price) >= 700000
+ORDER BY total_sales ASC;
