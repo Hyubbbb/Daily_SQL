d8990ab60887bea37921bf1b631849bc42d9fbb0
celinalab <pogang505799@ewhain.net>
2026-07-08 14:56:06 +0900

    Style: #31

--- programmers/string-date/오랜_기간_보호한_동물(2)/hyunjin.sql
+++ programmers/string-date/오랜_기간_보호한_동물(2)/hyunjin.sql
@@ -4,5 +4,5 @@ SELECT
 FROM animal_ins AS ins
     INNER JOIN animal_outs AS outs
         ON ins.animal_id = outs.animal_id
-ORDER BY (outs.datetime - ins.datetime) DESC
+ORDER BY DATEDIFF(outs.datetime, ins.datetime) DESC
 LIMIT 2;
