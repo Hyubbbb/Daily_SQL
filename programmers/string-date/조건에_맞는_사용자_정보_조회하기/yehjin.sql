e45dc71f498cc57b53707c5f0b3d57d926e4d645
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-07-11 13:58:18 +0900

    Submit: 이예진
    고난: 3건 이상 등록한 사용자를 조회하는 로직 / 하이픈을 전화번호 문자열 사이사이에 넣는 것
    해결: 다양한 방식으로 쿼리 작성해봄 / 검색해서 찾아냄

---
+++ programmers/string-date/조건에_맞는_사용자_정보_조회하기/yehjin.sql
@@ -0,0 +1,11 @@
+SELECT
+    user.user_id,
+    user.nickname,
+    CONCAT(user.city, ' ' ,user.street_address1, ' ', user.street_address2) AS 전체주소,
+    CONCAT(SUBSTRING(TLNO, 1, 3), '-', SUBSTRING(TLNO, 4, 4), '-', SUBSTRING(TLNO, 8, 4)) AS 전화번호
+FROM used_goods_board AS board
+    INNER JOIN used_goods_user AS user
+        ON board.writer_id = user.user_id
+GROUP BY user_id
+HAVING COUNT(*)>=3
+ORDER BY user_id DESC
