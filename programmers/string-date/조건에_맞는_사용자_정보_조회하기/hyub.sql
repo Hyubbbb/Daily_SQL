3fba32368879e7540b58582908d2738444cd0331
Hyubbbb <hyub@fnfcorp.com>
2026-07-14 14:44:27 +0900

    Style: #32

--- programmers/string-date/조건에_맞는_사용자_정보_조회하기/hyub.sql
+++ programmers/string-date/조건에_맞는_사용자_정보_조회하기/hyub.sql
@@ -2,10 +2,10 @@ SELECT
     u.user_id,
     u.nickname,
     CONCAT(u.city, ' ', u.street_address1, ' ', u.street_address2) AS '전체주소',
-    CONCAT(SUBSTRING(u.TLNO, 1, 3), '-',SUBSTRING(u.TLNO, 4, 4), '-', SUBSTRING(u.TLNO, 8, 4)) AS '전화번호'
+    CONCAT(SUBSTRING(u.tlno, 1, 3), '-', SUBSTRING(u.tlno, 4, 4), '-', SUBSTRING(u.tlno, 8, 4)) AS '전화번호'
 FROM used_goods_board AS b
     JOIN used_goods_user AS u
         ON b.writer_id = u.user_id
 GROUP BY b.writer_id
 HAVING COUNT(*) >= 3
-ORDER BY u.user_id DESC
+ORDER BY u.user_id DESC;
