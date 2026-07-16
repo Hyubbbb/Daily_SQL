5d52aa3a72532d1fe775204e78714a0df880b3cd
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-07-16 16:26:59 +0900

    Style: #32

--- programmers/string-date/조건에_맞는_사용자_정보_조회하기/yehjin.sql
+++ programmers/string-date/조건에_맞는_사용자_정보_조회하기/yehjin.sql
@@ -2,7 +2,7 @@ SELECT
     user.user_id,
     user.nickname,
     CONCAT(user.city, ' ' ,user.street_address1, ' ', user.street_address2) AS 전체주소,
-    CONCAT(SUBSTRING(TLNO, 1, 3), '-', SUBSTRING(TLNO, 4, 4), '-', SUBSTRING(TLNO, 8, 4)) AS 전화번호
+    CONCAT(SUBSTRING(user.tlno, 1, 3), '-', SUBSTRING(user.tlno, 4, 4), '-', SUBSTRING(user.tlno, 8, 4)) AS 전화번호
 FROM used_goods_board AS board
     INNER JOIN used_goods_user AS user
         ON board.writer_id = user.user_id
