97d8e6811859cd5fd9bf75f245104ae6a20b3eed
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-07-16 17:51:44 +0900

    Submit: 이예진
    고난: '종류가 세단인 자동차들 중 10월 대여'를 어떻게 해야 가장 간결하고 가독성 좋게 구현할 수 있을지에 대해 고민함
    해결: 처음에는 CTE를 사용해 종류가 세단인 것만 추출한 후 이후 코드를 작성하였는데 AND를 활용하면 CTE없이 더 간결하게 풀이할 수 있음을 깨달음

---
+++ programmers/string-date/대여_기록이_존재하는_자동차_리스트_구하기/yehjin.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT rent.car_id
+FROM car_rental_company_car AS rent
+    JOIN car_rental_company_rental_history AS history
+        ON rent.car_id = history.car_id
+WHERE 1=1
+    AND rent.car_type = '세단'
+        AND MONTH(history.start_date) = 10
+ORDER BY rent.car_id DESC;
