5bfaac00af7702d07170cec07f9f14470082acd1
celinalab <pogang505799@ewhain.net>
2026-08-09 20:14:54 +0900

    Submit: 임현진
    고난: 어렵지 않게 해결하였다!
    해결: "

---
+++ programmers/group-by/부서별_평균_연봉_조회하기/hyunjin.sql
@@ -0,0 +1,9 @@
+SELECT 
+    he.dept_id,
+    hd.dept_name_en,
+    ROUND(AVG(he.sal), 0) AS AVG_SAL 
+FROM hr_employees AS he
+    LEFT JOIN hr_department AS hd
+        ON he.dept_id = hd.dept_id
+GROUP BY he.dept_id
+ORDER BY avg_sal DESC
