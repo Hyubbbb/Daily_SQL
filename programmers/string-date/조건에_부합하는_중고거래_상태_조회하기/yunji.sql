ce9c82ffa330348cc4381216189f401909c32b6f
Son Yunji <100953788+yunjeee@users.noreply.github.com>
2026-08-26 01:35:11 +0900

    Style: #38

--- programmers/string-date/조건에_부합하는_중고거래_상태_조회하기/yunji.sql
+++ programmers/string-date/조건에_부합하는_중고거래_상태_조회하기/yunji.sql
@@ -8,8 +8,10 @@ SELECT
             THEN '판매중'
         WHEN status = 'RESERVED'
             THEN '예약중'
-        ELSE '거래완료'
+        WHEN status = 'DONE'
+            THEN '거래완료'
     END AS status
 FROM used_goods_board
-WHERE created_date = '2022-10-05'
+WHERE 1=1
+    AND created_date = '2022-10-05'
 ORDER BY board_id DESC;
