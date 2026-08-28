d3c907d40f94c53de66c872f14e0051f8cc04313
Yehjin-Lee <226120389+Yehjin-Lee@users.noreply.github.com>
2026-08-28 20:32:25 +0900

    Style: #38

--- programmers/string-date/조건에_부합하는_중고거래_상태_조회하기/yehjin.sql
+++ programmers/string-date/조건에_부합하는_중고거래_상태_조회하기/yehjin.sql
@@ -3,10 +3,13 @@ SELECT
     writer_id,
     title,
     price,
-    CASE status
-        WHEN 'SALE' THEN '판매중'
-        WHEN 'RESERVED' THEN '예약중'
-        WHEN 'DONE' THEN '거래완료'
+    CASE
+        WHEN status='SALE'
+					THEN '판매중'
+        WHEN status='RESERVED'
+					THEN '예약중'
+        WHEN status='DONE'
+					THEN '거래완료'
     END AS status
 FROM used_goods_board
 WHERE 1=1
