8da30c7ebe47cfa76c660babe7314f10748afae7
Jungmin Park <bakjeongmin021009@gmail.com>
2026-08-31 15:30:39 +0900

    Submit: 박정민
    - 고난: 셀프조인으로 문제를 풀고자 하였고, DATE 함수를 이용하여 timestamp를 Rolling하고자 하였는데, DATE_ADD안에 들어가는 함수를 잊음.
    - 해결: 구글링을 통해 DATE_ADD안에 INTERVAL이 들어가는 것을 찾고, 이를 적용하여 문제를 해결함.

---
+++ solvesql/다음날도_서울숲의_미세먼지_농도는_나쁨/jungmin.sql
@@ -0,0 +1,10 @@
+SELECT
+    today.measured_at AS today,
+    next_day.measured_at AS next_day,
+    today.pm10 AS pm10,
+    next_day.pm10 AS next_pm10
+FROM measurements AS today
+    JOIN measurements AS next_day
+        ON DATE_ADD(today.measured_at, INTERVAL 1 DAY) = next_day.measured_at
+WHERE 1=1
+    AND today.pm10 < next_day.pm10
