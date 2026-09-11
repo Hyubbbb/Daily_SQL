6880e5365b2f5c6e88f6ac63f00a6a58f3871956
celinalab <pogang505799@ewhain.net>
2026-09-11 15:53:00 +0900

    Submit: 임현진
    고난: 한 테이블을 두개로 이용하는 방법을 처음 접했다.
    해결: DATE_ADD를 이용해 해결하였다.

--- solvesql/다음날도_서울숲의_미세먼지_농도는_나쁨/hyunjin.sql
+++ solvesql/다음날도_서울숲의_미세먼지_농도는_나쁨/hyunjin.sql
@@ -7,4 +7,4 @@ FROM measurements AS m1
     JOIN measurements AS m2
         ON DATE_ADD(m1.measured_at, INTERVAL 1 DAY) = m2.measured_at
 WHERE 1=1
-  AND m1.pm10 < m2.pm10
+    AND m1.pm10 < m2.pm10
