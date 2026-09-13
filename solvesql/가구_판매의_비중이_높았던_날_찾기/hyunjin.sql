b69025803daa337c7f0a3060180f905ac2624c6e
celinalab <pogang505799@ewhain.net>
2026-09-13 23:32:03 +0900

    Submit: 임현진
    고난: 처음에 count행수로 계산안하고 quantity로 해서 애를 먹음..
    해결: count함수로 해결!

---
+++ solvesql/가구_판매의_비중이_높았던_날_찾기/hyunjin.sql
@@ -0,0 +1,21 @@
+WITH summary AS (
+  SELECT 
+      order_date,
+      COUNT(*) AS total_cnt,
+      COUNT(CASE 
+                WHEN category = 'Furniture' 
+                    THEN 1 
+            END) AS furniture
+  FROM records
+  GROUP BY order_date
+)
+
+SELECT
+    order_date,
+    furniture,
+    ROUND(furniture * 100.0 / total_cnt, 2) AS furniture_pct
+FROM summary
+WHERE 1=1
+    AND total_cnt >= 10
+    AND (furniture * 100.0 / total_cnt) >= 40
+ORDER BY furniture_pct DESC, order_date ASC
