ba3a5a835dffe07b3b204bd71c8047ee6d2486e9
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2026-09-20 23:28:07 +0900

    Submit: 이형주
    - 고난: pivot은 어떻게 하는건지 감도 안왔다.. pivot을 해결하니 숫자가 알파벳 순서대로 나열하며 더이상 이름이 없는 경우를 어떻게 해결하는지 또다시 막혔다..
    - 해결: gpt에게 pivot 하는 방법 팁을 얻어와서 CASE WHEN MAX()를 활용하는 것까진 성공했다!! 두번째 고난을 또 다시 gpt에게 도움 요청해서 ROW_NUMBER을 활용하는 방법을 획득해서 해결하고 이번에 배운 새로운 방법을 정리해서 익혀서 다음엔 꼭 스스로 풀겠다고 다짐했다..

---
+++ hackerrank/Occupations/hj.sql
@@ -0,0 +1,28 @@
+WITH ranked AS (
+    SELECT
+        name,
+        occupation,
+        ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY name) AS rn
+    FROM occupations
+)
+
+SELECT
+    MAX(CASE 
+            WHEN occupation = 'Doctor' 
+                THEN name 
+        END) AS doctor,
+    MAX(CASE 
+            WHEN occupation = 'Professor'   
+                THEN name 
+        END) AS professor,
+    MAX(CASE 
+            WHEN occupation = 'Singer' 
+                THEN name 
+        END) AS singer,
+    MAX(CASE 
+            WHEN occupation = 'Actor' 
+                THEN name 
+        END) AS actor
+FROM ranked
+GROUP BY rn
+ORDER BY rn;
