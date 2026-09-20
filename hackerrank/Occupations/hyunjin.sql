bb61f335dfd00252c6689ce76d20d5231ec661c1
celinalab <pogang505799@ewhain.net>
2026-09-20 23:28:15 +0900

    Submit: 임현진
    고난: 큰 어려움이 없었다
    해결: !

---
+++ hackerrank/Occupations/hyunjin.sql
@@ -0,0 +1,27 @@
+WITH ranked AS (
+    SELECT
+        name,
+        occupation,
+        ROW_NUMBER() OVER (PARTITION BY occupation ORDER BY name) AS rn
+    FROM occupations
+)
+SELECT
+    MAX(CASE 
+            WHEN occupation = 'Doctor' 
+                THEN name 
+        END) AS doctor,
+    MAX(CASE 
+            WHEN occupation = 'Professor' 
+                THEN name 
+        END) AS professor,
+    MAX(CASE 
+            WHEN occupation = 'Singer' 
+                THEN Name 
+        END) AS singer,
+    MAX(CASE 
+            WHEN occupation = 'Actor' 
+                THEN Name 
+        END) AS actor
+FROM ranked
+GROUP BY rn
+ORDER BY rn;
