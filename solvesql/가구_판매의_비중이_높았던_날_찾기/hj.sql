ac7016b622059c33166713b418b1d9cc23a180ed
leehj2586 <162440113+leehj2586@users.noreply.github.com>
2026-09-20 23:08:10 +0900

    Style: #40

--- solvesql/가구_판매의_비중이_높았던_날_찾기/hj.sql
+++ solvesql/가구_판매의_비중이_높았던_날_찾기/hj.sql
@@ -1,20 +1,20 @@
-WITH daily_counts AS (
+WITH daily_count AS (
     SELECT
         order_date,
         COUNT(DISTINCT CASE 
                           WHEN category = 'Furniture' 
                               THEN order_id 
                       END) AS furniture,
-        COUNT(DISTINCT order_id) AS total_orders
+        COUNT(DISTINCT order_id) AS total_order
     FROM records
     GROUP BY order_date
   )
 SELECT
     order_date,
     furniture,
-    ROUND(furniture * 100.0 / total_orders, 2) AS furniture_pct
-FROM daily_counts
+    ROUND(furniture * 100.0 / total_order, 2) AS furniture_pct
+FROM daily_count
 WHERE 1=1
-    AND total_orders >= 10
-    AND (furniture * 100.0 / total_orders) >= 40
+    AND total_order >= 10
+    AND (furniture * 100.0 / total_order) >= 40
 ORDER BY furniture_pct DESC, order_date;
